96fcc4f9de87adbef3b13bc91498a18a20482103
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-07-25 09:55:38 -0700

    Time: 144 ms (33.43%), Space: 47.5 MB (54.83%) - LeetHub

---
+++ 207-course-schedule/207-course-schedule.js
@@ -0,0 +1,38 @@
+/**
+ * @param {number} numCourses
+ * @param {number[][]} prerequisites
+ * @return {boolean}
+ */
+var canFinish = function(numCourses, prerequisites) {
+    const graph = {}
+    const indegree = {}
+    for (let i=0; i < numCourses; i++) {
+        graph[i] = []
+        indegree[i] = 0
+    }
+    for (let [course, req] of prerequisites) {
+        graph[req].push(course)
+        indegree[course]++
+    }
+    const set = new Set()
+    const q = []
+    for (let i=0; i < numCourses; i++) {
+        if (indegree[i] === 0) {
+            q.push(i)
+        }
+    }
+    while (q.length) {
+        const course = q.shift()
+        set.add(course)
+        for (let next of graph[course] || []) {
+            indegree[next]--
+            if (indegree[next] === 0) {
+                q.push(next)
+            }
+        }
+    }
+    for (let node in indegree) {
+        if (indegree[node] > 0) return false
+    }
+    return true
+};
