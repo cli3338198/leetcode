89429880784fada207fe4e6fed07e6d0aafc368b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-07-25 21:36:15 -0700

    Time: 77 ms (80.97%), Space: 42.1 MB (89.77%) - LeetHub

--- 91-decode-ways/91-decode-ways.js
+++ 91-decode-ways/91-decode-ways.js
@@ -3,18 +3,20 @@
  * @return {number}
  */
 var numDecodings = function(s) {
-    const dp = {}
-    return helper(0)
+    const n = s.length
+    const dp = Array(n + 1).fill(0)
     
-    function helper(idx) {
-        if (idx in dp) return dp[idx]
-        if (idx === s.length) return 1
-        if (s[idx] === '0') return 0
-        let res = 0
-        res += helper(idx + 1)
-        if (idx + 1 < s.length && Number(s[idx] + s[idx+1]) <= 26) {
-            res += helper(idx + 2)
+    dp[0] = 1
+    dp[1] = s[0] === '0' ? 0 : 1
+    
+    for (let i=2; i <= n; i++) {
+        if (s[i-1] !== '0') {
+            dp[i] += dp[i-1]
         }
-        return dp[idx] = res
+        if (s[i-2] == 1 || (s[i-2] == 2 && s[i-1] <= 6)) {
+            dp[i] += dp[i-2]
+        }
     }
+    
+    return dp[n]
 };
