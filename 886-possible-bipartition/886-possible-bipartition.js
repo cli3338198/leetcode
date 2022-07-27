93fdf0cc96dcee066dc76623d82ff1cebecde78f
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-07-26 19:46:46 -0700

    Time: 193 ms (64.15%), Space: 53.5 MB (63.21%) - LeetHub

--- 886-possible-bipartition/886-possible-bipartition.js
+++ 886-possible-bipartition/886-possible-bipartition.js
@@ -4,46 +4,26 @@
  * @return {boolean}
  */
 var possibleBipartition = function(n, dislikes) {
-    class UF {
-        constructor() {
-            this.root = {}
-        }
-        insert(a) {
-            if (!(a in this.root)) {
-                this.root[a] = a
-            }
-        }
-        find(a) {
-            if (this.root[a] !== a) {
-                this.root[a] = this.find(this.root[a])
-            }
-            return this.root[a]
-        }
-        union(a, b) {
-            const A = this.find(a)
-            const B = this.find(b)
-            if (A === B) return
-            this.root[B] = A
-        }
-    }
-    
-    const uf = new UF()
     const graph = {}
     for (let [u, v] of dislikes) {
         graph[u] ? graph[u].push(v) : graph[u] = [v]
         graph[v] ? graph[v].push(u) : graph[v] = [u]
     }
-    
-    for (let i=1; i <= n; i++) {
-        uf.insert(i)
-    }
-    
+    const colors = Array(n+1).fill(0)
     for (let i=1; i <= n; i++) {
         if (!(i in graph)) continue
-        for (let j of graph[i]) {
-            if (uf.find(i) === uf.find(j)) return false
-            uf.union(graph[i][0], j)
-        }
+        if (colors[i] === 0 && !helper(i, 1)) return false
     }
     return true
+    
+    function helper(node, color) {
+        if (colors[node] !== 0) {
+            return colors[node] === color
+        }
+        colors[node] = color
+        for (let next of graph[node] || []) {
+            if (!helper(next, -color)) return false
+        }
+        return true
+    }
 };
