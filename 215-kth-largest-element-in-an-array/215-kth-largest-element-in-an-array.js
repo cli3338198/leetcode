8c98f9a86d9e5d6e684f4000ca0a26ccfa194c6d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-08-03 16:56:05 -0700

    Time: 211 ms (15.51%), Space: 50.7 MB (12.01%) - LeetHub

--- 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
+++ 215-kth-largest-element-in-an-array/215-kth-largest-element-in-an-array.js
@@ -4,6 +4,21 @@
  * @return {number}
  */
 var findKthLargest = function(nums, k) {
-    nums.sort((a, b) => a - b)
-    return nums[nums.length-k]
+    k = nums.length-k
+    return quickSelect(0, nums.length-1)
+    
+    function quickSelect(l, r) {
+        const pivot = nums[r]
+        let p = l
+        for (let i=l; i < r; i++) {
+            if (nums[i] <= pivot) {
+                [nums[i], nums[p]] = [nums[p], nums[i]]
+                p++
+            } 
+        }
+        [nums[p], nums[r]] = [nums[r], nums[p]]
+        if (p > k) return quickSelect(l, p-1)
+        if (p < k) return quickSelect(p+1, r)
+        return nums[p]
+    }
 };
