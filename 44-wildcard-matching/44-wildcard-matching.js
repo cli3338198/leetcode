6698119b301a04f2eb517c2ab1c4c7693dfc33a9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-08-02 22:38:30 -0700

    Time: 167 ms (81.67%), Space: 57.7 MB (39.17%) - LeetHub

---
+++ 44-wildcard-matching/44-wildcard-matching.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @param {string} p
+ * @return {boolean}
+ */
+var isMatch = function(s, p) {
+    const m = s.length
+    const n = p.length
+    const dp = Array(m+1).fill(0).map(() => Array(n+1).fill(false))
+    dp[0][0] = true
+    for (let i=1; i <= n; i++) {
+        if (p[i-1] === '*') {
+            dp[0][i] = dp[0][i-1]
+        }
+    }
+    for (let i=1; i <= m; i++) {
+        for (let j=1; j <= n; j++) {
+            if (s[i-1] === p[j-1] || p[j-1] === "?") {
+                dp[i][j] = dp[i-1][j-1]
+            } else if (p[j-1] === '*') {
+                dp[i][j] = dp[i][j-1] || dp[i-1][j]
+            }
+        }
+    }
+    return dp[m][n]
+};
