09083d2aaf7a0b4566a4882d8ce507cba61cc8cc
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-08-03 16:26:57 -0700

    Time: 114 ms (60.42%), Space: 42.5 MB (66.90%) - LeetHub

--- 518-coin-change-2/518-coin-change-2.js
+++ 518-coin-change-2/518-coin-change-2.js
@@ -4,16 +4,14 @@
  * @return {number}
  */
 var change = function(amount, coins) {
-    coins.sort((a, b) => a - b)
-    const dp = Array(coins.length + 1).fill(0).map(() => Array(amount+1).fill(0))
-    for (let i=0; i <= coins.length; i++) {
-        dp[i][0] = 1
-    }
-    for (let i=1; i <= coins.length; i++) {
-        for (let j=1; j <= amount; j++) {
-            dp[i][j] = dp[i-1][j]
-            if (coins[i-1] <= j) dp[i][j] += dp[i][j-coins[i-1]]
+    const dp = Array(amount+1).fill(0)
+    dp[0] = 1
+    for (let coin of coins) {
+        for (let i=1; i <= amount; i++) {
+            if (coin <= i) {
+                dp[i] += dp[i - coin]
+            }
         }
     }
-    return dp[coins.length][amount]
+    return dp[amount]
 };
