036bad0a95bf111cd566d7a0a345341074dda2b9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-08-03 21:24:35 -0700

    Time: 331 ms (21.06%), Space: 49.1 MB (10.29%) - LeetHub

---
+++ 45-jump-game-ii/45-jump-game-ii.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var jump = function(nums) {
+    const n = nums.length
+    const dp = Array(n).fill(Infinity)
+    dp[0] = 0
+    for (let i=0; i < n; i++) {
+        for (let j=i; j <= i + nums[i]; j++) {
+            dp[j] = Math.min(dp[j], 1 + dp[i])
+        }
+    }
+    return dp[n-1]
+};
