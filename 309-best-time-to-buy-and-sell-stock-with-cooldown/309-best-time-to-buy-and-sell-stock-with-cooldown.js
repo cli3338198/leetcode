a7e58f82b4a8871286c1daffa5aa035ea72ef5b2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-08-08 17:18:14 -0700

    Time: 76 ms (85.09%), Space: 42.3 MB (82.24%) - LeetHub

--- 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.js
+++ 309-best-time-to-buy-and-sell-stock-with-cooldown/309-best-time-to-buy-and-sell-stock-with-cooldown.js
@@ -3,26 +3,16 @@
  * @return {number}
  */
 var maxProfit = function(prices) {
-    const dp = {}
-    return helper(0, true)
-    
-    function helper(idx, buying) {
-        // buy -> idx + 1
-        // sell -> idx + 2
-        if (idx >= prices.length) {
-            return 0
-        }
-        if ([idx, buying] in dp) {
-            return dp[[idx, buying]]
-        }
-        const cooldown = helper(idx+1, buying)
-        if (buying) {
-            const buy = helper(idx+1, !buying) - prices[idx]
-            dp[[idx, buying]] = Math.max(buy, cooldown)
-        } else {
-            const sell = helper(idx+2, !buying) + prices[idx]
-            dp[[idx, buying]] = Math.max(sell, cooldown)
-        }
-        return dp[[idx, buying]]
+    let hold = -Infinity
+    let sold = 0
+    let rest = 0
+    for (let i=0; i < prices.length; i++) {
+        const nextHold = Math.max(hold, rest - prices[i])
+        const nextSold = hold + prices[i]
+        const nextRest = Math.max(rest, sold)
+        hold = nextHold
+        sold = nextSold
+        rest = nextRest
     }
+    return Math.max(sold, rest)
 };
