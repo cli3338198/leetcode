3464b04b888457698f21c0f20725910ce5e07508
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-09-06 07:42:47 -0700

    Time: 90 ms (89.05%), Space: 44.3 MB (60.95%) - LeetHub

---
+++ 188-best-time-to-buy-and-sell-stock-iv/188-best-time-to-buy-and-sell-stock-iv.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number} k
+ * @param {number[]} prices
+ * @return {number}
+ */
+var maxProfit = function(k, prices) {
+    const n = prices.length
+    if (!n) return 0
+    const dp = Array(k+1).fill(0).map(() => Array(n).fill(0))
+    for (let i=1; i <= k; i++) {
+        let temp = -Infinity
+        for (let j=1; j < n; j++) {
+            temp = Math.max(temp, dp[i-1][j-1] - prices[j-1])
+            dp[i][j] = Math.max(dp[i][j-1], prices[j] + temp)
+        }
+    }
+    return dp[k][n-1]
+};
