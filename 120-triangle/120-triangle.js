0026b1a8f13c354ebc8be8ed1e5ead956a9e009d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-09-06 07:20:45 -0700

    Time: 91 ms (64.68%), Space: 43.1 MB (40.08%) - LeetHub

--- 120-triangle/120-triangle.js
+++ 120-triangle/120-triangle.js
@@ -3,11 +3,10 @@
  * @return {number}
  */
 var minimumTotal = function(triangle) {
-    const rows = triangle.length
-    for (let i=rows-2; i >= 0; i--) {
+    for (let i=triangle.length-2; i >= 0; i--) {
         for (let j=0; j < triangle[i].length; j++) {
             triangle[i][j] += Math.min(triangle[i+1][j], triangle[i+1][j+1])
         }
     }
-    return triangle[0][0]
+    return triangle[0]
 };
