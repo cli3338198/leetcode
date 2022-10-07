64120945b716c5fd57bb4ad6022439eed49c3666
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-10-06 19:33:04 -0700

    Time: 2413 ms (5.00%), Space: 69.2 MB (5.00%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.js
+++ 200-number-of-islands/200-number-of-islands.js
@@ -3,25 +3,51 @@
  * @return {number}
  */
 var numIslands = function(grid) {
-    const rows = grid.length
-    const cols = grid[0].length
-    let res = 0
-    for (let r=0; r < rows; r++) {
-        for (let c=0; c < cols; c++) {
-            if (grid[r][c] === '1') {
-                res++
-                helper(r, c)
+    class UF {
+        constructor() {
+            this.root = {}
+            this.islands = 0
+        }
+        insert(a) {
+            if (!(a in this.root)) {
+                this.root[a] = a
+                this.islands++
+            }
+        }
+        find(a) {
+            this.insert(a)
+            if (this.root[a] !== a) {
+                this.root[a] = this.find(this.root[a])
+            }
+            return this.root[a]
+        }
+        union(a, b) {
+            const A = this.find(a)
+            const B = this.find(b)
+            if (A !== B) {
+                this.root[B] = A
+                this.islands--
             }
         }
     }
-    return res
     
-    function helper(r, c) {
-        if (!(r in grid) || !(c in grid[r]) || grid[r][c] !== '1') return
-        grid[r][c] = '0'
-        helper(r + 1, c)
-        helper(r - 1, c)
-        helper(r, c + 1)
-        helper(r, c - 1)
+    const uf = new UF()
+    const rows = grid.length
+    const cols = grid[0].length
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < cols; c++) {
+            if (grid[r][c] === '1') {
+                uf.insert([r, c])
+                for (let [r1, c1] of [[0, 1], [1, 0], [0, -1], [-1, 0]]) {
+                    const rr = r + r1
+                    const cc = c + c1
+                    if (rr in grid && cc in grid[rr] && grid[rr][cc] === '1') {
+                        uf.insert([rr, cc])
+                        uf.union([r, c], [rr, cc])
+                    }
+                }
+            }
+        }
     }
+    return uf.islands
 };
