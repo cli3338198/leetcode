94078bc3b943ea1d318a96060500e57ef03caa7a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-10-06 20:12:35 -0700

    Time: 107 ms (67.73%), Space: 43.9 MB (45.53%) - LeetHub

--- 205-isomorphic-strings/205-isomorphic-strings.js
+++ 205-isomorphic-strings/205-isomorphic-strings.js
@@ -4,16 +4,13 @@
  * @return {boolean}
  */
 var isIsomorphic = function(s, t) {
-    let map = {}
+    const map1 = {}
+    const map2 = {}
     for (let i=0; i < s.length; i++) {
-        if (s[i] in map && map[s[i]] !== t[i]) return false
-        map[s[i]] = t[i]
-    }
-    [s, t] = [t, s]
-    map = {}
-    for (let i=0; i < s.length; i++) {
-        if (s[i] in map && map[s[i]] !== t[i]) return false
-        map[s[i]] = t[i]
+        if (s[i] in map1 && map1[s[i]] !== t[i]) return false
+        map1[s[i]] = t[i]
+        if (t[i] in map2 && map2[t[i]] !== s[i]) return false
+        map2[t[i]] = s[i]
     }
     return true
 };
