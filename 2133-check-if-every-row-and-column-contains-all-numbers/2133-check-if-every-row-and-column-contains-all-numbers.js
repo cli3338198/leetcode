37ca7919e524f7f7c638702042363f36a05e4920
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-10-12 21:07:21 -0700

    Time: 213 ms (33.18%), Space: 50.1 MB (85.20%) - LeetHub

--- 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.js
+++ 2133-check-if-every-row-and-column-contains-all-numbers/2133-check-if-every-row-and-column-contains-all-numbers.js
@@ -4,27 +4,23 @@
  */
 var checkValid = function(matrix) {
     const n = matrix.length
-    const map = {}
     for (let r=0; r < n; r++) {
-        for (let c= 0; c < n; c++) {
-            const row = `r${r}`
-            const col = `c${c}`
-            map[row] = map[row] || new Set()
-            map[col] = map[col] || new Set()
-            if (map[row].has(matrix[r][c])) return false
-            map[row].add(matrix[r][c])
-            if (map[col].has(matrix[r][c])) return false
-            map[col].add(matrix[r][c])
+        const set = new Set()
+        for (let c=0; c < n; c++) {
+            const val = matrix[r][c]
+            if (set.has(val)) return false
+            set.add(val)
         }
     }
-    return Object.values(map).every(isValidSet)
-    
-    function isValidSet(set) {
-        for (let i=1; i <= n; i++) {
-            if (!set.has(i)) return false
+    for (let c=0; c < n; c++) {
+        const set = new Set()
+        for (let r=0; r < n; r++) {
+            const val = matrix[r][c]
+            if (set.has(val)) return false
+            set.add(val)
         }
-        return true
     }
+    return true
 };
 
 
