df2ad4ee0045e212f58a03f586486f46dd4ad9fa
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-10-14 19:42:40 -0700

    Time: 158 ms (10.94%), Space: 48.2 MB (31.25%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.js
+++ 0799-champagne-tower/0799-champagne-tower.js
@@ -5,17 +5,14 @@
  * @return {number}
  */
 var champagneTower = function(poured, query_row, query_glass) {
-  const dp = [];
-  for (let i = 0; i < 101; i++) {
-    dp.push(Array(101).fill(0));
-  }
-  dp[0][0] = poured;
+  const dp = Array(101).fill(0).map(() => Array(101).fill(0))
+  dp[0][0] = poured
   for (let i = 0; i < query_row; i++) {
     for (let j = 0; j <= query_glass; j++) {
-      const remain = Math.max(dp[i][j] - 1, 0);
-      dp[i + 1][j] += remain / 2;
-      dp[i + 1][j + 1] += remain / 2;
+      const remain = Math.max(dp[i][j] - 1, 0)
+      dp[i + 1][j] += remain / 2
+      dp[i + 1][j + 1] += remain / 2
     }
   }
-  return Math.min(dp[query_row][query_glass], 1);
+  return Math.min(dp[query_row][query_glass], 1)
 };
