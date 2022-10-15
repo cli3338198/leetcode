4bd1a9febc2d7de861c5502f57a841ef7ff9dda2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-10-14 20:02:03 -0700

    Time: 6089 ms (6.25%), Space: 72.9 MB (25.00%) - LeetHub

---
+++ 1463-cherry-pickup-ii/1463-cherry-pickup-ii.js
@@ -0,0 +1,35 @@
+/**
+ * @param {number[][]} grid
+ * @return {number}
+ */
+var cherryPickup = function(grid) {
+    const dp = {}
+    const rows = grid.length
+    const cols = grid[0].length
+    return helper(0, 0, cols-1)
+    
+    function helper(r, c1, c2) {
+        if ([r, c1, c2] in dp) {
+            return dp[[r, c1, c2]]
+        }
+        if (r >= rows) {
+            return 0
+        }
+        if (c1 < 0 || c1 >= cols || c2 < 0 || c2 >= cols) {
+            return -Infinity
+        }
+        let cherries = 0
+        if (c1 === c2) {
+            cherries = grid[r][c1]
+        } else {
+            cherries = grid[r][c1] + grid[r][c2]
+        }
+        let nextCherries = -Infinity
+        for (const cc of [-1, 0, 1]) {
+            for (const ccc of [-1, 0, 1]) {
+                nextCherries = Math.max(nextCherries, helper(r+1, c1+cc, c2+ccc))
+            }
+        }
+        return dp[[r, c1, c2]] = nextCherries + cherries
+    }
+};
