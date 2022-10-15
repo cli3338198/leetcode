9a65db08a36bc133d82a6b7713cafe62130092e5
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-10-14 19:12:28 -0700

    Time: 105 ms (74.55%), Space: 45.6 MB (58.18%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.js
+++ 0740-delete-and-earn/0740-delete-and-earn.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var deleteAndEarn = function(nums) {
-    const counts = count(nums)
+    const count = counter(nums)
     nums = Array.from(new Set(nums)).sort((a, b) => a - b)
     let earn1 = 0
     let earn2 = 0
     for (let i=0; i < nums.length; i++) {
-        const curEarn = nums[i] * counts[nums[i]]
+        const curEarn = nums[i] * count[nums[i]]
         if (i > 0 && nums[i]-1 === nums[i-1]) {
             const temp = earn2
-            earn2 = Math.max(curEarn + earn1, earn2)
+            earn2 = Math.max(earn2, earn1 + curEarn)
             earn1 = temp
         } else {
             const temp = earn2
@@ -22,7 +22,7 @@ var deleteAndEarn = function(nums) {
     return earn2
 };
 
-function count(nums) {
+function counter(nums) {
     const map = {}
     for (const num of nums) {
         map[num] = map[num] + 1 || 1
