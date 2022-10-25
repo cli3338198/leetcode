549b9fbdfb2fbc1b0c64f04c343497ba06ed866f
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-10-24 22:06:21 -0700

    Time: 932 ms (5.15%), Space: 185.3 MB (5.15%) - LeetHub

--- 0877-stone-game/0877-stone-game.js
+++ 0877-stone-game/0877-stone-game.js
@@ -11,8 +11,15 @@ var stoneGame = function(piles) {
     for (let len=2; len <= n; len++) {
         for (let i=0; i < n - len + 1; i++) {
             const j = i + len - 1
-            dp[i][j] = [Math.max(dp[i][j-1][0], dp[i+1][j][0]), Math.min(dp[i][j-1][1], dp[i+1][j][1])]
+            dp[i][j] = dp[i][j] || []
+            if (piles[i] + dp[i+1][j][1] > piles[j] + dp[i][j-1][1]) {
+                dp[i][j][0] = piles[i] + dp[i+1][j][1]
+                dp[i][j][1] = dp[i+1][j][0]
+            } else {
+                dp[i][j][0] = piles[j] + dp[i][j-1][1]
+                dp[i][j][1] = dp[i][j-1][0]
+            }
         }
     }
-    return !!dp[0][n-1][0]
+    return dp[0][n-1][0]
 };
