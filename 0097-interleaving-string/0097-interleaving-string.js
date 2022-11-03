8266c8e0632f62ad74f42f7095a65f3fc26827c6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-02 20:49:34 -0700

    Time: 307 ms (5.09%), Space: 51.1 MB (6.36%) - LeetHub

---
+++ 0097-interleaving-string/0097-interleaving-string.js
@@ -0,0 +1,31 @@
+/**
+ * @param {string} s1
+ * @param {string} s2
+ * @param {string} s3
+ * @return {boolean}
+ */
+var isInterleave = function(s1, s2, s3) {
+    const dp = {}
+    return helper(0, 0)
+    
+    function helper(i, j) {
+        const key = `${i} ${j}`
+        console.log(key)
+        if (key in dp) {
+            return dp[key]
+        }
+        if (i === s1.length && j === s2.length) {
+            return i + j === s3.length
+        }
+        console.log(1, key)
+        let res = false
+        const k = i + j
+        if (i < s1.length && s1[i] === s3[k]) {
+            res = res || helper(i+1, j)
+        }
+        if (j < s2.length && s2[j] === s3[k]) {
+            res = res || helper(i, j+1)
+        }
+        return dp[key] = res
+    }
+};
