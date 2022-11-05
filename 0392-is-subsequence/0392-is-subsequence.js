b1844ab9d363fefad0ee2ea6f257d730b8eb4c2e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-04 18:54:32 -0700

    Time: 68 ms (89.90%), Space: 41.6 MB (93.54%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.js
+++ 0392-is-subsequence/0392-is-subsequence.js
@@ -6,16 +6,13 @@
 var isSubsequence = function(s, t) {
     const m = s.length
     const n = t.length
-    const dp = Array(m+1).fill(0).map(() => Array(n+1).fill(false))
-    dp[0].fill(true)
-    for (let i=1; i <= m; i++) {
-        for (let j=1; j <= n; j++) {
-            if (s[i-1] === t[j-1]) {
-                dp[i][j] = dp[i-1][j-1]
-            } else {
-                dp[i][j] = dp[i][j-1]
-            }
-        }
+    let a = 0
+    let b = 0
+    while (a < m && b < n) {
+        if (s[a] === t[b]) {
+            a++
+        } 
+        b++
     }
-    return dp[m][n]
+    return a === m
 };
