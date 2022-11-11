9e15afb13c1f6564e947ccf6154a39ee41787365
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-10 20:38:14 -0800

    Time: 380 ms (43.75%), Space: 76.6 MB (41.67%) - LeetHub

---
+++ 0354-russian-doll-envelopes/0354-russian-doll-envelopes.js
@@ -0,0 +1,33 @@
+/**
+ * @param {number[][]} envelopes
+ * @return {number}
+ */
+var maxEnvelopes = function(envelopes) {
+    envelopes.sort(([a, c], [b, d]) => a === b ? d - c : a - b)
+    const dp = []
+    for (const e of envelopes) {
+        const idx = binarySearch(dp, e)
+        if (idx === -1) {
+            dp.push(e)
+        } else {
+            dp[idx] = e
+        }
+    }
+    return dp.length
+    
+    function binarySearch(dp, e) {
+        let l = 0
+        let r = dp.length-1
+        let res = -1
+        while (l <= r) {
+            const m = Math.floor((l+r)/2)
+            if (dp[m][1] >= e[1]) {
+                res = m
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+        return res
+    }
+};
