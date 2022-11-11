46779f916035d64bb802341e6db2b9d1c609dd21
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-11 08:54:27 -0800

    Time: 91 ms (78.40%), Space: 44.9 MB (21.22%) - LeetHub

--- 0605-can-place-flowers/0605-can-place-flowers.js
+++ 0605-can-place-flowers/0605-can-place-flowers.js
@@ -6,24 +6,13 @@
 var canPlaceFlowers = function(flowerbed, n) {
     if (n === 0) return true
     let arr = flowerbed.slice()
-    let total = n
     for (let i=0; i < flowerbed.length; i++) {
         if (arr[i] === 0 && canPlace(arr, i)) {
             arr[i] = 1
-            total--
+            n--
         }
-        if (total === 0) return true
     }
-    arr = flowerbed.slice()
-    total = n
-    for (let i=flowerbed.length-1; i >= 0; i--) {
-        if (arr[i] === 0 && canPlace(arr, i)) {
-            arr[i] = 1
-            total--
-        }
-        if (total === 0) return true
-    }
-    return false
+    return n <= 0
     
     function canPlace(arr, i) {
         return ((arr[i-1] === undefined || arr[i-1] === 0) && (arr[i+1] === undefined || arr[i+1] === 0))
