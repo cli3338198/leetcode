eafc0739476429c32d7b9a3d110fd2cf71871fa3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-15 08:12:16 -0800

    Time: 94 ms (52.76%), Space: 42.5 MB (19.77%) - LeetHub

--- 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
+++ 2160-minimum-sum-of-four-digit-number-after-splitting-digits/2160-minimum-sum-of-four-digit-number-after-splitting-digits.js
@@ -3,7 +3,6 @@
  * @return {number}
  */
 var minimumSum = function(num) {
-    num = String(num).split("").map(Number).sort((a, b) => a - b)
-    const [a, c, b, d] = num
+    const [a, c, b, d] = String(num).split("").map(Number).sort((a, b) => a - b)
     return Number("" + a + b) + Number("" + c + d)
 };
