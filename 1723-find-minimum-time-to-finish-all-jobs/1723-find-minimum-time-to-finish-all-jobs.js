cd9675257ec977cccce0cd7bc955309c3d1d30a8
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-14 20:31:01 -0800

    Time: 9187 ms (14.29%), Space: 42.3 MB (100.00%) - LeetHub

--- 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.js
+++ 1723-find-minimum-time-to-finish-all-jobs/1723-find-minimum-time-to-finish-all-jobs.js
@@ -6,22 +6,28 @@
 var minimumTimeRequired = function(jobs, k) {
     const n = jobs.length
     jobs.sort((a, b) => b - a)
-    let res = jobs.reduce((acc, val) => acc + val, 0)
-    const workers = Array(k).fill(0)
-    dfs(0)
-    return res
+    let l = Math.max(...jobs)
+    let r = jobs.reduce((acc, val) => acc + val, 0)
+    while (l < r) {
+        const m = Math.floor((l+r)/2)
+        const cap = Array(k).fill(m)
+        if (dfs(cap, 0)) {
+            r = m
+        } else {
+            l = m + 1
+        }
+    }
+    return l
     
-    function dfs(i) {
-        if (i === n) {
-            return res = Math.min(res, Math.max(...workers))
-        }
+    function dfs(cap, i) {
+        if (i === n) return true
         for (let j=0; j < k; j++) {
-            if (workers[j] + jobs[i] < res) {
-                workers[j] += jobs[i]
-                dfs(i+1)
-                workers[j] -= jobs[i]
+            if (jobs[i] <= cap[j]) {
+                cap[j] -= jobs[i]
+                if (dfs(cap, i+1)) return true
+                cap[j] += jobs[i]
             }
-            if (workers[j] === 0) break
         }
+        return false
     }
 };
