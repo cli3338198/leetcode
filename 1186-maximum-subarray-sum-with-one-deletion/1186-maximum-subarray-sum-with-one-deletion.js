b463b1640bf16f1bfa3a8f3764424104afcdd82b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-14 22:18:50 -0800

    Time: 124 ms (42.86%), Space: 47.4 MB (78.57%) - LeetHub

---
+++ 1186-maximum-subarray-sum-with-one-deletion/1186-maximum-subarray-sum-with-one-deletion.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number[]} arr
+ * @return {number}
+ */
+var maximumSum = function(arr) {
+    const n = arr.length
+    if (n === 1) return arr[0]
+    let res = -Infinity
+    for (const n of arr) {
+        res = Math.max(res, n)
+    }
+    if (res < 0) return res
+    res = 0
+    let oneDelete = 0
+    let noDelete = arr[0]
+    for (let i=1; i < arr.length; i++) {
+        oneDelete = Math.max(oneDelete + arr[i], noDelete)
+        noDelete = Math.max(noDelete + arr[i], arr[i])
+        res = Math.max(res, oneDelete, noDelete)
+    }
+    return res
+};
