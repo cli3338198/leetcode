ac438df3cc5a5ea56de53d447ff26785c6c1e5f2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-14 20:54:29 -0800

    Time: 716 ms (5.04%), Space: 137.9 MB (5.04%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} stones
+ * @return {boolean}
+ */
+var canCross = function(stones) {
+    const n = stones.length
+    const dp = Array(n).fill(0).map(() => Array(n).fill(false))
+    dp[0][0] = true
+    for (let i=1; i < n; i++) {
+        for (let j=i-1; j >= 0; j--) {
+            const jump = stones[i] - stones[j]
+            if (jump > j + 1) break
+            dp[i][jump] = dp[j][jump-1] || dp[j][jump] || dp[j][jump+1]
+            if (i === n-1 && dp[i][jump] === true) return true
+        }
+    }
+    return false
+};
