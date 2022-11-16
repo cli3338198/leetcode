f02bd040dc10259cd002596a2503561ad3aad5c8
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-15 21:42:35 -0800

    Time: 328 ms (89.48%), Space: 66.6 MB (51.19%) - LeetHub

--- 0912-sort-an-array/0912-sort-an-array.js
+++ 0912-sort-an-array/0912-sort-an-array.js
@@ -2,26 +2,61 @@
  * @param {number[]} nums
  * @return {number[]}
  */
-var sortArray = function(nums) {
-    return mergeSort(nums)
-    
-    function mergeSort(nums) {
-        if (nums.length < 2) return nums
-        const mid = Math.floor(nums.length/2)
-        const left = mergeSort(nums.slice(0, mid))
-        const right = mergeSort(nums.slice(mid))
-        return merge(left, right)
+class Heap {
+    constructor(compare) {
+        this.compare = compare
+        this.h = []
     }
-    
-    function merge(left, right) {
-        const res = []
-        while (left.length && right.length) {
-            if (left[0] < right[0]) {
-                res.push(left.shift())
-            } else {
-                res.push(right.shift())
+    push(a) {
+        this.h.push(a)
+        this.bubbleUp(this.h.length-1)
+    }
+    bubbleUp(c) {
+        let p = Math.ceil(c / 2) - 1
+        while (p in this.h && this.compare(this.h[p], this.h[c])) {
+            this.swap(p, c)
+            c = p
+            p = Math.ceil(c / 2) - 1
+        }
+    }
+    swap(a, b) {
+        [this.h[a], this.h[b]] = [this.h[b], this.h[a]]
+    }
+    pop() {
+        if (this.h.length > 1) {
+            const temp = this.h[0]
+            this.h[0] = this.h.pop()
+            this.heapify(0)
+            return temp
+        } else {
+            return this.h.pop()
+        }
+    }
+    heapify(p) {
+        let l = 2 * p + 1
+        let r = 2 * p + 2
+        while ((l in this.h && this.compare(this.h[p], this.h[l])) || (r in this.h && this.compare(this.h[p], this.h[r]))) {
+            let t = p
+            if (l in this.h && this.compare(this.h[t], this.h[l])) t = l
+            if (r in this.h && this.compare(this.h[t], this.h[r])) t = r
+            if (t !== p) {
+                this.swap(t, p)
+                p = t
+                l = 2 * p + 1
+                r = 2 * p + 2
             }
         }
-        return [...res, ...left, ...right]
     }
+}
+
+var sortArray = function(nums) {
+    const pq = new Heap((a, b) => a > b)
+    for (const n of nums) {
+        pq.push(n)
+    }
+    const res = []
+    while (pq.h.length) {
+        res.push(pq.pop())
+    }
+    return res
 };
