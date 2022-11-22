d0bc643a606031b3ae684ea391ab1bd479c32b69
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-22 08:45:11 -0800

    Time: 112 ms (65.83%), Space: 61.1 MB (12.09%) - LeetHub

---
+++ 0334-increasing-triplet-subsequence/0334-increasing-triplet-subsequence.js
@@ -0,0 +1,16 @@
+/**
+ * @param {number[]} nums
+ * @return {boolean}
+ */
+var increasingTriplet = function(nums) {
+    return nums.reduce((acc, val, k, arr) => {
+        if (val <= acc[0]) {
+            acc[0] = val
+        } else if (val <= acc[1]) {
+            acc[1] = val
+        } else {
+            return [...acc, val]
+        }
+        return acc
+    }, [Infinity, Infinity]).length >= 3
+};
