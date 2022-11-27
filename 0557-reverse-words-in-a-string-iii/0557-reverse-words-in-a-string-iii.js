35d202522597c0a94110a79a2a8135f9a47dc034
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-27 10:58:06 -0800

    Time: 110 ms (72.07%), Space: 48.4 MB (55.33%) - LeetHub

--- 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
+++ 0557-reverse-words-in-a-string-iii/0557-reverse-words-in-a-string-iii.js
@@ -3,10 +3,7 @@
  * @return {string}
  */
 var reverseWords = function(s) {
-    return s.split(" ").map(reverse).join(" ")
-    
-    function reverse(str) {
-        str = str.split("")
-        return str.reverse().join("")
-    }
+    return s.split(" ")
+        .map(word => word.split("").reverse().join(""))
+        .join(" ")
 };
