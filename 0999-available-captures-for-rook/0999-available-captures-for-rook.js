9841d0854c0d61024e65cd610abacff3e7926351
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-27 09:53:20 -0800

    Time: 69 ms (79.38%), Space: 42 MB (23.71%) - LeetHub

---
+++ 0999-available-captures-for-rook/0999-available-captures-for-rook.js
@@ -0,0 +1,31 @@
+/**
+ * @param {character[][]} board
+ * @return {number}
+ */
+var numRookCaptures = function(board) {
+    const rows = board.length
+    const cols = board[0].length
+    let r0
+    let c0
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < cols; c++) {
+            if (board[r][c] === 'R') {
+                r0 = r
+                c0 = c
+            }
+        }
+    }
+    let res = 0
+    for (const [r, c] of [[0, 1], [1, 0], [-1, 0], [0, -1]]) {
+        for (let rr = r0 + r, cc = c0 + c; rr < rows && rr >= 0 && cc < cols && cc >= 0; rr += r, cc += c) {
+            if (board[rr][cc] === 'p') {
+                res++
+                break
+            }
+            if (board[rr][cc] !== '.') {
+                break
+            }
+        }
+    }
+    return res
+};
