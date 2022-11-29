fb9a810ade6a453d6621ed607fe5057445d11d97
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-11-29 08:22:40 -0800

    Time: 173 ms (45.28%), Space: 47.5 MB (90.41%) - LeetHub

---
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @param {number} x
+ * @return {number[]}
+ */
+var findClosestElements = function(arr, k, x) {
+    let l = 0, r = arr.length - k
+    while (l < r) {
+        const m = Math.floor((l + r) / 2)
+        if (x - arr[m] > arr[m + k] - x) {
+            l = m + 1
+        } else {
+            r = m
+        }
+    }
+    return arr.slice(l, l + k)
+};
