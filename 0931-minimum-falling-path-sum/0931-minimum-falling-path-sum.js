ec8894ef42bbab700e522928fcccd4c7bfa55752
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-06 21:47:55 -0800

    Time: 117 ms (47.74%), Space: 42.3 MB (95.48%) - LeetHub

--- 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
+++ 0931-minimum-falling-path-sum/0931-minimum-falling-path-sum.js
@@ -5,16 +5,14 @@
 var minFallingPathSum = function(matrix) {
     const rows = matrix.length
     const cols = matrix[0].length
-    const dp = Array(rows).fill(0).map(() => Array(cols).fill(101))
-    dp[0] = matrix[0].slice()
     for (let r=1; r < rows; r++) {
         for (let c=0; c < cols; c++) {
-            dp[r][c] = matrix[r][c] + Math.min(
-                dp[r-1][c-1] || Infinity,
-                dp[r-1][c],
-                dp[r-1][c+1] || Infinity
+            matrix[r][c] = matrix[r][c] + Math.min(
+                matrix[r-1][c-1] || Infinity,
+                matrix[r-1][c],
+                matrix[r-1][c+1] || Infinity
             )
         }
     }
-    return Math.min(...dp[rows-1])
+    return Math.min(...matrix[rows-1])
 };
