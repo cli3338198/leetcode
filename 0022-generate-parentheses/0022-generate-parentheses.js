e801a276404022167994a7d5c6916da30d4f9f83
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-19 21:21:28 -0800

    Time: 112 ms (24.28%), Space: 43.9 MB (28.69%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -3,20 +3,18 @@
  * @return {string[]}
  */
 var generateParenthesis = function(n) {
-    const res = []
-    if (n < 1) return res
-    helper(0, 0, '')
-    return res
-    
-    function helper(open, close, str) {
-        if (open === n && close === n) {
-            return res.push(str)
-        }
-        if (open < n) {
-            helper(open + 1, close, str + "(")
-        }
-        if (close < open) {
-            helper(open, close + 1, str + ")")
+    const dp = Array(n+2).fill(0).map(() => Array(n+1).fill(0).map(() => ([])))
+    dp[1][1].push("(")
+    for (let open=1; open < n + 2; open++) {
+        for (let close=1; close < n + 1; close++) {
+            if (open - 1 > close) continue
+            for (const k of dp[open-1][close]) {
+                dp[open][close].push(k + ")")
+            }
+            for (const k of dp[open][close-1]) {
+                dp[open][close].push(k + "(")
+            }
         }
     }
+    return dp[n+1][n]
 };
