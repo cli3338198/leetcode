b41cfc96e5039f77507974b3e0d0026f999722f2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-23 10:38:31 -0800

    Time: 84 ms (92.56%), Space: 46.7 MB (97.67%) - LeetHub

--- 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.js
+++ 0095-unique-binary-search-trees-ii/0095-unique-binary-search-trees-ii.js
@@ -12,24 +12,23 @@
  */
 var generateTrees = function(n) {
     const dp = {}
-    const res = []
-    if (n < 1) return res
     return helper(1, n)
     
-    function helper(lo, hi) {
-        if (lo > hi) return [null]
-        if ([lo, hi] in dp) return dp[[lo, hi]]
+    function helper(l, r) {
+        const key = `${l} ${r}`
+        if (key in dp) return dp[key]
+        if (l > r) return [null]
         const res = []
-        for (let root=lo; root <= hi; root++) {
-            for (const left of helper(lo, root-1)) {
-                for (const right of helper(root+1, hi)) {
+        for (let root=l; root <= r; root++) {
+            for (const left of helper(l, root-1)) {
+                for (const right of helper(root+1, r)) {
                     const tree = new TreeNode(root)
                     tree.left = left
                     tree.right = right
-                    res.push(tree)                    
+                    res.push(tree)
                 }
             }
         }
-        return dp[[lo, hi]] = res
+        return dp[key] = res
     }
 };
