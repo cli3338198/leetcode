0854393e70accf0797f0c4ac17ed16f7b2898486
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-23 11:15:50 -0800

    Time: 133 ms (67.33%), Space: 74.6 MB (26.74%) - LeetHub

--- 0115-distinct-subsequences/0115-distinct-subsequences.js
+++ 0115-distinct-subsequences/0115-distinct-subsequences.js
@@ -10,11 +10,11 @@ var numDistinct = function(s, t) {
     for (const row of dp) {
         row[0] = 1
     }
-    for (let i=1; i <= m; i++) {
-        for (let j=1; j <= n; j++) {
-            dp[i][j] = dp[i-1][j]
-            if (s[i-1] === t[j-1]) {
-                dp[i][j] += dp[i-1][j-1]
+    for (let r=1; r <= m; r++) {
+        for (let c=1; c <= n; c++) {
+            dp[r][c] = dp[r-1][c]
+            if (s[r-1] === t[c-1]) {
+                dp[r][c] += dp[r-1][c-1]
             }
         }
     }
