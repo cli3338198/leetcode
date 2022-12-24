bdd5cf0456ac64b6c770d6fc22cd8c87ebacc8f3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-23 22:02:25 -0800

    Time: 78 ms (87.69%), Space: 44.9 MB (44.62%) - LeetHub

---
+++ 0120-triangle/0120-triangle.ts
@@ -0,0 +1,8 @@
+function minimumTotal(triangle: number[][]): number {
+    for (let r=triangle.length-2; r >= 0; r--) {
+        for (let c=0; c < triangle[r].length; c++) {
+            triangle[r][c] += Math.min(triangle[r+1][c], triangle[r+1][c+1])
+        }
+    }
+    return triangle[0][0]
+};
