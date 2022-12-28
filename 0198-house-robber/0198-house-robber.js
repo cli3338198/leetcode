c29c77f0c04e1c160e9dc9670505d145706e907d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-27 20:11:30 -0800

    Time: 74 ms (48.21%), Space: 42.3 MB (20.18%) - LeetHub

---
+++ 0198-house-robber/0198-house-robber.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var rob = function(nums) {
+    const n = nums.length
+    const dp = Array(n).fill(0)
+    dp[0] = nums[0]
+    for (let i=1; i < n; i++) {
+        dp[i] = Math.max(dp[i-1], nums[i] + (dp[i-2] || 0))
+    }
+    return dp[n-1]
+};
