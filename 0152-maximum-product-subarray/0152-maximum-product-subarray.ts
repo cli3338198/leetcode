9b01bc6d353f57c1874370593874320e5b336ddf
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-27 19:43:41 -0800

    Time: 72 ms (90.63%), Space: 44.8 MB (70.83%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.ts
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.ts
@@ -1,9 +1,12 @@
-function maxProduct(prefix: number[]): number {
-    const n = prefix.length
-    const suffix = prefix.slice().reverse()
-    for (let i=1; i < n; i++) {
-        prefix[i] *= prefix[i-1] || 1
-        suffix[i] *= suffix[i-1] || 1
+function maxProduct(nums: number[]): number {
+    const n = nums.length
+    let prefix = 0
+    let suffix = 0
+    let res = -Infinity
+    for (let i=0; i < n; i++) {
+        prefix = (prefix || 1) * nums[i]
+        suffix = (suffix || 1) * nums[n-i-1]
+        res = Math.max(res, prefix, suffix)
     }
-    return Math.max(...prefix, ...suffix)
+    return res
 };
