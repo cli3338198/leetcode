2da61132cfc15aa6e91662242f2fb1e98414bd14
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-29 11:03:32 -0800

    Time: 67 ms (91.59%), Space: 43.5 MB (44.55%) - LeetHub

--- 0377-combination-sum-iv/0377-combination-sum-iv.js
+++ 0377-combination-sum-iv/0377-combination-sum-iv.js
@@ -4,15 +4,14 @@
  * @return {number}
  */
 var combinationSum4 = function(nums, target) {
-    const n = nums.length
     const dp = Array(target+1).fill(0)
     dp[0] = 1
     for (let i=1; i <= target; i++) {
         for (const n of nums) {
             if (n <= i) {
-                dp[i] += dp[i-n]
+                dp[i] += dp[i - n]
             }
         }
-    }    
+    }
     return dp[target]
 };
