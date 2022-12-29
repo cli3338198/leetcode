02a968d5c78a6f03e61bf4ed050ef3b4379bc438
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-28 19:36:37 -0800

    Time: 127 ms (83.86%), Space: 50.6 MB (44.94%) - LeetHub

--- 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.js
+++ 0329-longest-increasing-path-in-a-matrix/0329-longest-increasing-path-in-a-matrix.js
@@ -2,9 +2,9 @@
  * @param {number[][]} matrix
  * @return {number}
  */
-var longestIncreasingPath = function(matrix) {
-    const rows = matrix.length
-    const cols = matrix[0].length
+var longestIncreasingPath = function(mat) {
+    const rows = mat.length
+    const cols = mat[0].length
     const dp = Array(rows).fill(0).map(() => Array(cols).fill(0))
     let res = 1
     for (let r=0; r < rows; r++) {
@@ -20,10 +20,10 @@ var longestIncreasingPath = function(matrix) {
             return dp[r][c]
         }
         dp[r][c] = 1
-        for (let [rr, cc] of [[0, 1], [1, 0], [0, -1], [-1, 0]]) {
-            const rrr = r + rr
-            const ccc = c + cc
-            if (rrr in matrix && ccc in matrix[rrr] && matrix[r][c] < matrix[rrr][ccc]) {
+        for (let [rr, cc] of [[1, 0], [0, 1], [-1, 0], [0, -1]]) {
+            const rrr = rr + r
+            const ccc = cc + c
+            if (rrr in mat && ccc in mat[rrr] && mat[r][c] < mat[rrr][ccc]) {
                 dp[r][c] = Math.max(dp[r][c], 1 + helper(rrr, ccc))
             }
         }
