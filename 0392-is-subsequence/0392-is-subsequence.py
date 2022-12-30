178aa29b842a663e5415f14f37616bda0f5f6ddc
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-29 18:40:08 -0800

    Time: 73 ms (14.83%), Space: 15 MB (5.03%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.py
+++ 0392-is-subsequence/0392-is-subsequence.py
@@ -4,13 +4,16 @@ class Solution:
         m = len(s)
         n = len(t)
         
-        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        dp = [[False] * (n + 1) for _ in range(m + 1)]
+        
+        for i in range(n + 1):
+            dp[0][i] = True
         
         for i in range(1, m + 1):
             for j in range(1, n + 1):
                 if s[i-1] == t[j-1]:
-                    dp[i][j] = 1 + dp[i-1][j-1]
+                    dp[i][j] = dp[i-1][j-1]
                 else:
-                    dp[i][j] = max(dp[i-1][j], dp[i][j-1])
+                    dp[i][j] = dp[i][j-1]
         
-        return dp[m][n] == len(s)
+        return dp[m][n]
