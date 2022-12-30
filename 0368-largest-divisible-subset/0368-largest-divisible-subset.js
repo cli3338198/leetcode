a23099d5cd8f3e886bac417075901b9ac94dcba0
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-29 18:14:07 -0800

    Time: 102 ms (92.96%), Space: 42.4 MB (95.77%) - LeetHub

--- 0368-largest-divisible-subset/0368-largest-divisible-subset.js
+++ 0368-largest-divisible-subset/0368-largest-divisible-subset.js
@@ -5,24 +5,23 @@
 var largestDivisibleSubset = function(nums) {
     nums.sort((a, b) => a - b)
     const n = nums.length
-    if (n === 0) return res
-    let max = 1
     const dp = Array(n).fill(1)
+    let len = 1
     for (let i=1; i < n; i++) {
         for (let j=0; j < i; j++) {
             if (nums[i] % nums[j] === 0) {
                 dp[i] = Math.max(dp[i], 1 + dp[j])
             }
         }
-        max = Math.max(max, dp[i])
+        len = Math.max(len, dp[i])
     }
-    let prev = null
     const res = []
-    for (let i=n-1; i >= 0 && max > 0; i--) {
-        if ((prev % nums[i] === 0 || prev === null) && dp[i] === max) {
+    let prev = null
+    for (let i=n-1; i >= 0 && len > 0; i--) {
+        if ((prev === null || prev % nums[i] === 0) && dp[i] === len) {
             res.push(nums[i])
             prev = nums[i]
-            max--
+            len--
         }
     }
     return res.reverse()
