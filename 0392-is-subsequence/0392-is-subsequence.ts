50ae0b5939f71b908c03ed143dcc5ee456bfd19f
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-29 18:43:49 -0800

    Time: 67 ms (91.37%), Space: 44.4 MB (16.07%) - LeetHub

--- 0392-is-subsequence/0392-is-subsequence.ts
+++ 0392-is-subsequence/0392-is-subsequence.ts
@@ -1,11 +1,7 @@
 function isSubsequence(s: string, t: string): boolean {
     let a = 0
-    let b = 0
-    while (b < t.length) {
-        if (s[a] === t[b]) {
-            a++
-        }
-        b++
+    for (const c of t) {
+        if (s[a] === c) a++
     }
     return a === s.length
 };
