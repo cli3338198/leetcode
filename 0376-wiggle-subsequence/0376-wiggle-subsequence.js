703e14cf13526d7f2c2d325d4708754587f14b95
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-30 11:10:14 -0800

    Time: 82 ms (61.24%), Space: 42.5 MB (20.15%) - LeetHub

--- 0376-wiggle-subsequence/0376-wiggle-subsequence.js
+++ 0376-wiggle-subsequence/0376-wiggle-subsequence.js
@@ -3,17 +3,17 @@
  * @return {number}
  */
 var wiggleMaxLength = function(nums) {
-    if (nums.length < 2) {
-        return nums.length
-    }
-    let up = 1
-    let down = 1
-    for (let i=0; i < nums.length-1; i++) {
-        if (nums[i] < nums[i+1]) {
-            up = down + 1
-        } else if (nums[i] > nums[i+1]) {
-            down = up + 1
+    const n = nums.length
+    if (n < 2) return n
+    const dp = Array(n).fill(0).map(() => ([1, 1]))
+    for (let i=1; i < n; i++) {
+        for (let j=0; j < i; j++) {
+            if (nums[j] < nums[i]) {
+                dp[i][0] = Math.max(dp[i][0], 1 + dp[j][1])
+            } else if (nums[j] > nums[i]) {
+                dp[i][1] = Math.max(dp[i][1], 1 + dp[j][0])
+            }
         }
     }
-    return Math.max(up, down)
+    return Math.max(...dp[n-1])
 };
