752586701484c97be9b2716e7dd98c5775a6e663
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2022-12-29 19:19:58 -0800

    Time: 2397 ms (16.37%), Space: 45.4 MB (12.27%) - LeetHub

---
+++ 0403-frog-jump/0403-frog-jump.py
@@ -0,0 +1,26 @@
+class Solution:
+    def canCross(self, stones: List[int]) -> bool:
+        n = len(stones)
+        
+        dp = [[False] * n for _ in range(n)]
+        dp[0][1] = True
+        
+        for i in range(1, n):
+            for j in range(i):
+                jump = stones[i] - stones[j]
+                
+                if jump >= n or dp[j][jump] == False:
+                    continue
+                    
+                dp[i][jump] = True
+                
+                if jump - 1 >= 0:
+                    dp[i][jump - 1] = True
+                
+                if jump + 1 < n:
+                    dp[i][jump + 1] = True
+                    
+                if i == n - 1:
+                    return True
+        
+        return False
