f4dac8751a09ef42df106a6a67d4ae5589933737
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-03 21:17:32 -0800

    Time: 137 ms (66.67%), Space: 53.8 MB (33.33%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.ts
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.ts
@@ -1,23 +1,23 @@
 function minSumOfLengths(arr: number[], target: number): number {
     const n = arr.length
-    const prefixSums: Record<string, number> = {}
-    let sum = 0
-    let leftSize = Infinity
+    const best = Array(n).fill(Infinity)
     let res = Infinity
+    let bestSoFar = Infinity
+    let sum = 0
+    let start = 0
     for (let i=0; i < n; i++) {
         sum += arr[i]
-        prefixSums[sum] = i
-    }
-    prefixSums[0] = -1
-    sum = 0
-    for (let i=0; i < n; i++) {
-        sum += arr[i]
-        if (sum - target in prefixSums) {
-            leftSize = Math.min(leftSize, i - prefixSums[sum - target])
+        while (sum > target) {
+            sum -= arr[start]
+            start++
         }
-        if (sum + target in prefixSums) {
-            res = Math.min(res, prefixSums[sum + target] - i + leftSize)
+        if (sum === target) {
+            if (start > 0 && best[start-1] !== Infinity) {
+                res = Math.min(res, best[start-1] + i - start + 1)
+            }
+            bestSoFar = Math.min(bestSoFar, i - start + 1)
         }
+        best[i] = bestSoFar
     }
     return res === Infinity ? -1 : res
 };
