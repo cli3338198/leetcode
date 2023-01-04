44a14283aa62152cf9fca6bfe433d62acd1a60e6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-03 21:40:15 -0800

    Time: 991 ms (33.33%), Space: 130.8 MB (33.33%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.ts
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.ts
@@ -1,23 +1,30 @@
 function minSumOfLengths(arr: number[], target: number): number {
-    const n = arr.length
-    const best = Array(n).fill(Infinity)
-    let res = Infinity
-    let bestSoFar = Infinity
-    let sum = 0
-    let start = 0
-    for (let i=0; i < n; i++) {
-        sum += arr[i]
-        while (sum > target) {
-            sum -= arr[start]
-            start++
+    return helper(arr, target, 2)
+    
+    function helper(arr: number[], target: number, k: number): number {
+        const n = arr.length
+        const dp: number[][] = Array(n+1).fill(0).map(() => Array(k+1).fill(Infinity))
+        const prefixSums: Record<string, number> = {}
+        prefixSums[0] = 0
+        let sum = 0
+        for (let i=0; i <= n; i++) {
+            dp[i][0] = 0
         }
-        if (sum === target) {
-            if (start > 0 && best[start-1] !== Infinity) {
-                res = Math.min(res, best[start-1] + i - start + 1)
+        for (let i=1; i <= n; i++) {
+            let d = -1
+            sum += arr[i-1]
+            prefixSums[sum] = i
+            if (sum - target in prefixSums) {
+                d = prefixSums[sum - target]
             }
-            bestSoFar = Math.min(bestSoFar, i - start + 1)
+            for (let j=1; j <= k; j++) {
+                dp[i][j] = Math.min(dp[i][j], dp[i-1][j])
+                if (d !== -1) {
+                    dp[i][j] = Math.min(dp[i][j], dp[d][j-1] + i - d)
+                }
+            }
         }
-        best[i] = bestSoFar
+        if (dp[n][k] >= Infinity) return -1
+        return dp[n][k]
     }
-    return res === Infinity ? -1 : res
 };
