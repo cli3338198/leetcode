fbca18ea9e6407adc81114918adce047b9ca5a72
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-03 21:01:36 -0800

    Time: 101 ms (90.00%), Space: 53.9 MB (50.00%) - LeetHub

--- 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.js
+++ 1477-find-two-non-overlapping-sub-arrays-each-with-target-sum/1477-find-two-non-overlapping-sub-arrays-each-with-target-sum.js
@@ -26,3 +26,27 @@ var minSumOfLengths = function(arr, target) {
     }
     return res === Infinity ? -1 : res
 };
+
+minSumOfLengths = function(arr, target) {
+    const n = arr.length
+    const best = Array(n).fill(Infinity)
+    let res = Infinity
+    let bestSoFar = Infinity
+    let sum = 0
+    let start = 0
+    for (let i=0; i < n; i++) {
+        sum += arr[i]
+        while (sum > target) {
+            sum -= arr[start]
+            start++
+        }
+        if (sum === target) {
+            if (start > 0 && best[start - 1] !== Infinity) {
+                res = Math.min(res, best[start - 1] + i - start + 1)
+            }
+            bestSoFar = Math.min(bestSoFar, i - start + 1)
+        }
+        best[i] = bestSoFar
+    }
+    return res === Infinity ? -1 : res
+}
