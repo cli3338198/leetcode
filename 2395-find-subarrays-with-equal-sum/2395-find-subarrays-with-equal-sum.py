f3875f2bd7e6a42f7c07361d366c74b23b8102b9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-03 19:57:46 -0800

    Time: 67 ms (50.21%), Space: 14.1 MB (42.14%) - LeetHub

--- 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.py
+++ 2395-find-subarrays-with-equal-sum/2395-find-subarrays-with-equal-sum.py
@@ -2,19 +2,13 @@ class Solution:
     def findSubarrays(self, nums: List[int]) -> bool:
         
         dp = set()
-        l = 0
-        cur_sum = 0
         
-        for r in range(len(nums)):
-            cur_sum += nums[r]
+        for r in range(len(nums) - 1):
+            cur_sum = nums[r] + nums[r+1]
             
-            if r - l + 1 == 2:
-                if cur_sum in dp:
-                    return True
-                
-                dp.add(cur_sum)
-                cur_sum -= nums[l]
-                l += 1
-                
+            if cur_sum in dp:
+                return True
+            
+            dp.add(cur_sum)
+            
         return False
-            
