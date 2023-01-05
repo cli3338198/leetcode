ba3b37157efcbb9d0ddf2596e9af5e6be89674cf
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-04 19:33:04 -0800

    Time: 437 ms (6.49%), Space: 51 MB (5.41%) - LeetHub

--- 0397-integer-replacement/0397-integer-replacement.js
+++ 0397-integer-replacement/0397-integer-replacement.js
@@ -18,3 +18,31 @@ var integerReplacement = function(n) {
         return dp[n] = res
     }
 };
+
+integerReplacement = function(n) {
+    const dp = Array(n+1).fill(Infinity)
+    dp[0] = 1
+    dp[1] = 0
+    for (let i=2; i <= n; i++) {
+        if (i % 2 === 0) {
+            dp[i] = 1 + dp[i/2]
+        } else {
+            dp[i] = 1 + Math.min(1 + dp[(i+1)/2], dp[i-1])
+        }
+    }
+    return dp[n]
+}
+
+integerReplacement = function(n) {
+    const q = [[n, 0]]
+    while (q.length) {
+        const [n, res] = q.shift()
+        if (n === 1) return res
+        if (n % 2 === 0) {
+            q.push([n/2, res + 1])
+        } else {
+            q.push([n+1, res + 1])
+            q.push([n-1, res + 1])
+        }
+    }
+}
