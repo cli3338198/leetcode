a491630b16f5dab2d112105524b86bb8f79feb5b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-04 19:35:52 -0800

    Time: 41 ms (75.75%), Space: 13.9 MB (70.38%) - LeetHub

---
+++ 0397-integer-replacement/0397-integer-replacement.py
@@ -0,0 +1,11 @@
+class Solution:
+    def integerReplacement(self, n: int) -> int:
+        
+        @lru_cache(None)
+        
+        def helper(n: int) -> int:
+            if n == 1: return 0
+            if n % 2 == 0: return 1 + helper(n // 2)
+            return 1 + min(1 + helper((n+1) // 2), helper(n-1))
+        
+        return helper(n)
