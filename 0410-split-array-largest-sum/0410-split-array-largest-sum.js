22d7cd92c541809da1c9b46e37bee5994821cfc6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-05 19:30:03 -0800

    Time: 2914 ms (5.56%), Space: 57.7 MB (5.56%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.js
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.js
@@ -5,11 +5,11 @@
  */
 var splitArray = function(nums, k) {
     const n = nums.length
+    const dp = {}
     const suffixSums = Array(n).fill(0)
     for (let i=n-1; i >= 0; i--) {
-        suffixSums[i] = (suffixSums[i+1] || 0) + nums[i]
+        suffixSums[i] = (suffixSums[i+1] || 0) + nums[i] 
     }
-    const dp = {}
     return helper(0, k)
     
     function helper(i, k) {
@@ -18,7 +18,7 @@ var splitArray = function(nums, k) {
         if (k === 1) return suffixSums[i]
         let res = Infinity
         let curSum = 0
-        for (let j=i; j < n-1; j++) {
+        for (let j=i; j < n - 1; j++) {
             curSum += nums[j]
             const maxSum = Math.max(curSum, helper(j+1, k-1))
             res = Math.min(res, maxSum)
