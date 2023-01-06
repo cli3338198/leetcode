a0aee883c7b2b85d4f80e2863e94c564605998f2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-05 21:05:47 -0800

    Time: 406 ms (10.53%), Space: 44.9 MB (31.58%) - LeetHub

--- 0410-split-array-largest-sum/0410-split-array-largest-sum.ts
+++ 0410-split-array-largest-sum/0410-split-array-largest-sum.ts
@@ -1,22 +1,40 @@
 function splitArray(nums: number[], k: number): number {
-    const n = nums.length
-    const dp: Record<string, number> = {}
-    const suffixSums: number[] = Array(n+1).fill(0)
-    for (let i=n-1; i >= 0; i--) {
-        suffixSums[i] = suffixSums[i+1] + nums[i]
-    }
-    return helper(0, k)
+//     const n = nums.length
+//     const dp: Record<string, number> = {}
+//     const suffixSums: number[] = Array(n+1).fill(0)
+//     for (let i=n-1; i >= 0; i--) {
+//         suffixSums[i] = suffixSums[i+1] + nums[i]
+//     }
+//     return helper(0, k)
     
-    function helper(i: number, k: number): number {
-        const key = `${i} ${k}`
-        if (key in dp) return dp[key]
-        if (k === 1) return suffixSums[i]
-        let res = Infinity
-        let curSum = 0
-        for (let j=i; j < n-1; j++) {
-            curSum += nums[j]
-            res = Math.min(res, Math.max(curSum, helper(j+1, k-1)))
-        }
-        return dp[key] = res
+//     function helper(i: number, k: number): number {
+//         const key = `${i} ${k}`
+//         if (key in dp) return dp[key]
+//         if (k === 1) return suffixSums[i]
+//         let res = Infinity
+//         let curSum = 0
+//         for (let j=i; j < n-1; j++) {
+//             curSum += nums[j]
+//             res = Math.min(res, Math.max(curSum, helper(j+1, k-1)))
+//         }
+//         return dp[key] = res
+//     }
+    
+    const n = nums.length
+    const sums: number[] = Array(n+1).fill(0)
+    for (let i=0; i < n; i++) {
+        sums[i+1] = sums[i] + nums[i]
     }
+    const dp: number[][] = Array(k).fill(0).map(() => Array(n).fill(Infinity))
+    for (let i=0; i < n; i++) {
+        dp[0][i] = sums[n] - sums[i]
+    }
+    for (let s=1; s < k; s++) {
+        for (let i=0; i < n; i++) {
+            for (let j=i+1; j < n; j++) {
+                dp[s][i] = Math.min(dp[s][i], Math.max(dp[s-1][j], sums[j]-sums[i]))
+            }
+        }
+    }
+    return dp[k-1][0]
 };
