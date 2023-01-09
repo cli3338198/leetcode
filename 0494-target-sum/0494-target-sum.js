cb304a8505d5659936bd017fd5f67c62f38108b4
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-08 21:24:16 -0800

    Time: 75 ms (98.05%), Space: 44.9 MB (62.26%) - LeetHub

--- 0494-target-sum/0494-target-sum.js
+++ 0494-target-sum/0494-target-sum.js
@@ -17,3 +17,27 @@ var findTargetSumWays = function(nums, target) {
         return dp[key] = helper(idx+1, target-nums[idx]) + helper(idx+1, target+nums[idx])
     }
 };
+
+// find number of ways to divide nums into two sets such that set1 + set2 === target
+// s1 + s2 = sum
+// s1 - s2 = target
+// s1 - (sum - s1) = target
+// s1 = (target + sum) / 2
+// find number of ways to get s1
+findTargetSumWays = function(nums, target) {
+    const n = nums.length
+    const sum = nums.reduce((acc, val) => acc + val, 0)
+    if (Math.abs(target) > sum || (sum + target) % 2 !== 0) return 0
+    const s1 = Math.floor((target + sum) / 2)
+    const dp = Array(n+1).fill(0).map(() => Array(s1 + 1).fill(0))
+    dp[0][0] = 1
+    for (let i=1; i <= n; i++) {
+        for (let j=0; j <= s1; j++) {
+            dp[i][j] += dp[i-1][j]
+            if (nums[i-1] <= j) {
+                dp[i][j] += dp[i-1][j-nums[i-1]]
+            }
+        }
+    }
+    return dp[n][s1]
+}
