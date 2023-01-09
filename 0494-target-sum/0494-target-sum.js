29259f925c703bb53c8f4e501cd6c9a94a62f3d0
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-08 20:53:24 -0800

    Time: 340 ms (68.77%), Space: 53.9 MB (10.20%) - LeetHub

--- 0494-target-sum/0494-target-sum.js
+++ 0494-target-sum/0494-target-sum.js
@@ -4,19 +4,16 @@
  * @return {number}
  */
 var findTargetSumWays = function(nums, target) {
-    const sum = nums.reduce((acc, val) => acc + val, 0)
-    if (Math.abs(target) > sum || (sum + target) % 2 !== 0) return 0
-    const s1 = (sum + target) / 2
-    const dp = Array(nums.length+1).fill(0).map(() => Array(s1+1).fill(0))
-    dp[0][0] = 1
-    for (let i=1; i <= nums.length; i++) {
-        for (let j=0; j <= s1; j++) {
-            if (nums[i-1] <= j) {
-                dp[i][j] = dp[i-1][j] + dp[i-1][j-nums[i-1]]
-            } else {
-                dp[i][j] = dp[i-1][j]
-            }
+    const dp = {}
+    return helper(0, target)
+    
+    function helper(idx, target) {
+        const key = `${idx} ${target}`
+        if (key in dp) return dp[key]
+        if (idx === nums.length) {
+            if (target === 0) return 1
+            return 0
         }
+        return dp[key] = helper(idx+1, target-nums[idx]) + helper(idx+1, target+nums[idx])
     }
-    return dp[nums.length][s1]
 };
