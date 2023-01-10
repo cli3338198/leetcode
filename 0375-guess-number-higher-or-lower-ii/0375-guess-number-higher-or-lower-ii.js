6dc4dfaa59d81acb6e4d664dbdb8e1ab026fdb18
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-09 20:13:28 -0800

    Time: 236 ms (83.33%), Space: 51 MB (12.50%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.js
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.js
@@ -17,3 +17,19 @@ var getMoneyAmount = function(n) {
         return dp[key] = res
     }
 };
+
+getMoneyAmount = function(n) {
+    if (n === 1) return 0
+    const dp = Array(n+2).fill(0).map(() => Array(n+2).fill(0))
+    for (let len=2; len <= n; len++) {
+        for (let i=0; i <= n-len+1; i++) {
+            const j = i+len-1
+            let min = Infinity
+            for (let k=i; k <= j; k++) {
+                min = Math.min(min, k + Math.max(dp[i][k-1], dp[k+1][j]))
+            }
+            dp[i][j] = min
+        }
+    }
+    return dp[1][n]
+}
