daa121229fa4533a1132bd72c7990171600f40c1
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-09 20:05:43 -0800

    Time: 3153 ms (8.33%), Space: 57.1 MB (8.33%) - LeetHub

--- 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.js
+++ 0375-guess-number-higher-or-lower-ii/0375-guess-number-higher-or-lower-ii.js
@@ -3,18 +3,17 @@
  * @return {number}
  */
 var getMoneyAmount = function(n) {
-    if (n === 1) return 0
-    const dp = Array(n+2).fill(0).map(() => Array(n+2).fill(0))
-    for (let len=2; len <= n; len++) {
-        for (let i=1; i <= n-len+1; i++) {
-            const j = i+len-1
-            let min = Infinity
-            for (let k=i; k <= j; k++) {
-                const temp = k + Math.max(dp[i][k-1], dp[k+1][j])
-                min = Math.min(min, temp)
-            }
-            dp[i][j] = min
+    const dp = {}
+    return helper(1, n)
+    
+    function helper(lo, hi) {
+        const key = `${lo} ${hi}`
+        if (key in dp) return dp[key]
+        if (lo >= hi) return 0
+        let res = Infinity
+        for (let i=lo; i <= hi; i++) {
+            res = Math.min(res, i + Math.max(helper(lo, i-1), helper(i+1, hi)))
         }
+        return dp[key] = res
     }
-    return dp[1][n]
 };
