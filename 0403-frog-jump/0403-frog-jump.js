d5169ce66e44b99845a5db57dc2f9b881c6e5099
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-09 20:55:08 -0800

    Time: 620 ms (18.64%), Space: 110.5 MB (15.25%) - LeetHub

--- 0403-frog-jump/0403-frog-jump.js
+++ 0403-frog-jump/0403-frog-jump.js
@@ -4,17 +4,35 @@
  */
 var canCross = function(stones) {
     const n = stones.length
+    const dp = {}
+    return helper(0, 0)
+    
+    function helper(i, k) {
+        const key = `${i} ${k}`
+        if (key in dp) return dp[key]
+        for (let j=i+1; j < n; j++) {
+            const jump = stones[j] - stones[i]
+            if (jump < k - 1) continue
+            if (jump > k + 1) return false
+            if (helper(j, jump)) return true
+        }
+        return dp[key] = i === n-1
+    }
+};
+
+canCross = function(stones) {
+    const n = stones.length
     const dp = Array(n).fill(0).map(() => Array(n).fill(false))
     dp[0][1] = true
     for (let i=1; i < n; i++) {
         for (let j=0; j < i; j++) {
             const jump = stones[i] - stones[j]
-            if (jump >= n || !dp[j][jump]) continue
+            if (jump >= n || dp[j][jump] === false) continue
             dp[i][jump] = true
-            if (jump - 1 >= 0) dp[i][jump - 1] = true
-            if (jump + 1 < n) dp[i][jump + 1] = true
-            if (i === n - 1) return true
+            if (jump + 1 < n) dp[i][jump+1] = true
+            if (jump - 1 < n) dp[i][jump-1] = true
+            if (i === n-1) return true
         }
     }
     return false
-};
+}
