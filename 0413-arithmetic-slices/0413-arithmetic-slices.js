aa4be37837fdb667eacb78dd900d0159c77b95cf
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-10 19:20:19 -0800

    Time: 52 ms (97.92%), Space: 42.1 MB (40.14%) - LeetHub

--- 0413-arithmetic-slices/0413-arithmetic-slices.js
+++ 0413-arithmetic-slices/0413-arithmetic-slices.js
@@ -7,9 +7,9 @@ var numberOfArithmeticSlices = function(nums) {
     const dp = Array(n).fill(0)
     let res = 0
     for (let i=2; i < n; i++) {
-        if (nums[i]-nums[i-1] === nums[i-1]-nums[i-2]) {
-            dp[i] = dp[i-1] + 1
-        }
+        if (nums[i-2]-nums[i-1] === nums[i-1]-nums[i]) {
+            dp[i] = 1 + dp[i-1]
+        } 
         res += dp[i]
     }
     return res
