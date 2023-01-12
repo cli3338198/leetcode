ca088c956b55cdde3a7a97bd8305a6f6031166ad
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-11 20:01:37 -0800

    Time: 103 ms (87.10%), Space: 54.2 MB (93.55%) - LeetHub

--- 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.js
+++ 2270-number-of-ways-to-split-array/2270-number-of-ways-to-split-array.js
@@ -3,12 +3,13 @@
  * @return {number}
  */
 var waysToSplitArray = function(nums) {
-    const total = nums.reduce((acc, val) => acc + val, 0)
-    let prefix = 0
+    const n = nums.length
+    const totalSum = nums.reduce((acc, val) => acc + val, 0)
+    let prefixSum = 0
     let res = 0
-    for (let i=0; i < nums.length; i++) {
-        prefix += nums[i]
-        if (i < nums.length-1 && prefix >= total - prefix) {
+    for (let i=0; i < n-1; i++) {
+        prefixSum += nums[i]
+        if (prefixSum >= totalSum - prefixSum) {
             res++
         }
     }
