fc7688f32c9b580db488ab874cae9999a4283f41
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-11 20:53:17 -0800

    Time: 961 ms (68.97%), Space: 138 MB (10.34%) - LeetHub

--- 0464-can-i-win/0464-can-i-win.js
+++ 0464-can-i-win/0464-can-i-win.js
@@ -28,3 +28,24 @@ var canIWin = function(max, total) {
         return dp[key] = false
     }
 };
+
+canIWin = function(max, total) {
+    const sum = max * Math.floor((max + 1) / 2)
+    if (max >= total) return true
+    if (sum < total) return false
+    if (sum === total) return max % 2
+    const dp = {}
+    return helper(total, 0)
+    
+    function helper(total, k) {
+        if (k in dp) return dp[k]
+        for (let i=1; i <= max; i++) {
+            if ((k & (1 << i)) == 0) {
+                if (total - i <= 0 || !helper(total - i, k | (1 << i))) {
+                    return dp[k] = true
+                }
+            }
+        }
+        return dp[k] = false
+    }
+}
