0f318fbaf46dcd743f8772325f66606e1965abbd
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-12 20:51:28 -0800

    Time: 112 ms (93.33%), Space: 45.9 MB (50.00%) - LeetHub

--- 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.js
+++ 1547-minimum-cost-to-cut-a-stick/1547-minimum-cost-to-cut-a-stick.js
@@ -42,3 +42,20 @@ minCost = function(n, cuts) {
         return dp[key] = res
     }
 }
+
+minCost = function(n, cuts) {
+    cuts = [0, ...cuts, n]
+    cuts.sort((a, b) => a - b)
+    const m = cuts.length
+    const dp = Array(m).fill(0).map(() => Array(m).fill(0))
+    for (let len=2; len < m; len++) {
+        for (let i=0; i < m - len; i++) {
+            const j = i + len
+            dp[i][j] = Infinity
+            for (let k=i+1; k < j; k++) {
+                dp[i][j] = Math.min(dp[i][j], cuts[j] - cuts[i] + dp[i][k] + dp[k][j])
+            }
+        }
+    }
+    return dp[0].at(-1)
+}
