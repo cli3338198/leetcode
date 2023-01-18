cbbb4cee3ab51d2d6b462e1dcc8cde2e76af679e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-17 20:01:40 -0800

    Time: 179 ms (46.03%), Space: 55.7 MB (14.29%) - LeetHub

--- 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.js
+++ 1030-matrix-cells-in-distance-order/1030-matrix-cells-in-distance-order.js
@@ -18,3 +18,27 @@ var allCellsDistOrder = function(rows, cols, rCenter, cCenter) {
         return (Math.abs(a - rCenter) + Math.abs(b - cCenter)) < (Math.abs(c - rCenter) + Math.abs(d - cCenter)) ? -1 : 1
     }
 };
+
+allCellsDistOrder = function(rows, cols, rCenter, cCenter) {
+    const q = [[rCenter, cCenter]]
+    const res = [[rCenter, cCenter]]
+    const set = new Set()
+    set.add(`${rCenter} ${cCenter}`)
+    while (q.length) {
+        const len = q.length
+        for (let i=0; i < len; i++) {
+            const [r, c] = q.shift()
+            for (const [rr, cc] of [[0, 1], [1, 0], [-1, 0], [0, -1]]) {
+                const rrr = rr + r
+                const ccc = cc + c
+                const key = `${rrr} ${ccc}`
+                if (rrr >= 0 && rrr < rows && ccc >= 0 && ccc < cols && !set.has(key)) {
+                    set.add(key)
+                    res.push([rrr, ccc])
+                    q.push([rrr, ccc])
+                }
+            }
+        }
+    }
+    return res
+}
