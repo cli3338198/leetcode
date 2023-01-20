f9b85aae776564afc0ae20322a5c7df7b89ec23e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-20 14:45:46 -0800

    Time: 110 ms (51.19%), Space: 48.2 MB (33.89%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.js
+++ 0695-max-area-of-island/0695-max-area-of-island.js
@@ -27,3 +27,38 @@ var maxAreaOfIsland = function(grid) {
         return res
     }
 };
+
+maxAreaOfIsland = function(grid) {
+    const rows = grid.length
+    const cols = grid[0].length
+    let res = 0
+    const set = new Set()
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < cols; c++) {
+            if (grid[r][c] === 1 && !set.has(`${r} ${c}`)) {
+                res = Math.max(res, bfs(r, c))
+            }
+        }
+    }
+    return res
+    
+    function bfs(r, c) {
+        const q = [[r, c]]
+        let res = 0
+        while (q.length) {
+            const [r, c] = q.shift()
+            if (!set.has(`${r} ${c}`)) {
+                set.add(`${r} ${c}`)
+                res++
+                for (const [rr, cc] of [[0, 1], [1, 0], [0, -1], [-1, 0]]) {
+                    const rrr = rr + r
+                    const ccc = cc + c
+                    if (rrr in grid && ccc in grid[rrr] && grid[rrr][ccc] === 1 && !set.has(`${rrr} ${ccc}`)) {
+                        q.push([rrr, ccc])
+                    }
+                }
+            }
+        }
+        return res
+    }
+}
