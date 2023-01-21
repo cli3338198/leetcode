d21b120b5ae2ff3d3b825acc5c418af22da741c4
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-20 20:26:55 -0800

    Time: 74 ms (59.91%), Space: 44.4 MB (11.03%) - LeetHub

---
+++ 0744-find-smallest-letter-greater-than-target/0744-find-smallest-letter-greater-than-target.js
@@ -0,0 +1,20 @@
+/**
+ * @param {character[]} letters
+ * @param {character} target
+ * @return {character}
+ */
+var nextGreatestLetter = function(letters, target) {
+    letters.sort((a, b) => a - b)
+    let l = 0
+    let r = letters.length-1
+    while (l <= r) {
+        const m = Math.floor((l+r)/2)
+        if (letters[m] > target) {
+            res = letters[m]
+            r = m - 1
+        } else {
+            l = m + 1
+        }
+    }
+    return letters[l % letters.length]
+};
