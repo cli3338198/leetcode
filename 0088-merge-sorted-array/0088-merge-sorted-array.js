17f272c3e12ed3f682db02e9fa1198a62b93a4f4
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-23 18:27:29 -0800

    Time: 69 ms (59.65%), Space: 42.1 MB (48.82%) - LeetHub

---
+++ 0088-merge-sorted-array/0088-merge-sorted-array.js
@@ -0,0 +1,25 @@
+/**
+ * @param {number[]} nums1
+ * @param {number} m
+ * @param {number[]} nums2
+ * @param {number} n
+ * @return {void} Do not return anything, modify nums1 in-place instead.
+ */
+var merge = function(nums1, m, nums2, n) {
+    let o = m + n - 1
+    while (m > 0 && n > 0) {
+        if (nums1[m-1] > nums2[n-1]) {
+            nums1[o] = nums1[m-1]
+            m--
+        } else {
+            nums1[o] = nums2[n-1]
+            n--
+        }
+        o--
+    }
+    while (n > 0) {
+        nums1[o] = nums2[n-1]
+        o--
+        n--
+    }
+};
