81a06540b33f70bcc2245d5d7b1830a927ec3fda
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-26 21:02:11 -0800

    Time: 84 ms (42.31%), Space: 47.8 MB (19.23%) - LeetHub

--- 0799-champagne-tower/0799-champagne-tower.js
+++ 0799-champagne-tower/0799-champagne-tower.js
@@ -5,14 +5,14 @@
  * @return {number}
  */
 var champagneTower = function(poured, query_row, query_glass) {
-  const dp = Array(101).fill(0).map(() => Array(101).fill(0))
-  dp[0][0] = poured
-  for (let i = 0; i < query_row; i++) {
-    for (let j = 0; j <= query_glass; j++) {
-      const remain = Math.max(dp[i][j] - 1, 0)
-      dp[i + 1][j] += remain / 2
-      dp[i + 1][j + 1] += remain / 2
+    const dp = Array(101).fill(0).map(() => Array(101).fill(0))
+    dp[0][0] = poured
+    for (let r=0; r < query_row; r++) {
+        for (let c=0; c <= query_glass; c++) {
+            const remaining = Math.max(dp[r][c] - 1, 0)
+            dp[r+1][c] += remaining / 2
+            dp[r+1][c+1] += remaining / 2
+        }
     }
-  }
-  return Math.min(dp[query_row][query_glass], 1)
+    return Math.min(dp[query_row][query_glass], 1)
 };
