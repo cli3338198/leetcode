82126f7b31d6edb287bddfb07916dabee0e09e70
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-01-31 20:46:31 -0800

    Time: 117 ms (95.79%), Space: 43.7 MB (91.58%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -5,13 +5,14 @@
 var canPartition = function(nums) {
     const sum = nums.reduce((acc, val) => acc + val, 0)
     if (sum % 2 !== 0) return false
-    nums.sort((a, b) => a - b)
     const halfSum = sum / 2
     const dp = Array(halfSum + 1).fill(false)
     dp[0] = true
     for (let i=0; i < nums.length; i++) {
         for (let j=halfSum; j >= nums[i]; j--) {
-            if (dp[j - nums[i]] === true) dp[j] = true
+            if (dp[j-nums[i]] === true) {
+                dp[j] = true
+            }
         }
     }
     return dp[halfSum]
