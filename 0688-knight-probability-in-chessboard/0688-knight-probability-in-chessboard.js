096cd1f2e92ddea94c0857e9f36621de7a3c7d1e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-03 16:17:09 -0800

    Time: 168 ms (13.33%), Space: 48.1 MB (25.55%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.js
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.js
@@ -24,3 +24,30 @@ var knightProbability = function(n, k, row, column) {
         return dp[key] = res
     }
 };
+
+knightProbability = function(n, k, row, column) {
+    const moves = [[-2, -1], [-2, 1], [-1, 2], [1, 2], [2, -1], [2, 1], [-1, -2], [1, -2]]
+    let dp = Array(n).fill(0).map(() => Array(n).fill(0))
+    dp[row][column] = 1
+    for (let i=1; i <= k; i++) {
+        const newDp = Array(n).fill(0).map(() => Array(n).fill(0))
+        for (let r=0; r < n; r++) {
+            for (let c=0; c < n; c++) {
+                for (const [rr, cc] of moves) {
+                    const rrr = r + rr
+                    const ccc = c + cc
+                    if (rrr < 0 || rrr >= n || ccc < 0 || ccc >= n) continue
+                    newDp[r][c] += dp[rrr][ccc] 
+                }
+            }
+        }
+        dp = newDp
+    }
+    let res = 0
+    for (let r=0; r < n; r++) {
+        for (let c=0; c < n; c++) {
+            res += dp[r][c]
+        }
+    }
+    return res / Math.pow(8, k)
+}
