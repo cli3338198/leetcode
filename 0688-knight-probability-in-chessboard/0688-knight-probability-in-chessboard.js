6141b0b10e1e7a063281170e0f682cae1a331e8a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-05 13:42:29 -0800

    Time: 150 ms (20.45%), Space: 46.9 MB (36.36%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.js
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.js
@@ -43,3 +43,24 @@ knightProbability = function(n, k, row, column) {
     }
     return dp[k].reduce((acc, row) => acc + row.reduce((acc, cell) => acc + cell ,0) ,0)
 }
+
+knightProbability = function(n, k, row, column) {
+    const moves = [[-2, -1], [-2, 1], [-1, 2], [1, 2], [2, -1], [2, 1], [-1, -2], [1, -2]]
+    let dp = Array(n).fill(0).map(() => Array(n).fill(0))
+    dp[row][column] = 1
+    for (let i=1; i <= k; i++) {
+        const newDp = Array(n).fill(0).map(() => Array(n).fill(0))
+        for (let r=0; r < n; r++) {
+            for (let c=0; c < n; c++) {
+                for (const [rr, cc] of moves) {
+                    const rrr = r + rr
+                    const ccc = c + cc
+                    if (rrr < 0 || rrr >= n || ccc < 0 || ccc >= n) continue
+                    newDp[r][c] += dp[rrr][ccc] / 8
+                }
+            }
+        }
+        dp = newDp
+    }
+    return dp.reduce((acc, row) => acc + row.reduce((acc, cell) => acc + cell ,0) ,0)
+}
