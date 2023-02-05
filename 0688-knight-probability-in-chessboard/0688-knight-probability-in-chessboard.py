d4bcd802df2bf2c7f8f95b23eda8e8793fe28717
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-05 13:57:06 -0800

    Time: 377 ms (37.61%), Space: 16.6 MB (53.44%) - LeetHub

--- 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
+++ 0688-knight-probability-in-chessboard/0688-knight-probability-in-chessboard.py
@@ -2,12 +2,10 @@ class Solution:
     def knightProbability(self, n: int, k: int, row: int, column: int) -> float:
         
         moves = [[-2, -1], [-2, 1], [-1, 2], [1, 2], [2, -1], [2, 1], [-1, -2], [1, -2]]
-        dp = [[0] * n for _ in range(n)]
-        dp[row][column] = 1
+        dp = [[[0] * n for _ in range(n)] for _ in range(k+1)]
+        dp[0][row][column] = 1
         
         for i in range(1, k+1):
-            new_dp = [[0] * n for _ in range(n)]
-            
             for r in range(n):
                 for c in range(n):
                     for rr, cc in moves:
@@ -16,15 +14,7 @@ class Solution:
                         
                         if rrr < 0 or rrr >= n or ccc < 0 or ccc >= n:
                             continue
+                            
+                        dp[i][r][c] += dp[i-1][rrr][ccc] / 8
                         
-                        new_dp[r][c] += dp[rrr][ccc]
-            
-            dp = new_dp
-        
-        res = 0
-        for r in range(n):
-            for c in range(n):
-                res += dp[r][c]
-        
-        return res / pow(8, k)
-            
+        return sum(map(sum, dp[k]))
