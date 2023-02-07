fc93a6e951032d914a909ec5cd7e11dafb6cd3a1
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-06 20:23:42 -0800

    Time: 879 ms (16.67%), Space: 171.3 MB (16.67%) - LeetHub

--- 0808-soup-servings/0808-soup-servings.js
+++ 0808-soup-servings/0808-soup-servings.js
@@ -21,3 +21,22 @@ var soupServings = function(n) {
         return dp[key] = res
     }
 };
+
+soupServings = function(n) {
+    if (n >= 4800) return 1
+    const ops = [[-100, 0], [-75, -25], [-50, -50], [-25, -75]]
+    const dp = Array(n+1).fill(0).map(() => Array(n+1).fill(0))
+    dp[0].fill(1)
+    dp[0][0] = 0.5
+    for (let i=1; i <= n; i++) {
+        for (let j=1; j <= n; j++) {
+            for (const [ii, jj] of ops) {
+                const iii = i + ii
+                const jjj = j + jj
+                const temp = dp[Math.max(0, iii)][Math.max(0, jjj)]
+                dp[i][j] += 0.25 * temp
+            }
+        }
+    }
+    return dp[n][n]
+}
