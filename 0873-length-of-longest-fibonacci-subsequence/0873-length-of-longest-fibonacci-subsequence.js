6ec8f3c53c81aaee48dd089a3dde0650ddc8692a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-07 21:45:30 -0800

    Time: 589 ms (40.00%), Space: 77.4 MB (20.00%) - LeetHub

--- 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.js
+++ 0873-length-of-longest-fibonacci-subsequence/0873-length-of-longest-fibonacci-subsequence.js
@@ -39,3 +39,21 @@ lenLongestFibSubseq = function(arr) {
     }
     return res > 2 ? res : 0
 }
+
+lenLongestFibSubseq = function(arr) {
+    const n = arr.length
+    const dp = Array(n).fill(0).map(() => Array(n).fill(2))
+    const map = {}
+    let res = 0
+    for (let i=0; i < n; i++) {
+        map[arr[i]] = i
+        for (let j=0; j < i; j++) {
+            if (arr[i] - arr[j] < arr[j] && arr[i] - arr[j] in map) {
+                const k = map[arr[i] - arr[j]]
+                dp[j][i] = Math.max(dp[j][i], 1 + dp[k][j])
+            }
+            res = Math.max(res, dp[j][i])
+        }
+    }
+    return res > 2 ? res : 0
+}
