53272ec7ee3c1b1e58ce1db71be012f68f887296
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-08 20:57:52 -0800

    Time: 83 ms (57.03%), Space: 45 MB (74.19%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -12,24 +12,3 @@ var permute = function(nums) {
     }
     return res
 };
-
-permute = function(nums) {
-    const n = nums.length
-    const q = [[[], Array(n).fill(false)]]
-    for (let i=0; i < n; i++) {
-        const len = q.length
-        for (let j=0; j < len; j++) {
-            const [curList, used] = q.shift()
-            for (let k=0; k < n; k++) {
-                if (used[k] === false) {
-                    const copyUsed = used.slice()
-                    copyUsed[k] = true
-                    const copyList = curList.slice()
-                    copyList.push(nums[k])
-                    q.push([copyList, copyUsed])
-                }
-            }
-        }
-    }
-    return q.map(x => x[0])
-}
