30e38ba377843dfc3a8b323e44ba210798193930
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-09 20:17:31 -0800

    Time: 73 ms (91.67%), Space: 44.3 MB (93.84%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.js
+++ 0740-delete-and-earn/0740-delete-and-earn.js
@@ -18,3 +18,17 @@ var deleteAndEarn = function(nums) {
     }
 };
 
+deleteAndEarn = function(nums) {
+    const max = Math.max(...nums)
+    const freq = Array(max+1).fill(0)
+    const dp = Array(max+1).fill(0)
+    for (const n of nums) {
+        freq[n]++
+    }
+    dp[1] = 1 * freq[1]
+    for (let i=2; i <= max; i++) {
+        dp[i] = Math.max(dp[i-1], dp[i-2] + i * freq[i])
+    }
+    return dp[max]
+}
+
