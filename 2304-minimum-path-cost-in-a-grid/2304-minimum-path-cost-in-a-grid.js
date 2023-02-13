378728cbb1958e02c7e90f3b7d5410b77d47460e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-13 15:23:07 -0800

    Time: 127 ms (71.79%), Space: 53.5 MB (87.18%) - LeetHub

---
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[][]} grid
+ * @param {number[][]} moveCost
+ * @return {number}
+ */
+var minPathCost = function(grid, moveCost) {
+    const rows = grid.length
+    const cols = grid[0].length
+    const dp = Array(rows).fill(0).map(() => Array(cols).fill(Infinity))
+    dp[0] = grid[0].slice()
+    for (let r=1; r < rows; r++) {
+        for (let c=0; c < cols; c++) {
+            for (let k=0; k < cols; k++) {
+                dp[r][c] = Math.min(dp[r][c], grid[r][c] + dp[r-1][k] + moveCost[grid[r-1][k]][c])
+            }
+        }
+    }
+    return Math.min(...dp[rows-1])
+};
