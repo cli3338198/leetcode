d0550b70d3c924f69fe03d4c554358168396e53e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-13 22:24:11 -0800

    Time: 62 ms (81.60%), Space: 42 MB (62.98%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -31,3 +31,13 @@ uniquePaths = function(m, n) {
     }
     return dp[0][0]
 }
+
+uniquePaths = function(m, n) {
+    const dp = Array(m).fill(0).map(() => Array(n).fill(1))
+    for (let i=1; i < m; i++) {
+        for (let j=1; j < n; j++) {
+            dp[i][j] = dp[i-1][j] + dp[i][j-1] 
+        }
+    }
+    return dp[m-1][n-1]
+}
