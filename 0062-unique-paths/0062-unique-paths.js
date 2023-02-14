102edfe43e914c8fbad164ffe0e070e59093e18a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-13 22:06:47 -0800

    Time: 73 ms (37.15%), Space: 44.1 MB (10.51%) - LeetHub

--- 0062-unique-paths/0062-unique-paths.js
+++ 0062-unique-paths/0062-unique-paths.js
@@ -3,19 +3,15 @@
  * @param {number} n
  * @return {number}
  */
-var uniquePaths = function(rows, cols) {
-    const dp = Array(rows).fill(0).map(() => Array(cols).fill(0))
-    dp[rows-1][cols-1] = 1
-    for (let r=rows-1; r >= 0; r--) {
-        dp[r][cols-1] = 1
+var uniquePaths = function(m, n) {
+    const dp = {}
+    return helper(0, 0)
+    
+    function helper(r, c) {
+        const key = `${r} ${c}`
+        if (key in dp) return dp[key]
+        if (r >= m || c >= n) return 0
+        if (r === m-1 && c === n-1) return 1
+        return dp[key] = helper(r+1, c) + helper(r, c+1)
     }
-    for (let c=cols-1; c >= 0; c--) {
-        dp[rows-1][c] = 1
-    }
-    for (let r=rows-2; r >= 0; r--) {
-        for (let c=cols-2; c >= 0; c--) {
-            dp[r][c] = dp[r+1][c] + dp[r][c+1]
-        }
-    }
-    return dp[0][0]
 };
