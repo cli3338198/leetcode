ecac39fd902b9b428b97beb5e11efbb8fd5a8a4b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-15 20:03:18 -0800

    Time: 106 ms (35.60%), Space: 49.4 MB (21.47%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.js
+++ 0072-edit-distance/0072-edit-distance.js
@@ -16,3 +16,19 @@ var minDistance = function(word1, word2) {
         return dp[key] = 1 + Math.min(helper(i+1, j), helper(i, j+1), helper(i+1, j+1))
     }
 };
+
+minDistance = function(word1, word2) {
+    const m = word1.length
+    const n = word2.length
+    const dp = Array(m+1).fill(0).map((_,i) => Array(n+1).fill(0).map((_,j) => i === 0 ? j : j === 0 ? i : 0))
+    for (let i=1; i <= m; i++) {
+        for (let j=1; j <= n; j++) {
+            if (word1[i-1] === word2[j-1]) {
+                dp[i][j] = dp[i-1][j-1]
+            } else {
+                dp[i][j] = 1 + Math.min(dp[i-1][j], dp[i][j-1], dp[i-1][j-1])
+            }
+        }
+    }
+    return dp[m][n]
+}
