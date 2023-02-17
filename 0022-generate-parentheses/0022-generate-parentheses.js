3c97431fac13504c244ce8e7e22854c9cd000098
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-02-16 19:19:22 -0800

    Time: 57 ms (94.49%), Space: 42.1 MB (90.14%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.js
+++ 0022-generate-parentheses/0022-generate-parentheses.js
@@ -7,44 +7,11 @@ var generateParenthesis = function(n) {
     helper(0, 0, '')
     return res
     
-    function helper(left, right, cur) {
-        if (left === right && left === n) {
+    function helper(l, r, cur) {
+        if (l === r && l === n) {
             return res.push(cur)
         }
-        if (left < n) helper(left + 1, right, cur + "(")
-        if (right < left) helper(left, right + 1, cur + ")")
+        if (l < n) helper(l+1, r, cur + "(")
+        if (r < l) helper(l, r+1, cur + ")")
     }
 };
-
-generateParenthesis = function(n) {
-    const q = [[0, 0, '']]
-    for (let i=0; i < (n * 2); i++) {
-        const len = q.length
-        for (let i=0; i < len; i++) {
-            const [left, right, cur] = q.shift()
-            if (left < n) q.push([left + 1, right, cur + "("])
-            if (right < left) q.push([left, right + 1, cur + ")"])
-        }
-        
-    }
-    return q.map(arr => arr[2])
-}
-
-generateParenthesis = function(n) {
-    const dp = {}
-    return helper(n)
-    
-    function helper(n) {
-        if (n <= 0) return [""]
-        if (n in dp) return dp[n]
-        const res = []
-        for (let i=0; i < n; i++) {
-            for (const left of helper(n-i-1)) {
-                for (const right of helper(i)) {
-                    res.push('(' + right + ')' + left)
-                }
-            }
-        }
-        return dp[n] = res
-    }
-}
