f3450d9877c45147f4518a2433443ba7977788dc
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-03-01 22:11:57 -0800

    Time: 78 ms (87.65%), Space: 44.1 MB (29.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -16,3 +16,30 @@ var lengthOfLIS = function(nums) {
     }
     return max
 };
+
+lengthOfLIS = function(nums) {
+    const dp = []
+    for (const n of nums) {
+        if (!dp.length || dp[dp.length-1].at(-1) < n) {
+            dp.push([n])
+        } else {
+            const idx = binarySearch(n)
+            dp[idx].push(n)
+        }
+    }
+    return dp.length
+    
+    function binarySearch(n) {
+        let l = 0, r = dp.length-1
+        while (l < r) {
+            const m = Math.floor((l+r)/2)
+            const num = dp[m].at(-1)
+            if (num < n) {
+                l = m + 1
+            } else {
+                r = m
+            }
+        }
+        return l
+    }
+}
