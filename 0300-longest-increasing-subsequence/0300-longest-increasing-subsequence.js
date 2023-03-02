a204463ff993f9842de447309906a034e68c4f40
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-03-01 22:05:03 -0800

    Time: 196 ms (48.07%), Space: 43.6 MB (61.76%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.js
@@ -5,12 +5,14 @@
 var lengthOfLIS = function(nums) {
     const n = nums.length
     const dp = Array(n).fill(1)
+    let max = 1
     for (let i=1; i < n; i++) {
         for (let j=0; j < i; j++) {
             if (nums[j] < nums[i]) {
                 dp[i] = Math.max(dp[i], 1 + dp[j])
             }
         }
+        max = Math.max(max, dp[i])
     }
-    return Math.max(...dp)
+    return max
 };
