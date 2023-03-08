6721199dbc99e5858f551c0818a46ae07ea6d849
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-03-08 12:04:16 -0800

    Time: 183 ms (75.68%), Space: 54 MB (40.54%) - LeetHub

--- 1462-course-schedule-iv/1462-course-schedule-iv.js
+++ 1462-course-schedule-iv/1462-course-schedule-iv.js
@@ -24,3 +24,35 @@ var checkIfPrerequisite = function(numCourses, prerequisites, queries) {
         return false
     }
 };
+
+checkIfPrerequisite = function(numCourses, prerequisites, queries) {
+    const graph = {}
+    const indegree = {}
+    const map = {}
+    const q = []
+    for (let i=0; i < numCourses; i++) {
+        graph[i] = []
+        indegree[i] = 0
+        map[i] = new Set()
+    }
+    for (const [u, v] of prerequisites) {
+        graph[u].push(v)
+        indegree[v]++
+    }
+    for (const key in indegree) {
+        if (indegree[key] === 0) {
+            q.push(key)
+        }
+    }
+    while (q.length) {
+        const u = q.shift()
+        for (const v of graph[u]) {
+            [u, ...Array.from(map[u])].forEach(map[v].add, map[v])
+            indegree[v]--
+            if (indegree[v] === 0) {
+                q.push(v)
+            }
+        }
+    }
+    return queries.map(([u, v]) => map[v].has(Number(u)) || map[v].has(String(u)))
+}
