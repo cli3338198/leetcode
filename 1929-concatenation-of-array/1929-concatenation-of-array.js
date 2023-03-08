56a1f26617ee2de3ab4098d4003db7c71e34efb2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-03-07 16:23:27 -0800

    Time: 74 ms (85.74%), Space: 45.6 MB (42.56%) - LeetHub

--- 1929-concatenation-of-array/1929-concatenation-of-array.js
+++ 1929-concatenation-of-array/1929-concatenation-of-array.js
@@ -9,3 +9,11 @@ var getConcatenation = function(nums) {
 getConcatenation = function(nums) {
     return [...nums, ...nums]
 }
+
+getConcatenation = function(nums) {
+    const n = nums.length
+    for (let i=0; i < n; i++) {
+        nums[i+n] = nums[i]
+    }
+    return nums
+}
