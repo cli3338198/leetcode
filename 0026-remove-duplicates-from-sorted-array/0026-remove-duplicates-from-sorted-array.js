4e43188d59e7c873f44bc054e7d9dcc0bb3aad2f
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-03-07 16:13:54 -0800

    Time: 71 ms (85.49%), Space: 44.6 MB (67.12%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var removeDuplicates = function(nums) {
+    let i = 0
+    for (let j=0; j < nums.length; j++) {
+        if (nums[i] !== nums[j]) {
+            nums[++i] = nums[j]
+        }
+    }
+    return i + 1
+};
