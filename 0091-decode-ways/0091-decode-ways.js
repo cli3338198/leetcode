0478eeb60ce72d34489d5cd41bbc50d6d2cde0a4
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-03-10 12:26:42 -0800

    Time: 66 ms (74.11%), Space: 42.6 MB (59.05%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.js
+++ 0091-decode-ways/0091-decode-ways.js
@@ -18,3 +18,20 @@ var numDecodings = function(s) {
         return dp[idx] = res
     }
 };
+
+numDecodings = function(s) {
+    if (s[0] == 0) return 0
+    const n = s.length
+    const dp = Array(n+1).fill(0)
+    dp[0] = 1
+    dp[1] = 1
+    for (let i=2; i <= n; i++) {
+        if (s[i-1] != 0) {
+            dp[i] += dp[i-1]
+        }
+        if (s[i-2] == 1 || (s[i-2] == 2 && s[i-1] <= 6)) {
+            dp[i] += dp[i-2]
+        }
+    }
+    return dp[n]
+}
