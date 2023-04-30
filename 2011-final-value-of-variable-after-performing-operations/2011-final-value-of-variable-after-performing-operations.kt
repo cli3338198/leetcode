3aceede0b43d368cfe9f916181a604a5fb1a4dbc
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-04-29 21:28:59 -0700

    Time: 175 ms (50.85%), Space: 40 MB (5.08%) - LeetHub

--- 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.kt
+++ 2011-final-value-of-variable-after-performing-operations/2011-final-value-of-variable-after-performing-operations.kt
@@ -2,10 +2,10 @@ class Solution {
     fun finalValueAfterOperations(operations: Array<String>): Int {
         var res: Int = 0
         for (op in operations) {
-            if (op == "++X" || op == "X++") {
-                res += 1
-            } else {
-                res -= 1
+            when (op) {
+                "++X" -> res += 1
+                "X++" -> res += 1
+                else -> res -= 1
             }
         }
         return res
