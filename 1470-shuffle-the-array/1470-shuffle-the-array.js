1219e61b5804ed1eb859efcf97cf556490701f8e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-04-30 19:14:41 -0700

    Time: 74 ms (29.27%), Space: 44.5 MB (44.92%) - LeetHub

--- 1470-shuffle-the-array/1470-shuffle-the-array.js
+++ 1470-shuffle-the-array/1470-shuffle-the-array.js
@@ -12,3 +12,12 @@ var shuffle = function(nums, n) {
     }
     return res
 };
+
+shuffle = function(nums, n) {
+    const res = []
+    for (let i=0; i < n; i++) {
+        res.push(nums[i])
+        res.push(nums[i+n])
+    }
+    return res
+}
