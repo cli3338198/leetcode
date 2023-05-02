c8cc55df8a908d5f4c0cbc3dcb3971b43707865a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-01 21:39:38 -0700

    Time: 63 ms (30.29%), Space: 42.1 MB (35.45%) - LeetHub

--- 1512-number-of-good-pairs/1512-number-of-good-pairs.js
+++ 1512-number-of-good-pairs/1512-number-of-good-pairs.js
@@ -13,3 +13,15 @@ var numIdenticalPairs = function(nums) {
     }
     return res
 };
+
+numIdenticalPairs = function(nums) {
+    const map = {}
+    for (const num of nums) {
+        map[num] = map[num] + 1 || 1
+    }
+    let res = 0
+    for (const num in map) {
+        res += Math.floor((map[num] * (map[num]-1)) / 2)
+    }
+    return res
+}
