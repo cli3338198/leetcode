c034111f47088f04d279e86c91b6b0faede36fd6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-12 21:23:30 -0700

    Time: 80 ms (86.00%), Space: 45.5 MB (16.29%) - LeetHub

--- 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.js
@@ -7,3 +7,24 @@ var maxProductDifference = function(nums) {
     const a = nums.at(0), b = nums.at(1), c = nums.at(-1), d = nums.at(-2)
     return (a * b) - (c * d)
 };
+
+maxProductDifference = function(nums) {
+    const max = [Math.max(nums[0], nums[1]), Math.min(nums[0], nums[1])]
+    const min = [Math.min(nums[0], nums[1]), Math.max(nums[0], nums[1])]
+    for (let i=2; i < nums.length; i++) {
+        const cur = nums[i]
+        if (cur > max[0] && cur > max[1]) {
+            max[1] = max[0]
+            max[0] = cur
+        } else if (cur > max[1] && cur <= max[0]) {
+            max[1] = cur
+        }
+        if (cur < min[0] && cur < min[1]) {
+            min[1] = min[0]
+            min[0] = cur
+        } else if (cur < min[1] && cur >= min[0]) {
+            min[1] = cur
+        }
+    }
+    return (max[0] * max[1]) - (min[0] * min[1])
+}
