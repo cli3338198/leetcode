79921eca9485a37f302bb15c2080ca847617f1e3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-12 22:00:14 -0700

    Time: 55 ms (75.14%), Space: 43.3 MB (5.08%) - LeetHub

---
+++ 1732-find-the-highest-altitude/1732-find-the-highest-altitude.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} gain
+ * @return {number}
+ */
+var largestAltitude = function(gain) {
+    return gain.reduce(reducer, [0, 0])[1]
+    
+    function reducer(acc, val) {
+        let [ht, mx] = acc
+        return [ht + val, Math.max(mx, ht + val)]
+    }
+};
