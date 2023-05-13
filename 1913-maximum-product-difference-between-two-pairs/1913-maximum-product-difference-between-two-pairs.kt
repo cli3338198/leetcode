88649f24964c419886a65f3781eccb1bbfa481cb
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-12 21:32:15 -0700

    Time: 343 ms (20.00%), Space: 56.3 MB (20.00%) - LeetHub

---
+++ 1913-maximum-product-difference-between-two-pairs/1913-maximum-product-difference-between-two-pairs.kt
@@ -0,0 +1,6 @@
+class Solution {
+    fun maxProductDifference(nums: IntArray): Int {
+        nums.sortDescending()
+        return (nums[0] * nums[1]) - (nums[nums.size-1] * nums[nums.size-2])
+    }
+}
