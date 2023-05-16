01dc2bc89e398b494f6db9d973accb336fbebc92
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-15 21:18:14 -0700

    Time: 110 ms (89.55%), Space: 47.3 MB (69.31%) - LeetHub

--- 0561-array-partition/0561-array-partition.js
+++ 0561-array-partition/0561-array-partition.js
@@ -5,8 +5,8 @@
 var arrayPairSum = function(nums) {
     nums.sort((a, b) => a - b)
     let res = 0
-    for (let i=0; i < nums.length-1; i += 2) {
+    for (let i=0; i < nums.length; i += 2) {
         res += Math.min(nums[i], nums[i+1])
     }
-    return res
+    return res    
 };
