9bd87d5de36abcb3f05085291bd41ad74f32ef48
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-17 19:30:19 -0700

    Time: 104 ms (5.36%), Space: 49.1 MB (5.00%) - LeetHub

--- 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
+++ 2185-counting-words-with-a-given-prefix/2185-counting-words-with-a-given-prefix.js
@@ -13,3 +13,35 @@ var prefixCount = function(words, pref) {
     }
     return res
 };
+
+prefixCount = function(words, pref) {
+    class Trie {
+        constructor() {
+            this.children = {}
+            this.size = 0
+        }
+        insert(word) {
+            let cur = this
+            for (const char of word) {
+                if (!(char in cur.children)) {
+                    cur.children[char] = new Trie()
+                }
+                cur = cur.children[char]
+                cur.size++
+            }
+        }
+        find(pref) {
+            let cur = this
+            for (const char of pref) {
+                if (!(char in cur.children)) return 0
+                cur = cur.children[char]
+            }
+            return cur.size
+        }
+    }
+    const t = new Trie()
+    for (const word of words) {
+        t.insert(word)
+    }
+    return t.find(pref)
+}
