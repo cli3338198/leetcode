f4daa61cfaf0173e9fff6e254adc6e57eaaf818a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-18 20:11:30 -0700

    Time: 55 ms (91.50%), Space: 44.9 MB (20.45%) - LeetHub

---
+++ 2529-maximum-count-of-positive-integer-and-negative-integer/2529-maximum-count-of-positive-integer-and-negative-integer.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maximumCount = function(nums) {
+    let pos = 0, neg = 0
+    for (const num of nums) {
+        if (num > 0) {
+            pos++
+        } else if (num < 0) {
+            neg++
+        }
+    }
+    return Math.max(pos, neg)
+};
