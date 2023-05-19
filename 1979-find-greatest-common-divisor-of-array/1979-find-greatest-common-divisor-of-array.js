1a04f036733c24fb61691f667670c3c4f4311ad3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-18 19:49:11 -0700

    Time: 74 ms (10.03%), Space: 44.5 MB (10.32%) - LeetHub

---
+++ 1979-find-greatest-common-divisor-of-array/1979-find-greatest-common-divisor-of-array.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findGCD = function(nums) {
+    let smallest = Infinity
+    let greatest = -Infinity
+    for (const num of nums) {
+        smallest = Math.min(smallest, num)
+        greatest = Math.max(greatest, num)
+    }
+    let res = 1
+    for (let i=1; i <= greatest; i++) {
+        if (smallest % i === 0 && greatest % i === 0) {
+            res = Math.max(res, i)
+        }
+    }
+    return res
+};
