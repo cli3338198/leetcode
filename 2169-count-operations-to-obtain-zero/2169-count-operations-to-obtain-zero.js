816798c497d95a740c033cbb6dbc9d3234a9e7e1
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-22 20:28:13 -0700

    Time: 61 ms (64.74%), Space: 41.7 MB (83.97%) - LeetHub

---
+++ 2169-count-operations-to-obtain-zero/2169-count-operations-to-obtain-zero.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number} num1
+ * @param {number} num2
+ * @return {number}
+ */
+var countOperations = function(num1, num2) {
+    let cnt = 0
+    while (num1 > 0 && num2 > 0) {
+        if (num1 >= num2) {
+            num1 -= num2
+        } else {
+            num2 -= num1
+        }
+        cnt++
+    }
+    return cnt
+};
