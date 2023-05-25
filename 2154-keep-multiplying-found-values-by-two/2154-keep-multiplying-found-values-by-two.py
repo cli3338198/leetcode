f5a4fce2a8a6c59a48ef1636009b43c8f9bdb29b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-24 22:36:14 -0700

    Time: 79 ms (24.92%), Space: 16.6 MB (22.93%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.py
@@ -1,16 +1,18 @@
 class Solution:
-    def findFinalValue(self, nums: List[int], original: int) -> int:
-        def bin_search(original: int, l: int, r: int, nums: List[int]) -> bool: 
-            while l <= r:
-                m = (l+r)//2
-                if nums[m] == original:
-                    return True
-                elif nums[m] > original:
-                    r = m - 1
-                else:
-                    l = m + 1
-            return False
+    def bin_search(self, nums: List[int], original: int) -> bool:
+        l, r = 0, len(nums)-1
+        while l <= r:
+            m = (l+r)//2
+            if nums[m] == original:
+                return True
+            elif nums[m] > original:
+                r = m - 1
+            else:
+                l = m + 1
+        return False
+    
+    def findFinalValue(self, nums: List[int], original: int) -> int:         
         nums.sort()
-        if bin_search(original, 0, len(nums)-1, nums):
+        if self.bin_search(nums, original):
             return self.findFinalValue(nums, original*2)
         return original
