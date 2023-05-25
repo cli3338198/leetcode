8ea13b31f29af5ead539aa181ed23ec137d7a3a6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-24 22:20:43 -0700

    Time: 66 ms (33.51%), Space: 43.1 MB (42.16%) - LeetHub

--- 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
+++ 2154-keep-multiplying-found-values-by-two/2154-keep-multiplying-found-values-by-two.js
@@ -10,3 +10,24 @@ var findFinalValue = function(nums, original) {
     }
     return original
 };
+
+findFinalValue = function(nums, original) {
+    nums.sort((a, b) => a - b)
+    if (binSearch(original)) return findFinalValue(nums, original*2)
+    return original
+    
+    function binSearch(original) {
+        let l=0, r=nums.length-1
+        while (l <= r) {
+            const m = Math.floor((l+r)/2)
+            if (nums[m] === original) {
+                return true
+            } else if (nums[m] > original) {
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+        return false
+    }
+}
