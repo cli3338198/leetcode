6755b1e97299453707309189561d94cc692d6ac8
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-25 21:49:47 -0700

    Time: 87 ms (5.60%), Space: 45.4 MB (6.80%) - LeetHub

--- 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
+++ 2357-make-array-zero-by-subtracting-equal-amounts/2357-make-array-zero-by-subtracting-equal-amounts.js
@@ -17,3 +17,70 @@ var minimumOperations = function(nums) {
     }
     return res
 };
+
+minimumOperations = function(nums) {
+    class Heap {
+        constructor(compare) {
+            this.h = []
+            this.compare = compare
+        }
+        insert(a) {
+            this.h.push(a)
+            this.bubbleUp(this.h.length-1)
+        }
+        bubbleUp(c) {
+            let p = Math.ceil(c/2) - 1
+            while (p in this.h && this.compare(this.h[p], this.h[c])) {
+                this.swap(c, p)
+                c = p
+                p = Math.ceil(c/2) - 1
+            }
+        }
+        pop() {
+            if (this.h.length > 1) {
+                const temp = this.h[0]
+                this.h[0] = this.h.pop()
+                this.heapify(0)
+                return temp
+            } else {
+                return this.h.pop()
+            }
+        }
+        heapify(p) {
+            let l = 2 * p + 1
+            let r = 2 * p + 2
+            while ((l in this.h && this.compare(this.h[p], this.h[l])) || (r in this.h && this.compare(this.h[p], this.h[r]))) {
+                let t = p
+                if (l in this.h && this.compare(this.h[t], this.h[l])) t = l
+                if (r in this.h && this.compare(this.h[t], this.h[r])) t = r
+                if (t !== p) {
+                    this.swap(t, p)
+                    p = t
+                    l = 2 * p + 1
+                    r = 2 * p + 2
+                }
+            }
+        }
+        swap(i, j) {
+            [this.h[i], this.h[j]] = [this.h[j], this.h[i]]
+        }
+        subtractMin() {
+            const min = this.h[0]
+            this.h = this.h.map(n => n - min)
+        }
+    }
+    const heap = new Heap((a, b) => a > b)
+    for (const n of nums) {
+        heap.insert(n)
+    }
+    let res = 0
+    while (heap.h.length) {
+        if (heap.h[0] === 0) {
+            heap.pop()
+        } else {
+            heap.subtractMin()
+            res++
+        }
+    }
+    return res
+}
