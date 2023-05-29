b8275d3d57963a7f01352ffc84363e52640b51ba
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-29 15:18:55 -0700

    Time: 58 ms (50.95%), Space: 17.9 MB (43.68%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.py
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.py
@@ -6,13 +6,33 @@
 #         self.right = right
 class Solution:
     def tree2str(self, root: Optional[TreeNode]) -> str:
+        # if not root:
+        #     return ""
+        # res = "" + str(root.val)
+        # if root.left:
+        #     res += f"({self.tree2str(root.left)})"
+        # if root.right:
+        #     if not root.left:
+        #         res += "()"
+        #     res += f"({self.tree2str(root.right)})"
+        # return res
+        
         if not root:
             return ""
-        res = "" + str(root.val)
-        if root.left:
-            res += f"({self.tree2str(root.left)})"
-        if root.right:
-            if not root.left:
-                res += "()"
-            res += f"({self.tree2str(root.right)})"
-        return res
+        stack, res, seen = [], "", set()
+        stack.append(root)
+        while stack:
+            node = stack[-1]
+            if node in seen:
+                stack.pop()
+                res += ")"
+            else:
+                seen.add(node)
+                res += "(" + str(node.val)
+                if not node.left and node.right:
+                    res += "()"
+                if node.right:
+                    stack.append(node.right)
+                if node.left:
+                    stack.append(node.left)
+        return res[1:-1:]
