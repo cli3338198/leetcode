f8c25ead9e57c95ee85cd978da238b18dbd91523
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-29 10:56:50 -0700

    Time: 212 ms (67.77%), Space: 50.3 MB (99.31%) - LeetHub

---
+++ 0326-power-of-three/0326-power-of-three.js
@@ -0,0 +1,9 @@
+/**
+ * @param {number} n
+ * @return {boolean}
+ */
+var isPowerOfThree = function(n) {
+    if (n === 1) return true
+    if (n <= 1 || n % 3 !== 0) return false
+    return isPowerOfThree(n / 3)
+};
