148f3174b9bbfde545be05559d7fae936f4c1d39
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-29 15:08:19 -0700

    Time: 76 ms (49.67%), Space: 48.9 MB (5.30%) - LeetHub

--- 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.js
+++ 0606-construct-string-from-binary-tree/0606-construct-string-from-binary-tree.js
@@ -44,3 +44,31 @@ tree2str = function(root) {
     }
     return str + "(" + tree2str(root.left) + ")" + "(" + tree2str(root.right) + ")"
 }
+
+tree2str = function(root) {
+    if (!root) return ""
+    const stack = []
+    const set = new Set()
+    let res = ""
+    stack.push(root)
+    while (stack.length) {
+        const node = stack.at(-1)
+        if (set.has(node)) {
+            stack.pop()
+            res += ")"
+        } else {
+            set.add(node)
+            res += "(" + node.val
+            if (!node.left && node.right) {
+                res += "()"
+            }
+            if (node.right) {
+                stack.push(node.right)
+            }
+            if (node.left) {
+                stack.push(node.left)
+            }
+        }
+    }
+    return res.substring(1, res.length-1)
+}
