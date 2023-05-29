d2a4d34e55f4fba1ab5fca83ef578439bbb853ac
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-29 15:43:59 -0700

    Time: 72 ms (29.23%), Space: 44.9 MB (6.92%) - LeetHub

--- 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.js
+++ 0993-cousins-in-binary-tree/0993-cousins-in-binary-tree.js
@@ -33,3 +33,21 @@ var isCousins = function(root, x, y) {
     }
     return l1 === l2 && p1 !== p2
 };
+
+isCousins = function(root, x, y) {
+    const q = [[root, null]]
+    while (q.length) {
+        const len = q.length
+        let p1 = null
+        let p2 = null
+        for (let i=0; i < len; i++) {
+            const [root, parent] = q.shift()
+            if (root.val === x) p1 = parent
+            if (root.val === y) p2 = parent
+            if (p1 && p2) return p1 !== p2
+            if (root.left) q.push([root.left, root])
+            if (root.right) q.push([root.right, root])
+        }
+    }
+    return false
+}
