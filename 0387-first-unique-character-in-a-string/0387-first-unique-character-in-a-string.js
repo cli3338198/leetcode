dbba4b668b20fc5ffb32c6e909a106fa17df25bf
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-29 11:30:45 -0700

    Time: 73 ms (97.40%), Space: 44.9 MB (85.38%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.js
@@ -18,3 +18,21 @@ var firstUniqChar = function(s) {
     }
     return -1
 };
+
+firstUniqChar = function(s) {
+    const abc = Array(26).fill(-Infinity)
+    for (let i=0; i < s.length; i++) {
+        const c = s[i]
+        const idx = c.charCodeAt(0) - 97
+        if (abc[idx] === -Infinity) {
+            abc[idx] = i
+        } else {
+            abc[idx] = -1
+        }
+    }
+    for (const c of s) {
+        const idx = c.charCodeAt(0) - 97
+        if (abc[idx] >= 0) return abc[idx]
+    }
+    return -1
+}
