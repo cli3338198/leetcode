ca5b38adbec329f00d2045725905aa2f4d7bb7d3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-29 11:58:13 -0700

    Time: 67 ms (51.02%), Space: 44.9 MB (32.24%) - LeetHub

---
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var sumOfLeftLeaves = function(root) {
+    if (!root) return 0
+    let sum = 0
+    if (root.left && !root.left.left && !root.left.right) {
+        sum += root.left.val
+    }
+    return sum + sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right)
+};
