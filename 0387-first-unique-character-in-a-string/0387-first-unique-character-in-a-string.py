30d61604a153838239bebb511a598d74c8b70f21
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-29 11:39:21 -0700

    Time: 89 ms (87.77%), Space: 16.7 MB (10.75%) - LeetHub

--- 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
+++ 0387-first-unique-character-in-a-string/0387-first-unique-character-in-a-string.py
@@ -1,12 +1,21 @@
 class Solution:
     def firstUniqChar(self, s: str) -> int:
-        abc = [-100] * 26
+        # abc = [-100] * 26
+        # for i, c in enumerate(s):
+        #     if abc[ord(c)-97] == -100:
+        #         abc[ord(c)-97] = i
+        #     else:
+        #         abc[ord(c)-97] = -1
+        # for c in s:
+        #     if abc[ord(c)-97] >= 0:
+        #         return abc[ord(c)-97]
+        # return -1
+        
+        chars = Counter(list(s))
+        singles = [c for c, v in chars.items() if v == 1]
+        if not singles:
+            return -1
         for i, c in enumerate(s):
-            if abc[ord(c)-97] == -100:
-                abc[ord(c)-97] = i
-            else:
-                abc[ord(c)-97] = -1
-        for c in s:
-            if abc[ord(c)-97] >= 0:
-                return abc[ord(c)-97]
-        return -1
+            if c in singles:
+                return i
+            
