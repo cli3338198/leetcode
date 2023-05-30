e4b4ef258b86c1a8980264dee4835233e2c36b99
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-30 12:48:20 -0700

    Time: 143 ms (90.77%), Space: 63.5 MB (32.31%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.js
@@ -0,0 +1,41 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var balanceBST = function(root) {
+    const arr = dfs(root)
+    return helper(0, arr.length-1)
+    
+    function helper(lo, hi) {
+        if (lo > hi) return null
+        const mid = Math.floor((lo+hi)/2)
+        const root = new TreeNode(arr[mid])
+        root.left = helper(lo, mid-1)
+        root.right = helper(mid+1, hi)
+        return root
+    }
+    
+    function dfs(cur) {
+        const res = []
+        const stack = []
+        while (cur || stack.length) {
+            if (cur) {
+                stack.push(cur)
+                cur = cur.left
+            } else {
+                cur = stack.pop()
+                res.push(cur.val)
+                cur = cur.right
+            }
+        }
+        return res
+    }
+};
