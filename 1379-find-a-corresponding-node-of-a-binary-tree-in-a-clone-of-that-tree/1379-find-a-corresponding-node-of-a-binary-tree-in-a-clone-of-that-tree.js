6ce88717a24b6c3879187c52f3f5c69a758c1b7d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-30 13:11:36 -0700

    Time: 271 ms (77.01%), Space: 88 MB (5.36%) - LeetHub

--- 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
+++ 1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree/1379-find-a-corresponding-node-of-a-binary-tree-in-a-clone-of-that-tree.js
@@ -21,3 +21,18 @@ var getTargetCopy = function(original, cloned, target) {
         return find(cur.left) || find(cur.right)
     }
 };
+
+getTargetCopy = function(original, cloned, target) {
+    return bfs(cloned)
+    
+    function bfs(root) {
+        const stack = [root]
+        while (stack.length) {
+            const node = stack.shift()
+            if (node.val === target.val) return node
+            if (node.left) stack.push(node.left)
+            if (node.right) stack.push(node.right)
+        }
+        return null
+    }
+}
