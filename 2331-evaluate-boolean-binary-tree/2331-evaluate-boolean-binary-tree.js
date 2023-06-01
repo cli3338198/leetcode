f9aeb6ecec6e9f015a00609c6f367126e14f2ecc
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-01 09:51:13 -0700

    Time: 80 ms (22.14%), Space: 48.4 MB (84.29%) - LeetHub

--- 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
+++ 2331-evaluate-boolean-binary-tree/2331-evaluate-boolean-binary-tree.js
@@ -19,3 +19,47 @@ var evaluateTree = function(root) {
         return evaluateTree(root.left) && evaluateTree(root.right)
     }
 };
+
+evaluateTree = function(root) {
+    if (!root.left || !root.right) return root.val === 0 ? false : true
+    const original = root
+    const stack = []
+    const map = {}
+    let prev = null
+    while (root || stack.length) {
+        if (root) {
+            stack.push(root)
+            root = root.left
+        } else {
+            root = stack.at(-1)
+            if (root.right && root.right !== prev) {
+                root = root.right
+            } else {
+                if (!root.left && !root.right) {
+                    map[root] = root.val
+                } else if (root.val === 2) {
+                    map[root] = map[root.left] || map[root.right]
+                } else {
+                    map[root] = map[root.left] && map[root.right]
+                }
+                stack.pop()
+                prev = root
+                root = null
+            }
+        }
+    }
+    return map[original]
+}
+
+evaluateTree = function(root) {
+    switch (root.val) {
+        case 1:
+            return true
+        case 2:
+            return evaluateTree(root.left) || evaluateTree(root.right)
+        case 3:
+            return evaluateTree(root.left) && evaluateTree(root.right)
+        default:
+            return false
+    }
+}
