1731ca47400481aeb8515f8d2f30d6c498f4c7b9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-01 10:02:54 -0700

    Time: 69 ms (9.00%), Space: 43.1 MB (30.00%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.js
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.js
@@ -19,3 +19,10 @@ var isUnivalTree = function(root) {
         return dfs(root.left, val) && dfs(root.right, val)
     }
 };
+
+isUnivalTree = function(root) {
+    if (!root) return true
+    if (root.left && root.left.val !== root.val) return false
+    if (root.right && root.right.val !== root.val) return false
+    return isUnivalTree(root.left) && isUnivalTree(root.right)
+}
