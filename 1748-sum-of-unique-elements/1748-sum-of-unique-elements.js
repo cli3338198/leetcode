739a12f0e07173a307e684bd325e1fab01252a21
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-05-31 20:36:03 -0700

    Time: 59 ms (62.33%), Space: 42.3 MB (53.81%) - LeetHub

--- 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
+++ 1748-sum-of-unique-elements/1748-sum-of-unique-elements.js
@@ -5,3 +5,19 @@
 var sumOfUnique = function(nums) {
     return Object.entries(nums.reduce((acc, n) => ({...acc, [n]: acc[n] + 1 || 1}), {})).filter(([,cnt]) => cnt === 1).reduce((acc, [n]) => acc + +n, 0)
 };
+
+sumOfUnique = function(nums) {
+    const yes = new Set()
+    const no = new Set()
+    for (const n of nums) {
+        if (no.has(n)) {
+            continue
+        } else if (yes.has(n)) {
+            yes.delete(n)
+            no.add(n)
+        } else {
+            yes.add(n)
+        }
+    }
+    return Array.from(yes).reduce((acc, val) => acc + val, 0)
+}
