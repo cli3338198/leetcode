5c637f7a146d1616109a2fa8a798ace74f43a4b9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-02 09:56:07 -0700

    Time: 175 ms (66.67%), Space: 59.2 MB (59.37%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.js
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.js
@@ -23,3 +23,39 @@ var getAllElements = function(root1, root2) {
         ]
     }
 };
+
+getAllElements = function(root1, root2) {
+    const a = dfs(root1)
+    const b = dfs(root2)
+    return merge(a, b)
+    
+    function merge(a, b) {
+        const res = []
+        while (a.length && b.length) {
+            if (a[0] < b[0]) {
+                res.push(a.shift())
+            } else {
+                res.push(b.shift())
+            }
+        }
+        res.push(...a)
+        res.push(...b)
+        return res
+    }
+    
+    function dfs(root) {
+        const stack = []
+        const res = []
+        while (root || stack.length) {
+            if (root) {
+                stack.push(root)
+                root = root.left
+            } else {
+                root = stack.pop()
+                res.push(root.val)
+                root = root.right
+            }
+        }
+        return res
+    }
+}
