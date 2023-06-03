2af7a29b87259f1acb814168bda6acbb33f3cbbb
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-02 21:25:35 -0700

    Time: 59 ms (71.48%), Space: 44.2 MB (28.34%) - LeetHub

--- 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
+++ 1572-matrix-diagonal-sum/1572-matrix-diagonal-sum.js
@@ -13,3 +13,17 @@ var diagonalSum = function(mat) {
     if (mat.length % 2) res -= mat[Math.floor(mat.length/2)][Math.floor(mat.length/2)]
     return res
 };
+
+diagonalSum = function(mat) {
+    let left = 0, right = mat[0].length-1, res = 0
+    for (let r=0; r < mat.length; r++) {
+        if (left === right) {
+            res += mat[r][left]
+        } else {
+            res += mat[r][left] + mat[r][right]   
+        }
+        left++
+        right--
+    }
+    return res
+}
