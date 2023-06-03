178814dbee07aa8dd542a49da0abe08acd63b9bb
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-02 19:31:05 -0700

    Time: 90 ms (5.26%), Space: 48.7 MB (5.26%) - LeetHub

--- 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
+++ 2206-divide-array-into-equal-pairs/2206-divide-array-into-equal-pairs.js
@@ -3,5 +3,5 @@
  * @return {boolean}
  */
 var divideArray = function(nums) {
-    return Object.entries(nums.reduce((acc, val) => ({...acc, [val]: acc[val] + 1 || 1}), {})).every(([,x]) => x % 2 === 0)
+    return Object.values(nums.reduce((acc, val) => ({...acc, [val]: acc[val] + 1 || 1}), {})).every((x) => x % 2 === 0)
 };
