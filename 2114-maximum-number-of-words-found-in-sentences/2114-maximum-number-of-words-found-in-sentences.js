063d9ece1336211451f7550768ec9d27b200cfac
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-03 20:11:37 -0700

    Time: 69 ms (26.27%), Space: 44.6 MB (21.26%) - LeetHub

---
+++ 2114-maximum-number-of-words-found-in-sentences/2114-maximum-number-of-words-found-in-sentences.js
@@ -0,0 +1,15 @@
+/**
+ * @param {string[]} sentences
+ * @return {number}
+ */
+var mostWordsFound = function(sentences) {
+    let res = 0
+    for (const sentence of sentences) {
+        let cur = 0
+        for (const c of sentence) {
+            if (c === " ") cur++
+        }
+        res = Math.max(res, cur + 1)
+    }
+    return res
+};
