f73bb0d402335dcbccdb65b586b10465ada01327
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-04 10:28:14 -0700

    Time: 76 ms (35.30%), Space: 43.3 MB (56.55%) - LeetHub

--- 2629-function-composition/2629-function-composition.js
+++ 2629-function-composition/2629-function-composition.js
@@ -12,6 +12,12 @@ var compose = function(functions) {
     }
 };
 
+compose = function(functions) {
+    return function(x) {
+        return functions.reduceRight((acc, fn) => fn(acc), x)
+    }
+}
+
 /**
  * const fn = compose([x => x + 1, x => 2 * x])
  * fn(4) // 9
