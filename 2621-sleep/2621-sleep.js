feb01020786668e68df46e466d8eb6a6e54ab2a5
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-04 10:55:37 -0700

    Time: 63 ms (37.07%), Space: 42.2 MB (7.17%) - LeetHub

--- 2621-sleep/2621-sleep.js
+++ 2621-sleep/2621-sleep.js
@@ -9,6 +9,13 @@ async function sleep(millis) {
     }).then(res => res)
 }
 
+sleep = async function(millis) {
+    const p = await new Promise((res, rej) => {
+        setTimeout(res, millis)
+    })
+    return p
+}
+
 /** 
  * let t = Date.now()
  * sleep(100).then(() => console.log(Date.now() - t)) // 100
