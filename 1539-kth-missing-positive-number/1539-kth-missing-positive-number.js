f4e87262f9561bc041de8f8c252c7534cb987466
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-03 20:41:41 -0700

    Time: 47 ms (97.41%), Space: 42.6 MB (51.29%) - LeetHub

---
+++ 1539-kth-missing-positive-number/1539-kth-missing-positive-number.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} arr
+ * @param {number} k
+ * @return {number}
+ */
+var findKthPositive = function(arr, k) {
+    let left = 0, right = arr.length-1
+    while (left <= right) {
+        const mid = Math.floor((left+right)/2)
+        if (arr[mid] - mid - 1 < k) {
+            left = mid + 1
+        } else {
+            right = mid - 1
+        }
+    }
+    return left + k
+};
