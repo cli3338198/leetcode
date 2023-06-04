5ac222140976152be4c1dbc5f050b46ab8918457
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-04 09:59:31 -0700

    Time: 78 ms (27.47%), Space: 45.3 MB (74.04%) - LeetHub

---
+++ 2665-counter-ii/2665-counter-ii.ts
@@ -0,0 +1,21 @@
+type ReturnObj = {
+    increment: () => number,
+    decrement: () => number,
+    reset: () => number,
+}
+
+function createCounter(init: number): ReturnObj {
+    let cur = init
+    return {
+        increment: () => ++cur,
+        decrement: () => --cur,
+        reset: () => cur = init
+    }
+};
+
+/**
+ * const counter = createCounter(5)
+ * counter.increment(); // 6
+ * counter.reset(); // 5
+ * counter.decrement(); // 4
+ */
