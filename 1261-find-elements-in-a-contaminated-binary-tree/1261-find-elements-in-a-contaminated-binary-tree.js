b6ab3b4cc5ef49cccaec1434a9b8caf8c2a863f4
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-04 21:14:32 -0700

    Time: 83 ms (100.00%), Space: 51.5 MB (45.45%) - LeetHub

--- 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.js
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.js
@@ -9,34 +9,52 @@
 /**
  * @param {TreeNode} root
  */
-var FindElements = function(root) {
-    this.root = this.recover(root, 0)
-};
+// var FindElements = function(root) {
+//     this.root = this.recover(root, 0)
+// };
 
-FindElements.prototype.recover = function(root, x) {
-    if (!root) return null
-    root.val = x
-    root.left = this.recover(root.left, 2 * x + 1)
-    root.right = this.recover(root.right, 2 * x + 2)
-    return root
-}
+// FindElements.prototype.recover = function(root, x) {
+//     if (!root) return null
+//     root.val = x
+//     root.left = this.recover(root.left, 2 * x + 1)
+//     root.right = this.recover(root.right, 2 * x + 2)
+//     return root
+// }
 
 /** 
  * @param {number} target
  * @return {boolean}
  */
-FindElements.prototype.find = function(target) {
-    return dfs(this.root, target)
+// FindElements.prototype.find = function(target) {
+//     return dfs(this.root, target)
     
-    function dfs(root, target) {
-        if (!root) return false
-        if (root.val === target) return true
-        return dfs(root.left, target) || dfs(root.right, target)
-    }
-};
+//     function dfs(root, target) {
+//         if (!root) return false
+//         if (root.val === target) return true
+//         return dfs(root.left, target) || dfs(root.right, target)
+//     }
+// };
 
 /** 
  * Your FindElements object will be instantiated and called as such:
  * var obj = new FindElements(root)
  * var param_1 = obj.find(target)
  */
+
+class FindElements {
+    constructor(root) {
+        this.set = new Set()
+        this.root = this.recover(root, 0)
+    }
+    recover(root, x) {
+        if (!root) return null
+        root.val = x
+        this.set.add(x)
+        root.left = this.recover(root.left, 2 * x + 1)
+        root.right = this.recover(root.right, 2 * x + 2)
+        return root
+    }
+    find(target) {
+        return this.set.has(target)
+    }
+}
