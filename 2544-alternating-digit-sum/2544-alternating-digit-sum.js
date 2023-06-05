e70f9ae2b4bf2eaf251ad872825e18e4df7da99d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-04 20:12:54 -0700

    Time: 67 ms (15.32%), Space: 41.9 MB (78.23%) - LeetHub

--- 2544-alternating-digit-sum/2544-alternating-digit-sum.js
+++ 2544-alternating-digit-sum/2544-alternating-digit-sum.js
@@ -19,3 +19,17 @@ var alternateDigitSum = function(n) {
         return res
     }
 };
+
+alternateDigitSum = function(n) {
+    const arr = []
+    while (n) {
+        arr.push(n % 10)
+        n = Math.floor(n / 10)
+    }
+    let sign = 1, res = 0
+    for (let i=arr.length-1; i >= 0; i--) {
+        res += sign * arr[i]
+        sign *= -1
+    }
+    return res
+}
