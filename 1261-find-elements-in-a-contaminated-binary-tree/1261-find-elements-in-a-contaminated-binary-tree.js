ab3c6c47ba143ce2ee413f6b22f800792e3b1e2d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-04 21:10:45 -0700

    Time: 2616 ms (6.82%), Space: 51.1 MB (77.27%) - LeetHub

---
+++ 1261-find-elements-in-a-contaminated-binary-tree/1261-find-elements-in-a-contaminated-binary-tree.js
@@ -0,0 +1,42 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ */
+var FindElements = function(root) {
+    this.root = this.recover(root, 0)
+};
+
+FindElements.prototype.recover = function(root, x) {
+    if (!root) return null
+    root.val = x
+    root.left = this.recover(root.left, 2 * x + 1)
+    root.right = this.recover(root.right, 2 * x + 2)
+    return root
+}
+
+/** 
+ * @param {number} target
+ * @return {boolean}
+ */
+FindElements.prototype.find = function(target) {
+    return dfs(this.root, target)
+    
+    function dfs(root, target) {
+        if (!root) return false
+        if (root.val === target) return true
+        return dfs(root.left, target) || dfs(root.right, target)
+    }
+};
+
+/** 
+ * Your FindElements object will be instantiated and called as such:
+ * var obj = new FindElements(root)
+ * var param_1 = obj.find(target)
+ */
