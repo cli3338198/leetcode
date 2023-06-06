b211d02664a5600378809ad32a2ff89c8b5d579b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-06 14:03:55 -0700

    Time: 932 ms (84.26%), Space: 16.9 MB (11.11%) - LeetHub

--- 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
+++ 2643-row-with-maximum-ones/2643-row-with-maximum-ones.py
@@ -1,6 +1,14 @@
 class Solution:
     def rowAndMaximumOnes(self, mat: List[List[int]]) -> List[int]:
         res = [None, 0]
+        for r, row in enumerate(mat):
+            sm = sum(row)
+            if res[0] == None or sm > res[1]:
+                res[0] = r
+                res[1] = sm
+        return res
+        
+        res = [None, 0]
         for r in range(0, len(mat)):
             sm = 0
             for c in range(0, len(mat[r])):
