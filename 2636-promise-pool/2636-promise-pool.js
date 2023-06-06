21a46c3312393c95ad1070163c6b44857d204bd6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-05 20:56:29 -0700

    Time: 70 ms (26.87%), Space: 42.1 MB (61.64%) - LeetHub

---
+++ 2636-promise-pool/2636-promise-pool.js
@@ -0,0 +1,19 @@
+/**
+ * @param {Function[]} functions
+ * @param {number} n
+ * @return {Function}
+ */
+var promisePool = async function(functions, n) {
+    await Promise.all(functions.splice(0, n).map(fn => call(fn)))
+    
+    async function call(fn) {
+        await fn()
+        if (functions.length) await call(functions.shift())
+    }
+};
+
+/**
+ * const sleep = (t) => new Promise(res => setTimeout(res, t));
+ * promisePool([() => sleep(500), () => sleep(400)], 1)
+ *   .then(console.log) // After 900ms
+ */
