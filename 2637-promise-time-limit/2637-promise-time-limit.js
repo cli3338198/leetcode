f4fc50d850d5fd944a9cee84359bf6604791ba74
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-05 20:38:49 -0700

    Time: 70 ms (12.90%), Space: 41.4 MB (96.13%) - LeetHub

--- 2637-promise-time-limit/2637-promise-time-limit.js
+++ 2637-promise-time-limit/2637-promise-time-limit.js
@@ -6,20 +6,11 @@
 var timeLimit = function(fn, t) {
 	return async function(...args) {
         return new Promise((resolve, reject) => {
-            setTimeout(() => {
-                reject("Time Limit Exceeded")
-            }, t)
+            setTimeout(() => reject("Time Limit Exceeded"), t)
             fn(...args).then(resolve).catch(reject)
         })
     }
 };
-
-timeLimit = function(fn, t) {
-    return async function(...args) {
-        const timeout = new Promise((resolve, reject) => setTimeout(() => reject("Time Limit Exceeded"), t))
-        return Promise.race([fn(...args), timeout])
-    }
-}
 
 /**
  * const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
