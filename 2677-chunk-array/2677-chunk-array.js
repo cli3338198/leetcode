19f90aae167d7775865a87009955b89bc61d400a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-05 20:33:58 -0700

    Time: 62 ms (65.25%), Space: 44 MB (88.68%) - LeetHub

--- 2677-chunk-array/2677-chunk-array.js
+++ 2677-chunk-array/2677-chunk-array.js
@@ -33,3 +33,11 @@ chunk = function(arr, size) {
         return acc
     }, [])
 }
+
+chunk = function(arr, size) {
+    const res = []
+    for (let i=0; i < arr.length; i += size) {
+        res.push(arr.slice(i, i+size))
+    }
+    return res
+}
