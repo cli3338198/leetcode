ee0e060b7c1fadb6bf954e2ed583cb2435d34786
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-07 20:17:15 -0700

    Time: 82 ms (35.42%), Space: 44.2 MB (75.00%) - LeetHub

---
+++ 2309-greatest-english-letter-in-upper-and-lower-case/2309-greatest-english-letter-in-upper-and-lower-case.js
@@ -0,0 +1,19 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var greatestLetter = function(s) {
+    const set = new Set()
+    let res = null
+    for (const c of s) {
+        set.add(c)
+        const upper = c.toUpperCase()
+        const lower = c.toLowerCase()
+        if (set.has(upper) && set.has(lower)) {
+            if (res === null || res < upper) {
+                res = upper
+            } 
+        }
+    }
+    return res !== null ? res : ""
+};
