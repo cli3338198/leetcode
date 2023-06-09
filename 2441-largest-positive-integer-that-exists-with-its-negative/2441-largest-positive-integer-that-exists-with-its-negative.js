a29919331f00647a98d3ae1ce727a3944f85533d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-08 20:12:52 -0700

    Time: 62 ms (97.06%), Space: 45.4 MB (47.55%) - LeetHub

---
+++ 2441-largest-positive-integer-that-exists-with-its-negative/2441-largest-positive-integer-that-exists-with-its-negative.js
@@ -0,0 +1,14 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var findMaxK = function(nums) {
+    nums = new Set(nums)
+    let res = -Infinity
+    for (const n of nums) {
+        if (nums.has(-n)) {
+            res = Math.max(res, Math.abs(n))
+        }
+    }
+    return res === -Infinity ? -1 : res
+};
