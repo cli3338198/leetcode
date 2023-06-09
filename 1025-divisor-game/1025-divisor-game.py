d41aaf9a4503a081aaa7cf98294a9db37ed43cc2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-08 20:39:41 -0700

    Time: 114 ms (6.84%), Space: 16.4 MB (17.13%) - LeetHub

--- 1025-divisor-game/1025-divisor-game.py
+++ 1025-divisor-game/1025-divisor-game.py
@@ -1,5 +1,15 @@
 class Solution:
     def divisorGame(self, n: int) -> bool:
+        dp = [False] * (n + 1)
+        if len(dp) > 2: 
+            dp[2] = True
+        for m in range(3, n+1):
+            for x in range(1, m):
+                if m % x == 0 and not dp[m - x]:
+                    dp[m] = True
+                    break
+        return dp[n]
+        
         
         @lru_cache(None)
         def helper(n: int) -> bool:
