c6864ac496b779a17d1aab97489790be92b0e945
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-09 20:18:08 -0700

    Time: 77 ms (9.06%), Space: 43.8 MB (20.00%) - LeetHub

--- 1122-relative-sort-array/1122-relative-sort-array.js
+++ 1122-relative-sort-array/1122-relative-sort-array.js
@@ -22,3 +22,44 @@ var relativeSortArray = function(arr1, arr2) {
     })
     return arr1
 };
+
+relativeSortArray = function(arr1, arr2) {
+    const map = {}
+    for (let i=0; i < arr2.length; i++) {
+        map[arr2[i]] = i
+    }
+    return mergeSort(arr1)
+    
+    function mergeSort(arr) {
+        if (arr.length <= 1) return arr
+        const mid = Math.floor(arr.length/2)
+        const left = mergeSort(arr.slice(0, mid))
+        const right = mergeSort(arr.slice(mid))
+        return merge(left, right)
+    }
+    
+    function merge(a, b) {
+        const res = []
+        let i=0, j=0
+        while (i < a.length && j < b.length) {
+            if (a[i] in map && b[j] in map) {
+                if (map[a[i]] < map[b[j]]) {
+                    res.push(a[i++])
+                } else {
+                    res.push(b[j++])
+                }
+            } else if (a[i] in map) {
+                res.push(a[i++])
+            } else if (b[j] in map) {
+                res.push(b[j++])
+            } else if (a[i] < b[j]) {
+                res.push(a[i++])
+            } else {
+                res.push(b[j++])
+            }
+        }
+        for (i; i < a.length; i++) res.push(a[i])
+        for (j; j < b.length; j++) res.push(b[j])
+        return res
+    }
+}
