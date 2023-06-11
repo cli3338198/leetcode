f828a00889f2551e5467fa7104bf700ab5ef6bb9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-10 21:24:21 -0700

    Time: 88 ms (9.52%), Space: 49.8 MB (5.19%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.js
@@ -22,3 +22,29 @@ var findBall = function(grid) {
         }
     }
 };
+
+findBall = function(grid) {
+    const rows = grid.length
+    const cols = grid[0].length
+    const res = Array(cols).fill(-1)
+    const dp = {}
+    for (let c=0; c < cols; c++) {
+        res[c] = dfs(0, c)
+    }
+    return res
+    
+    function dfs(r, c) {
+        const key = `${r} ${c}`
+        if (key in dp) return dp[key]
+        if (r === grid.length) return dp[key] = c
+        if (c === 0 && grid[r][c] === -1) return dp[key] = -1
+        if (c === grid[0].length-1 && grid[r][c] === 1) return dp[key] = -1
+        if (grid[r][c] === 1 && grid[r][c+1] === -1) return dp[key] = -1
+        if (grid[r][c-1] === 1 && grid[r][c] === -1) return dp[key] = -1
+        if (grid[r][c] === 1) {
+            return dp[key] = dfs(r+1, c+1)
+        } else {
+            return dp[key] = dfs(r+1, c-1)
+        }
+    }
+}
