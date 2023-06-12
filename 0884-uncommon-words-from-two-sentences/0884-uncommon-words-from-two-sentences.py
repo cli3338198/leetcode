09ef72fb4884bc512710cb9d4da1e6cea153c164
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-11 20:24:31 -0700

    Time: 50 ms (31.33%), Space: 16.3 MB (56.67%) - LeetHub

--- 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
+++ 0884-uncommon-words-from-two-sentences/0884-uncommon-words-from-two-sentences.py
@@ -1,29 +1,11 @@
-/**
- * @param {string} s1
- * @param {string} s2
- * @return {string[]}
- */
-var uncommonFromSentences = function(s1, s2) {
-    return helper(s1, s2).concat(helper(s2, s1))
-    
-    function helper(s1, s2) {
-        s1 = s1.split(" ")
-        const map = {}
-        for (const word of s1) {
-            map[word] = map[word] + 1 || 1
-        }
-        const set = new Set(s2.split(" "))
-        return Object.entries(map).reduce((acc, [word, cnt]) => cnt === 1 && !set.has(word) ? [...acc, word] : acc, [])
-    }
-};
-
-uncommonFromSentences = function(s1, s2) {
-    const map = {}, res = []
-    for (const word of s1.split(" ")) {
-        map[word] = map[word] + 1 || 1
-    }
-    for (const word of s2.split(" ")) {
-        map[word] = map[word] + 1 || 1
-    }
-    return Object.entries(map).filter(([,cnt]) => cnt === 1).map(([word]) => word)
-}
+class Solution:
+    def uncommonFromSentences(self, s1: str, s2: str) -> List[str]:
+        mp, res = {}, []
+        for word in s1.split(" "):
+            mp[word] = mp.get(word, 0) + 1
+        for word in s2.split(" "):
+            mp[word] = mp.get(word, 0) + 1
+        for word, cnt in mp.items():
+            if cnt == 1:
+                res.append(word)
+        return res
