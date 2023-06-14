1aa85ae91431a84db63987747d288490ae40a484
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-13 21:40:20 -0700

    Time: 365 ms (8.36%), Space: 79 MB (20.58%) - LeetHub

--- 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.js
+++ 1557-minimum-number-of-vertices-to-reach-all-nodes/1557-minimum-number-of-vertices-to-reach-all-nodes.js
@@ -10,3 +10,44 @@ var findSmallestSetOfVertices = function(n, edges) {
     }
     return Array.from(nodes)
 };
+
+findSmallestSetOfVertices = function(n, edges) {
+    class UF {
+        constructor() {
+            this.root = {}
+        }
+        add(n) {
+            if (!(n in this.root)) {
+                this.root[n] = n
+            }
+        }
+        find(n) {
+            this.add(n)
+            if (this.root[n] !== n) {
+                this.root[n] = this.find(this.root[n])
+            }
+            return this.root[n]
+        }
+        union(a, b) {
+            const A = this.find(a)
+            const B = this.find(b)
+            if (A !== B) {
+                this.root[b] = A
+            }
+        }
+        getRoots() {
+            const res = []
+            for (const node in this.root) {
+                if (this.root[node] == node) {
+                    res.push(this.root[node])
+                }
+            }
+            return res
+        }
+    }
+    const uf = new UF()
+    for (const [u, v] of edges) {
+        uf.union(u, v)
+    }
+    return uf.getRoots()
+}
