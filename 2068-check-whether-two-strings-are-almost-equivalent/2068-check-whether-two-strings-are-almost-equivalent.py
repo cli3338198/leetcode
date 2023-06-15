51b57ea9e5e97458f6c339c319fb208b908d6ce4
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-14 20:10:58 -0700

    Time: 45 ms (59.82%), Space: 16.3 MB (61.42%) - LeetHub

---
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -0,0 +1,8 @@
+class Solution:
+    def checkAlmostEquivalent(self, word1: str, word2: str) -> bool:
+        chars = [0] * 26
+        for c in word1:
+            chars[ord(c)-97] += 1
+        for c in word2:
+            chars[ord(c)-97] -= 1
+        return sum([1 for cnt in chars if abs(cnt) <= 3]) == 26
