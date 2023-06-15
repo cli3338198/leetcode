62b907edb86940ca823510922fe88b524f709880
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-14 20:12:52 -0700

    Time: 59 ms (6.68%), Space: 16.4 MB (24.23%) - LeetHub

--- 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
+++ 2068-check-whether-two-strings-are-almost-equivalent/2068-check-whether-two-strings-are-almost-equivalent.py
@@ -5,4 +5,11 @@ class Solution:
             chars[ord(c)-97] += 1
         for c in word2:
             chars[ord(c)-97] -= 1
+        return max(map(lambda x: abs(x), chars)) <= 3
+        
+        chars = [0] * 26
+        for c in word1:
+            chars[ord(c)-97] += 1
+        for c in word2:
+            chars[ord(c)-97] -= 1
         return sum([1 for cnt in chars if abs(cnt) <= 3]) == 26
