5da247928bdba15df721377a8ac8d1e5b05f8327
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-14 20:53:42 -0700

    Time: 44 ms (63.67%), Space: 16.2 MB (57.35%) - LeetHub

--- 2490-circular-sentence/2490-circular-sentence.py
+++ 2490-circular-sentence/2490-circular-sentence.py
@@ -3,12 +3,9 @@ class Solution:
         if sentence[0] != sentence[-1]:
             return False
         sentence = sentence.split(" ")
-        if len(sentence) == 1:
-            return sentence[0][0] == sentence[0][-1]
-        else:
-            for i in range(0, len(sentence)):
-                cur_word = sentence[i]
-                next_word = sentence[i+1] if i+1 < len(sentence) else sentence[0]
-                if cur_word[-1] != next_word[0]:
-                    return False
-            return True
+        for i in range(0, len(sentence)):
+            cur_word = sentence[i]
+            next_word = sentence[i+1] if i+1 < len(sentence) else sentence[0]
+            if cur_word[-1] != next_word[0]:
+                return False
+        return True
