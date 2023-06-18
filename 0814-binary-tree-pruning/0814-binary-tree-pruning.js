f666f9ad12b607d9d91ca3d6e631850794732a6c
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-17 21:07:00 -0700

    Time: 73 ms (5.13%), Space: 44.3 MB (5.13%) - LeetHub

---
+++ 0814-binary-tree-pruning/0814-binary-tree-pruning.js
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var pruneTree = function(root) {
+    return dfs(root)[0]
+    
+    function dfs(root) {
+        if (!root) return [root, false]
+        const [left, leftHasOne] = dfs(root.left)
+        const [right, rightHasOne] = dfs(root.right)
+        root.left = leftHasOne ? left : null
+        root.right = rightHasOne ? right : null
+        if (root.val === 1 || root.left || root.right) {
+            return [root, true]
+        } else {
+            return [null, false]
+        }
+    }
+};
