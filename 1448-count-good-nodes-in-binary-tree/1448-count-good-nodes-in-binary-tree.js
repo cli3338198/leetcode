4d37d22eaa3cc1e7a01921a635bd9c00ab18d7f9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-17 20:46:18 -0700

    Time: 138 ms (72.43%), Space: 69.8 MB (74.49%) - LeetHub

---
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var goodNodes = function(root) {
+    return dfs(root, root.val)
+    
+    function dfs(root, max) {
+        if (!root) return 0
+        max = Math.max(max, root.val)
+        const left = dfs(root.left, max)
+        const right = dfs(root.right, max)
+        return (root.val >= max ? 1 : 0) + left + right
+    }
+};
