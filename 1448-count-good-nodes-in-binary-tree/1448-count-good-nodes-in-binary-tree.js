c2e9747f7444b74ed9e39ca9bde982fbfc5438f4
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-17 20:48:31 -0700

    Time: 153 ms (30.79%), Space: 70.2 MB (46.04%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.js
@@ -21,3 +21,13 @@ var goodNodes = function(root) {
         return (root.val >= max ? 1 : 0) + left + right
     }
 };
+
+goodNodes = function(root) {
+    return dfs(root, root.val)
+    
+    function dfs(root, max) {
+        if (!root) return 0
+        max = Math.max(root.val, max)
+        return (root.val >= max ? 1 : 0) + dfs(root.left, max) + dfs(root.right, max)
+    }
+}
