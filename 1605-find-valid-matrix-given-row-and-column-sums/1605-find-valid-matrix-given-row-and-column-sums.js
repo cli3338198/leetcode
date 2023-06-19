57bf477565066e00a94e1b91717241ffc1e0023a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-18 21:57:49 -0700

    Time: 355 ms (15.38%), Space: 66.8 MB (15.38%) - LeetHub

---
+++ 1605-find-valid-matrix-given-row-and-column-sums/1605-find-valid-matrix-given-row-and-column-sums.js
@@ -0,0 +1,46 @@
+/**
+ * @param {number[]} rowSum
+ * @param {number[]} colSum
+ * @return {number[][]}
+ */
+var restoreMatrix = function(rowSum, colSum) {
+    const rows = rowSum.length
+    const cols = colSum.length
+    const mat = Array(rows).fill(0).map(() => Array(cols).fill(0))
+    while (rowSum.some(x => x > 0) || colSum.some(x => x > 0)) {
+        const [rMin, rIdx] = getMin(rowSum)
+        const [cMin, cIdx] = getMin(colSum)
+        if (rMin < cMin) {
+            rowSum[rIdx] -= rMin
+            for (let c=0; c < colSum.length; c++) {
+                if (colSum[c] >= rMin) {
+                    mat[rIdx][c] = rMin
+                    colSum[c] -= rMin
+                    break
+                }
+            }
+        } else {
+            colSum[cIdx] -= cMin
+            for (let r=0; r < rowSum.length; r++) {
+                if (rowSum[r] >= cMin) {
+                    mat[r][cIdx] = cMin
+                    rowSum[r] -= cMin
+                    break
+                }
+            }
+        }
+    }
+    return mat
+    
+    function getMin(arr) {
+        let min = Infinity
+        let idx = null
+        for (let i=0; i < arr.length; i++) {
+            if (arr[i] > 0 && arr[i] < min) {
+                min = arr[i]
+                idx = i
+            }
+        }
+        return [min, idx]
+    }
+};
