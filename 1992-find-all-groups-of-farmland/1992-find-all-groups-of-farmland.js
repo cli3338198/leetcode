355265d2deced60ba58a719d851bacce1639270f
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-20 15:59:14 -0700

    Time: 430 ms (10.42%), Space: 100.7 MB (29.17%) - LeetHub

---
+++ 1992-find-all-groups-of-farmland/1992-find-all-groups-of-farmland.js
@@ -0,0 +1,34 @@
+/**
+ * @param {number[][]} land
+ * @return {number[][]}
+ */
+var findFarmland = function(land) {
+    const rows = land.length
+    const cols = land[0].length
+    const seen = new Set()
+    const res = []
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < cols; c++) {
+            const key = `${r} ${c}`
+            if (land[r][c] === 1 && !seen.has(key)) {
+                res.push(dfs(r, c, [r, c, r, c]))
+            }
+        }
+    }
+    return res
+    
+    function dfs(r, c, coords) {
+        const key = `${r} ${c}`
+        if (r in land && c in land[r] && land[r][c] === 1 && !seen.has(key)) {
+            seen.add(key)
+            coords[2] = Math.max(coords[2], r)
+            coords[3] = Math.max(coords[3], c)
+            for (const [rr, cc] of [[0, 1], [1, 0]]) {
+                const rrr = r + rr
+                const ccc = c + cc
+                dfs(rrr, ccc, coords)
+            }
+        }
+        return coords
+    }
+};
