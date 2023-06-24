b81053e56d15394dea25119772f18119eff65eb8
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-23 22:41:33 -0700

    Time: 341 ms (5.35%), Space: 51.7 MB (5.35%) - LeetHub

--- 1254-number-of-closed-islands/1254-number-of-closed-islands.js
+++ 1254-number-of-closed-islands/1254-number-of-closed-islands.js
@@ -31,5 +31,66 @@ var closedIsland = function(grid) {
         flip(r, c+1)
         flip(r, c-1)
     }
-
 };
+
+closedIsland = function(grid) {
+    class UF {
+        constructor() {
+            this.root = {}
+            this.islands = 0
+        }
+        add(a) {
+            if (!(a in this.root)) {
+                this.root[a] = a
+                this.islands++
+            }
+        }
+        find(a) {
+            this.add(a)
+            if (this.root[a] !== a) {
+                this.root[a] = this.find(this.root[a])
+            }
+            return this.root[a]
+        }
+        union(a, b) {
+            const A = this.find(a)
+            const B = this.find(b)
+            if (A === B) return
+            this.root[B] = A
+            this.islands--
+        }
+    }
+    const uf = new UF()
+    for (let r=0; r < grid.length; r++) {
+        for (let c=0; c < grid[r].length; c++) {
+            if (r === 0 || r === grid.length-1 || c === 0 || c === grid[r].length-1) {
+                if (grid[r][c] === 0) {
+                    flip(r, c)
+                }
+            }
+        }
+    }
+    for (let r=0; r < grid.length; r++) {
+        for (let c=0; c < grid[r].length; c++) {
+            if (grid[r][c] === 0) {
+                uf.add([r, c])
+                for (const [rr, cc] of [[0, 1], [1, 0], [0, -1], [-1, 0]]) {
+                    const rrr = r + rr
+                    const ccc = c + cc
+                    if (!(rrr in grid) || !(ccc in grid[rrr]) || grid[rrr][ccc] !== 0) continue
+                    uf.union([r, c], [rrr, ccc])
+                }
+            }
+        }
+    }
+    return uf.islands
+    
+    function flip(r, c) {
+        if (!(r in grid) || !(c in grid[r]) || grid[r][c] !== 0) return
+        grid[r][c] = 1
+        flip(r+1, c)
+        flip(r-1, c)
+        flip(r, c+1)
+        flip(r, c-1)
+    }
+}
