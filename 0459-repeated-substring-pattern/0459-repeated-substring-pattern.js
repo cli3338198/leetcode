77e4f6fed41abb87a4ea30ef4d7bcae718b8814c
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-26 20:51:25 -0700

    Time: 60 ms (88.59%), Space: 44.1 MB (96.60%) - LeetHub

---
+++ 0459-repeated-substring-pattern/0459-repeated-substring-pattern.js
@@ -0,0 +1,26 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var repeatedSubstringPattern = function(s) {
+    for (let i=0; i < s.length; i++) {
+        for (let j=i+1; j < s.length; j++) {
+            const substring = s.substring(i, j)
+            if (isSubstring(substring, s)) return true
+        }
+    }
+    return false
+    
+    function isSubstring(substring, string) {
+        const aLen = substring.length
+        const bLen = string.length
+        const multiplier = bLen / aLen
+        return substring.repeat(multiplier) === string
+    }
+};
+
+repeatedSubstringPattern = function(s) {
+    if (s.length === 0) return false
+    const ss = (s + s).substring(1, s.length * 2 - 1)
+    return ss.indexOf(s) !== -1    
+}
