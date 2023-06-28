a2f5b8d253f6eac43f90c62428722bd11ddc499e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-27 20:48:04 -0700

    Time: 69 ms (28.35%), Space: 43 MB (44.96%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.js
@@ -16,3 +16,28 @@ var twoSum = function(numbers, target) {
         }
     }
 };
+
+twoSum = function(numbers, target) {
+    for (let i=0; i < numbers.length; i++) {
+        const idx = binSearch(i+1, numbers.length-1, target-numbers[i])
+        if (idx !== -1) return [i+1, idx+1]
+    }
+    
+    function binSearch(l, r, target) {
+        while (l < r) {
+            const m = Math.floor((l+r)/2)
+            if (numbers[m] === target) {
+                return m
+            } else if (numbers[m] > target) {
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+        if (numbers[l] === target) {
+            return l
+        } else {
+            return -1
+        }
+    }
+}
