3a62fd952462540759ea8ecb74f33d5f60de59c1
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-28 12:55:08 -0700

    Time: 172 ms (5.55%), Space: 74.5 MB (5.56%) - LeetHub

--- 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.js
+++ 2491-divide-players-into-teams-of-equal-skill/2491-divide-players-into-teams-of-equal-skill.js
@@ -15,3 +15,49 @@ var dividePlayers = function(skill) {
     }
     return teams.reduce((acc, team) => acc + team.reduce((acc, player) => acc * player, 1) ,0)
 };
+
+dividePlayers = function(skill) {
+    const sum = skill.reduce((acc, val) => acc + val, 0)
+    const noTeams = skill.length / 2
+    const skillPerTeam = sum / noTeams
+    const teams = makeTeams(Array(skill.length).fill(false), [], 0, [])
+    if (teams.length === noTeams) return teams.reduce((acc, team) => acc + team.reduce((acc, player) => acc * player, 1), 0)
+    return -1                                            
+    
+    function makeTeams(used, curTeam, curSum, teams) {
+        for (let i=0; i < skill.length; i++) {
+            if (!used[i]) {
+                if (curSum > 0 && curSum + skill[i] === skillPerTeam) {
+                    used[i] = true
+                    curTeam.push(skill[i])
+                    teams.push(curTeam.slice())
+                    makeTeams(used, [], 0, teams)
+                } else if (curSum === 0) {
+                    used[i] = true
+                    curTeam.push(skill[i])
+                    curSum += skill[i]
+                    makeTeams(used, curTeam, curSum, teams)
+                }
+            }
+        }
+        return teams
+    }
+}
+
+dividePlayers = function(skill) {
+    const sum = skill.reduce((acc, val) => acc + val, 0)
+    const noTeams = skill.length / 2
+    const skillPerTeam = sum / noTeams
+    const map = {}
+    const teams = []
+    for (const player of skill) {
+        if (map[skillPerTeam - player]) {
+            map[skillPerTeam - player]--
+            teams.push([player, skillPerTeam - player])
+        } else {
+            map[player] = map[player] + 1 || 1
+        }
+    }
+    if (teams.length === noTeams) return teams.reduce((acc, team) => acc + team.reduce((acc, player) => acc * player, 1), 0)
+    return -1
+}
