9ef3d9ffc8c31d7877b67fdcdc423e8c807452b7
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-29 22:05:39 -0700

    Time: 105 ms (20.88%), Space: 44.9 MB (6.79%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -50,3 +50,49 @@ threeSumClosest = function(nums, target) {
     }
     return sum
 }
+
+threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b)
+    return kSumClosest(3, 0, target)
+    
+    function kSumClosest(k, i, target) {
+        if (k === 2) return twoSumClosest(i, target)
+        let diff = Infinity
+        let sum = null
+        for (i; i < nums.length; i++) {
+            if (i > 0 && nums[i] === nums[i-1]) continue
+            const kMinusOneClosestSum = kSumClosest(k-1, i+1, target-nums[i])
+            const curSum = nums[i] + kMinusOneClosestSum
+            const curDiff = Math.abs(target - curSum)
+            if (curDiff === 0) {
+                return curSum
+            } else if (curDiff < diff) {
+                diff = curDiff
+                sum = curSum
+            }
+        }
+        return sum
+    }
+    
+    function twoSumClosest(i, target) {
+        let l = i
+        let r = nums.length-1
+        let diff = Infinity
+        let sum
+        while (l < r) {
+            const curSum = nums[l] + nums[r]
+            const curDiff = Math.abs(target - curSum)
+            if (curDiff === 0) {
+                return curSum
+            } else if (curDiff < diff) {
+                diff = curDiff
+                sum = curSum
+            } else if (curSum > target) {
+                r--
+            } else {
+                l++
+            }
+        }
+        return sum
+    }
+}
