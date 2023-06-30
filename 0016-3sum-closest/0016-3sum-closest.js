044ed54ee8421ae6b4921d1fbf678c95c2464dd3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-06-29 21:48:57 -0700

    Time: 83 ms (64.52%), Space: 44.3 MB (28.87%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.js
+++ 0016-3sum-closest/0016-3sum-closest.js
@@ -24,3 +24,29 @@ var threeSumClosest = function(nums, target) {
     }
     return target - res
 };
+
+threeSumClosest = function(nums, target) {
+    nums.sort((a, b) => a - b)
+    let diff = Infinity
+    let sum
+    for (let i=0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i-1]) continue
+        let l = i + 1
+        let r = nums.length-1
+        while (l < r) {
+            const curSum = nums[i] + nums[l] + nums[r]
+            const curDiff = Math.abs(target - curSum)
+            if (curDiff === 0) {
+                return curSum
+            } else if (curDiff < diff) {
+                sum = curSum
+                diff = curDiff
+            } else if (curSum > target) {
+                r--
+            } else {
+                l++
+            }
+        }
+    }
+    return sum
+}
