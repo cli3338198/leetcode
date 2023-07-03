4224cb6a98f03ace275e501289254783be1d6782
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-02 20:56:07 -0700

    Time: 4299 ms (6.38%), Space: 51.5 MB (6.38%) - LeetHub

--- 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.js
+++ 1884-egg-drop-with-2-eggs-and-n-floors/1884-egg-drop-with-2-eggs-and-n-floors.js
@@ -12,9 +12,29 @@ var twoEggDrop = function(n) {
         if (n < 2) return n
         if (eggs === 1) return n
         let res = Infinity
-        for (let i=1; i < n; i++) {
+        for (let i=1; i <= n; i++) {
             res = Math.min(res, 1 + Math.max(helper(i-1, eggs-1), helper(n-i, eggs)))
         }
         return dp[key] = res
     }
 };
+
+// twoEggDrop = function(n) {
+//     return helper(n, 2)
+    
+//     function helper(n, eggs) {
+//         const dp = Array(n+1).fill(0).map(() => Array(eggs+1).fill(Infinity))
+//         for (const lvl in dp) {
+//             dp[lvl][0] = lvl
+//             dp[lvl][1] = lvl
+//         }
+//         dp[0].fill(0)
+//         dp[1].fill(1)
+//         for (let lvl=2; lvl <= n; lvl++) {
+//             for (let curEggs=1; curEggs <= eggs; curEggs++) {
+//                 dp[lvl][curEggs] = 1 + Math.max(dp[lvl-1][curEggs-1], dp[n-lvl][eggs])
+//             }
+//         }
+//         return Math.min(...dp[n])
+//     }
+// }
