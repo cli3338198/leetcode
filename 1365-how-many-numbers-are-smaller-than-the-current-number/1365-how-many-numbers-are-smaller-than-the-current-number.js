a48417085fb25ed7a179dcdca4cbd3bb8c21d883
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-03 20:43:39 -0700

    Time: 80 ms (66.17%), Space: 44.9 MB (18.91%) - LeetHub

--- 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
+++ 1365-how-many-numbers-are-smaller-than-the-current-number/1365-how-many-numbers-are-smaller-than-the-current-number.js
@@ -3,40 +3,13 @@
  * @return {number[]}
  */
 var smallerNumbersThanCurrent = function(nums) {
-    const map = {}
-    for (const n of nums) {
-        map[n] = map[n] + 1 || 1
-    }
-    return nums.map(helper)
-    
-    function helper(num) {
-        let cnt = 0
-        for (const n in map) {
-            if (n < num) cnt += map[n]
-        }
-        return cnt
-    }
-};
-
-smallerNumbersThanCurrent = function(nums) {
+    nums = nums.map((n, i) => ([n, i])).sort(([a], [b]) => a - b)
     const res = []
     for (let i=0; i < nums.length; i++) {
-        let cnt = 0
-        for (let j=0; j < nums.length; j++) {
-            if (nums[j] < nums[i]) cnt++
-        }
-        res.push(cnt)
+        const [n, idx] = nums[i]
+        let j = i
+        while (j > 0 && nums[j-1][0] === nums[j][0]) j--
+        res[idx] = j
     }
     return res
-}
-
-smallerNumbersThanCurrent = function(nums) {
-    const numbers = Array(101).fill(0)
-    for (const n of nums) {
-        numbers[n]++
-    }
-    for (let i=1; i < numbers.length; i++) {
-        numbers[i] += numbers[i-1]
-    }
-    return nums.map(n => n === 0 ? 0 : numbers[n-1])
-}
+};
