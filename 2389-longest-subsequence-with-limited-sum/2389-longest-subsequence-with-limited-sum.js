d718461a845ad1e41436793bcf0c012bc2dba2d8
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-03 20:31:12 -0700

    Time: 82 ms (70.97%), Space: 44.8 MB (64.52%) - LeetHub

--- 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
+++ 2389-longest-subsequence-with-limited-sum/2389-longest-subsequence-with-limited-sum.js
@@ -20,3 +20,32 @@ var answerQueries = function(nums, queries) {
     }
     return res
 };
+
+answerQueries = function(nums, queries) {
+    nums.sort((a, b) => a - b)
+    const res = []
+    for (let i=1; i < nums.length; i++) {
+        nums[i] += nums[i-1]
+    }
+    for (const q of queries) {
+        res.push(binSearch(q))
+    }
+    return res
+        
+    function binSearch(sum) {
+        let l = 0
+        let r = nums.length-1
+        while (l < r) {
+            const m = Math.floor((l+r)/2)
+            const curSum = nums[m]
+            if (curSum === sum) {
+                return m + 1
+            } else if (curSum > sum) {
+                r = m - 1
+            } else {
+                l = m + 1
+            }
+        }
+        return nums[l] > sum ? l : l + 1
+    }
+}
