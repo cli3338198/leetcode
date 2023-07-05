f15a38a3026ab7143a45075cd76e2997f1497d4d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-04 20:18:51 -0700

    Time: 55 ms (89.69%), Space: 42.8 MB (60.87%) - LeetHub

---
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.js
@@ -0,0 +1,24 @@
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+    nums1.sort((a, b) => a - b)
+    nums2.sort((a, b) => a - b)
+    let i = 0, j = 0, res = []
+    while (i in nums1 && j in nums2) {
+        if (nums1[i] === nums2[j]) {
+            res.push(nums1[i])
+            i++
+            j++
+            while (i in nums1 && nums1[i] === nums1[i-1]) i++
+            while (j in nums2 && nums2[j] === nums2[j-1]) j++
+        } else if (nums1[i] < nums2[j]) {
+            i++
+        } else {
+            j++
+        }
+    }
+    return res
+};
