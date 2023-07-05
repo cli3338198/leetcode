9145482b15e91423b132783529fa8999dbfc267e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-04 20:32:04 -0700

    Time: 142 ms (5.17%), Space: 52.4 MB (5.17%) - LeetHub

--- 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.js
+++ 2248-intersection-of-multiple-arrays/2248-intersection-of-multiple-arrays.js
@@ -10,3 +10,17 @@ var intersection = function(nums) {
     }
     return res
 };
+
+intersection = function(nums) {
+    nums.forEach(arr => arr.sort((a, b) => a - b))
+    console.log(nums)
+    const arr = Array(1001).fill(0)
+    for (const row of nums) {
+        for (let i=0; i < row.length; i++) {
+            if (i > 0 && row[i] === row[i-1]) continue
+            arr[row[i]]++
+        }
+    }
+    console.log(arr)
+    return arr.map((cnt, i) => ([cnt, i])).filter(([cnt, i]) => cnt === nums.length).map(([, i]) => i)
+}
