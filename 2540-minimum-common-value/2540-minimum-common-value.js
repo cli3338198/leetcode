2b5d4af3980bb4c3a340fbdc511a128df5c97503
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-04 20:09:39 -0700

    Time: 86 ms (51.17%), Space: 49.2 MB (95.77%) - LeetHub

--- 2540-minimum-common-value/2540-minimum-common-value.js
+++ 2540-minimum-common-value/2540-minimum-common-value.js
@@ -27,3 +27,17 @@ getCommon = function(nums1, nums2) {
     }
     return -1
 }
+
+getCommon = function(nums1, nums2) {
+    let i = 0, j = 0
+    while (i < nums1.length && j < nums2.length) {
+        if (nums1[i] === nums2[j]) {
+            return nums1[i]
+        } else if (nums1[i] < nums2[j]) {
+            i++
+        } else {
+            j++
+        }
+    }
+    return -1
+}
