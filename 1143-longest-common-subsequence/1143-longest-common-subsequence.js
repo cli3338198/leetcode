defdf9f2feac596c0c3137471f7e3eae8c1d9867
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-10 21:09:46 -0700

    Time: 2098 ms (5.51%), Space: 222.3 MB (5.40%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -4,21 +4,17 @@
  * @return {number}
  */
 var longestCommonSubsequence = function(text1, text2) {
-    const m = text1.length
-    const n = text2.length
-    const dp = Array(m+1).fill(0).map(() => Array(n+1).fill(""))
-    for (let i=1; i <= m; i++) {
-        for (let j=1; j <= n; j++) {
-            if (text1[i-1] === text2[j-1]) {
-                dp[i][j] = dp[i-1][j-1] + text1[i-1]
-            } else {
-                if (dp[i-1][j].length > dp[i][j-1].length) {
-                    dp[i][j] = dp[i-1][j]
-                } else {
-                    dp[i][j] = dp[i][j-1]
-                }
-            }
+    const dp = {}
+    return lcs(0, 0)
+    
+    function lcs(i, j) {
+        const key = `${i} ${j}`
+        if (i === text1.length || j === text2.length) return 0
+        if (key in dp) return dp[key]
+        if (text1[i] === text2[j]) {
+            return 1 + lcs(i+1, j+1)
+        } else {
+            return dp[key] = Math.max(lcs(i+1, j), lcs(i, j+1))
         }
     }
-    return dp[m][n].length
 };
