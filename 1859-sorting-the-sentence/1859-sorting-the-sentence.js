cfad251ff9b6f36deb2f68bf8c487453b06074a6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-10 20:30:11 -0700

    Time: 61 ms (38.18%), Space: 42.3 MB (8.41%) - LeetHub

--- 1859-sorting-the-sentence/1859-sorting-the-sentence.js
+++ 1859-sorting-the-sentence/1859-sorting-the-sentence.js
@@ -12,3 +12,11 @@ var sortSentence = function(s) {
     }
     return res.join(" ").trim()
 };
+
+sortSentence = function(s) {
+    return s
+        .split(" ")
+        .sort((a, b) => a.at(-1) - b.at(-1))
+        .map(w => w.substring(0, w.length-1))
+        .join(" ")
+}
