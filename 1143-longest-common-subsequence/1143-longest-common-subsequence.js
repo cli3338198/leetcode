7f134b4798486e90bbda5f2e21961ba7444aede8
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-10 21:13:53 -0700

    Time: 153 ms (36.46%), Space: 72.9 MB (59.56%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.js
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.js
@@ -18,3 +18,19 @@ var longestCommonSubsequence = function(text1, text2) {
         }
     }
 };
+
+longestCommonSubsequence = function(t1, t2) {
+    const m = t1.length
+    const n = t2.length
+    const dp = Array(m+1).fill(0).map(() => Array(n+1).fill(0))
+    for (let i=1; i <= m; i++) {
+        for (let j=1; j <= n; j++) {
+            if (t1[i-1] === t2[j-1]) {
+                dp[i][j] = 1 + dp[i-1][j-1]
+            } else {
+                dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1])
+            }
+        }
+    }
+    return dp[m][n]
+}
