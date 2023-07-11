3927b5ffdace5c6dfb142072ce44679b500f0bb6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-10 21:00:53 -0700

    Time: 76 ms (21.84%), Space: 45.3 MB (18.35%) - LeetHub

---
+++ 2325-decode-the-message/2325-decode-the-message.js
@@ -0,0 +1,30 @@
+/**
+ * @param {string} key
+ * @param {string} message
+ * @return {string}
+ */
+var decodeMessage = function(key, message) {
+    const set = new Set()
+    key = key.replace(/\s/g, "")
+    let newKey = ""
+    for (const c of key) {
+        if (!set.has(c)) {
+            set.add(c)
+            newKey += c
+        }
+    }
+    const map = {}
+    const abc = 'abcdefghijklmnopqrstuvwxyz'
+    for (let i=0; i < newKey.length; i++) {
+        map[newKey[i]] = abc[i]
+    }
+    let res = ""
+    for (const c of message) {
+        if (c === " ") {
+            res += c
+        } else {
+            res += map[c]
+        }
+    }
+    return res
+};
