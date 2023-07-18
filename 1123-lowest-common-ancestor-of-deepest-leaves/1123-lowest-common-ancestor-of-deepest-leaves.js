897280fa4010beb5d2f9d289eb832ca585abaa61
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-18 15:29:15 -0700

    Time: 81 ms (46.67%), Space: 46.2 MB (93.33%) - LeetHub

--- 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.js
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.js
@@ -34,3 +34,16 @@ var lcaDeepestLeaves = function(root) {
         return Math.max(left, right)
     }
 };
+
+lcaDeepestLeaves = function(root) {
+    return dfs(root)[1]
+    
+    function dfs(root) {
+        if (!root) return [-1, null]
+        const [lDepth, llca] = dfs(root.left)
+        const [rDepth, rlca] = dfs(root.right)
+        if (lDepth > rDepth) return [lDepth + 1, llca]
+        if (rDepth > lDepth) return [rDepth + 1, rlca]
+        return [lDepth + 1, root]
+    }
+}
