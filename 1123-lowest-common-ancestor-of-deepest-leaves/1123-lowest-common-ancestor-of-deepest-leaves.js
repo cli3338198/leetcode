55e47ed563c1bca9b5098f19d54b220befb346dc
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-18 15:13:26 -0700

    Time: 67 ms (93.18%), Space: 46.3 MB (93.33%) - LeetHub

---
+++ 1123-lowest-common-ancestor-of-deepest-leaves/1123-lowest-common-ancestor-of-deepest-leaves.js
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {TreeNode}
+ */
+var lcaDeepestLeaves = function(root) {
+    if (!root) return root
+    let maxDepth = -1
+    let lca = null
+    dfs(root, 0)
+    return lca
+    
+    function dfs(root, depth) {
+        if (!root) return -1
+        if (!root.left && !root.right) {
+            if (depth > maxDepth) {
+                maxDepth = depth
+                lca = root
+            }
+            return depth
+        }
+        const left = dfs(root.left, depth + 1)
+        const right = dfs(root.right, depth + 1)
+        if (left === right && left === maxDepth) {
+            lca = root
+        }
+        return Math.max(left, right)
+    }
+};
