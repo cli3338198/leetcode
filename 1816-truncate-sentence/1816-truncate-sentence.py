d101330e5c20c9abef30b251fff88512d39dd8d1
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-19 20:40:34 -0700

    Time: 39 ms (92.41%), Space: 16.4 MB (39.99%) - LeetHub

--- 1816-truncate-sentence/1816-truncate-sentence.py
+++ 1816-truncate-sentence/1816-truncate-sentence.py
@@ -1,3 +1,4 @@
 class Solution:
     def truncateSentence(self, s: str, k: int) -> str:
+        return " ".join(s.split()[:k:])
         return " ".join([word for i, word in enumerate(s.split()) if i < k])
