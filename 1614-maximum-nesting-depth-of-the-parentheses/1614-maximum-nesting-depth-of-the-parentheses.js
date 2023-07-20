7d4e7b6d39426cffb2fd41cd25f0fcda16ddc6fb
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-19 20:23:58 -0700

    Time: 61 ms (55.62%), Space: 42.2 MB (49.31%) - LeetHub

---
+++ 1614-maximum-nesting-depth-of-the-parentheses/1614-maximum-nesting-depth-of-the-parentheses.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var maxDepth = function(s) {
+    const stack = []
+    let max = 0
+    for (const c of s) {
+        if (c === "(") {
+            stack.push(stack[stack.length-1] + 1 || 1)
+        } else if (c === ")") {
+            max = Math.max(max, stack.pop())
+        } else {
+            continue
+        }
+    }
+    return max
+};
