a24d4d2b454b332702886b183c199315509bb013
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-19 20:51:27 -0700

    Time: 61 ms (42.17%), Space: 42 MB (53.70%) - LeetHub

---
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.js
@@ -0,0 +1,20 @@
+/**
+ * @param {string} command
+ * @return {string}
+ */
+var interpret = function(command) {
+    const map = {"G": ["G", 1], "()": ["o", 2], "(a": ["al", 4]}
+    let res = ""
+    let i = 0
+    while (i < command.length) {
+        if (command[i] in map) {
+            res += map[command[i]][0]
+            i += map[command[i]][1]
+        }
+        if (command[i] + command[i+1] in map) {
+            res += map[command[i] + command[i+1]][0]
+            i += map[command[i] + command[i+1]][1]
+        }
+    }
+    return res
+};
