c1e341ebd5042d182c9202b09dd0d53d74f43dfb
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-26 21:52:24 -0700

    Time: 55 ms (32.32%), Space: 17 MB (6.82%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.py
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -1,5 +1,16 @@
 class Solution:
     def generateParenthesis(self, n: int) -> List[str]:
+        q = deque([[0, 0, ""]])
+        for _ in range(0, 2 * n):
+            length = len(q)
+            for _ in range(0, length):
+                left, right, cur = q.popleft()
+                if left < n:
+                    q.append([left + 1, right, cur + "("])
+                if left > right:
+                    q.append([left, right + 1, cur + ")"])
+        return map(lambda x: x[2], q)
+        
         res = []
         
         def helper(left: int, right: int, cur: str) -> None:
