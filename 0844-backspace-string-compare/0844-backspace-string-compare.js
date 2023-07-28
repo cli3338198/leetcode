aab5452cc6fe26decc3ec78482b5c0826c50cf7a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-27 21:18:13 -0700

    Time: 49 ms (97.50%), Space: 42.1 MB (87.92%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.js
+++ 0844-backspace-string-compare/0844-backspace-string-compare.js
@@ -20,3 +20,39 @@ var backspaceCompare = function(s, t) {
         return stack.join("")
     }
 };
+
+backspaceCompare = function(s, t) {
+    let i = s.length-1
+    let j = t.length-1
+    while (i > -1 || j > -1) {
+        const i1 = helper(s, i)
+        const j1 = helper(t, j)
+        if (i1 < 0 && j1 < 0) {
+            return true
+        }
+        if (i1 < 0 || j1 < 0) {
+            return false
+        }
+        if (s[i1] !== t[j1]) {
+            return false
+        }
+        i = i1 - 1
+        j = j1 - 1
+    }
+    return true
+    
+    function helper(s, i) {
+        let backspace = 0
+        while (i >= 0) {
+            if (s[i] === "#") {
+                backspace++
+            } else if (backspace > 0) {
+                backspace--
+            } else {
+                break
+            }
+            i--
+        }
+        return i
+    }
+}
