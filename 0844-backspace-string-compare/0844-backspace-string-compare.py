4f7d018b1e304afd1f77ed169995c470a185ca12
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-07-27 21:22:23 -0700

    Time: 35 ms (97.96%), Space: 16.2 MB (96.79%) - LeetHub

--- 0844-backspace-string-compare/0844-backspace-string-compare.py
+++ 0844-backspace-string-compare/0844-backspace-string-compare.py
@@ -1,5 +1,30 @@
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
+        def get_next(s: str, i: int) -> int:
+            backspaces = 0
+            while i >= 0:
+                if s[i] == "#":
+                    backspaces += 1
+                elif backspaces > 0:
+                    backspaces -= 1
+                else:
+                    break
+                i -= 1
+            return i
+        
+        i, j = len(s)-1, len(t)-1
+        while i > -1 or j > -1:
+            i1, j1 = get_next(s, i), get_next(t, j)
+            if i1 < 0 and j1 < 0:
+                return True
+            if i1 < 0 or j1 < 0:
+                return False
+            if s[i1] != t[j1]:
+                return False
+            i, j = i1 - 1, j1 - 1
+        return True
+            
+        
         def helper(s: str) -> str:
             stack = []
             for c in s:
