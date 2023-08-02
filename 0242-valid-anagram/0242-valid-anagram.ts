a4e7e58e89c331e45fa3733ac5050b4795fdf5c1
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-02 13:51:12 -0700

    Time: 63 ms (98.00%), Space: 44.6 MB (87.51%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.ts
+++ 0242-valid-anagram/0242-valid-anagram.ts
@@ -1,4 +1,13 @@
 function isAnagram(s: string, t: string): boolean {
+    const arr: number[] = Array(26).fill(0)
+    for (const c of s) {
+        arr[c.charCodeAt(0)-97]++
+    }
+    for (const c of t) {
+        arr[c.charCodeAt(0)-97]--
+    }
+    return arr.every(x => x === 0)
+    
     return toString(splitSort(s)) === toString(splitSort(t))
 };
 
