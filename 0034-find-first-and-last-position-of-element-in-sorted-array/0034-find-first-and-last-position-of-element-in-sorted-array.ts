c28741fa6b6e6073e57486048d71bae19ca4de77
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-02 14:03:29 -0700

    Time: 54 ms (91.53%), Space: 43.5 MB (94.92%) - LeetHub

--- 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.ts
+++ 0034-find-first-and-last-position-of-element-in-sorted-array/0034-find-first-and-last-position-of-element-in-sorted-array.ts
@@ -1,4 +1,20 @@
 function searchRange(nums: number[], target: number): number[] {
+    let l = 0, r = nums.length-1
+    while (l <= r) {
+        const m = Math.floor((l+r)/2)
+        if (nums[m] === target) {
+            l = m, r = m
+            while (nums[l] === nums[m]) l--
+            while (nums[r] === nums[m]) r++
+            return [l+1, r-1]
+        } else if (nums[m] > target) {
+            r = m - 1
+        } else {
+            l = m + 1
+        }
+    }
+    return [-1, -1]
+    
     for (let i=0; i < nums.length; i++) {
         if (nums[i] === target) {
             let j = i
