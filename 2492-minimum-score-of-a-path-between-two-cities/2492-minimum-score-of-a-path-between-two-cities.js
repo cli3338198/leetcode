80022d83b9e0008ba93305792f77ca18ec1b332a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-02 21:19:08 -0700

    Time: 552 ms (71.43%), Space: 147.1 MB (57.14%) - LeetHub

---
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number} n
+ * @param {number[][]} roads
+ * @return {number}
+ */
+var minScore = function(n, roads) {
+    const graph = {}
+    for (const [u, v, d] of roads) {
+        graph[u] = graph[u] || []
+        graph[v] = graph[v] || []
+        graph[u].push([v, d])
+        graph[v].push([u, d])
+    }
+    return dfs(1, new Set())
+    
+    function dfs(cur, set) {
+        set.add(cur)
+        let res = Infinity
+        for (const [next, d] of graph[cur] || []) {
+            res = Math.min(res, d)
+            if (!set.has(next)) {
+                res = Math.min(res, dfs(next, set))
+            }
+        }
+        return res
+    }
+};
