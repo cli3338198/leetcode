243d3ac77dc6095c5851b7ce7099e20a88357294
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-02 21:57:43 -0700

    Time: 1772 ms (41.45%), Space: 144.6 MB (5.14%) - LeetHub

--- 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
+++ 2492-minimum-score-of-a-path-between-two-cities/2492-minimum-score-of-a-path-between-two-cities.py
@@ -1,5 +1,35 @@
+class UF:
+    def __init__(self):
+        self.root = {}
+    
+    def insert(self, a: int) -> None:
+        if a not in self.root:
+            self.root[a] = a
+    
+    def find(self, a: int) -> int:
+        self.insert(a)
+        if self.root[a] != a:
+            self.root[a] = self.find(self.root[a])
+        return self.root[a]
+    
+    def union(self, a: int, b: int) -> None:
+        a, b = self.find(a), self.find(b)
+        if a != b:
+            self.root[b] = a
+
 class Solution:
     def minScore(self, n: int, roads: List[List[int]]) -> int:
+        uf = UF()
+        for u, v, _ in roads:
+            uf.union(u, v)
+        res = float("inf")
+        root = uf.find(1)
+        for u, v, d in roads:
+            u, v = uf.find(u), uf.find(v)
+            if u == root or v == root:
+                res = min(res, d)
+        return res
+        
         graph = {}
         for u, v, d in roads:
             if u not in graph:
