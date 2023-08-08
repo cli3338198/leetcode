450820bdfac634ced8579448fd9b410c6cbcb463
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-08 13:10:24 -0700

    Time: 64 ms (81.82%), Space: 46 MB (94.81%) - LeetHub

--- 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.js
+++ 0513-find-bottom-left-tree-value/0513-find-bottom-left-tree-value.js
@@ -1,21 +1,45 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def findBottomLeftValue(self, root: Optional[TreeNode]) -> int:
-        res = None
-        q = deque([root])
-        while q:
-            length = len(q)
-            for i in range(0, length):
-                node = q.popleft()
-                if i == 0:
-                    res = node.val
-                if node.left:
-                    q.append(node.left)
-                if node.right:
-                    q.append(node.right)
-        return res
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {number}
+ */
+var findBottomLeftValue = function(root) {
+    const q = [root]
+    let res = null
+    while (q.length) {
+        const len = q.length
+        for (let i=0; i < len; i++) {
+            const {val, left, right} = q.shift()
+            if (i === 0) res = val
+            if (left) q.push(left)
+            if (right) q.push(right)
+        }
+    }
+    return res
+};
+
+findBottomLeftValue = function(root) {
+    let maxDepth = 0
+    let res = root.val
+    dfs(root, 0)
+    return res
+    
+    function dfs(root, depth) {
+        if (!root) return
+        if (!root.left) {
+            if (depth > maxDepth) {
+                maxDepth = depth
+                res = root.val
+            }
+        }
+        dfs(root.left, depth + 1)
+        dfs(root.right, depth + 1)
+    }
+}
