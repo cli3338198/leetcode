0ab53c01981b084ee7c468da7c48d64186cbbc57
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-08 21:05:33 -0700

    Time: 47 ms (100.00%), Space: 46.5 MB (91.30%) - LeetHub

--- 1014-best-sightseeing-pair/1014-best-sightseeing-pair.js
+++ 1014-best-sightseeing-pair/1014-best-sightseeing-pair.js
@@ -11,3 +11,15 @@ var maxScoreSightseeingPair = function(values) {
     }
     return res
 };
+
+maxScoreSightseeingPair = function(values) {
+    let max = 0
+    let pair1 = values[0]
+    for (let i=1; i < values.length; i++) {
+        const pair2 = values[i] - i
+        const pairValue = pair1 + pair2
+        max = Math.max(max, pairValue)
+        pair1 = Math.max(pair1, values[i] + i)
+    }
+    return max
+}
