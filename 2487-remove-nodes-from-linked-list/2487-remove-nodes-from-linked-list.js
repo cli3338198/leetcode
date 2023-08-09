c224926e7190d7e6e9d3caddb039a7f351a0fee3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-08 19:33:09 -0700

    Time: 296 ms (34.68%), Space: 97.5 MB (38.71%) - LeetHub

---
+++ 2487-remove-nodes-from-linked-list/2487-remove-nodes-from-linked-list.js
@@ -0,0 +1,26 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var removeNodes = function(head) {
+    const stack = []
+    while (head) {
+        while (stack.length && head.val > stack.at(-1).val) stack.pop()
+        stack.push(head)
+        head = head.next
+    }
+    const dummy = new ListNode()
+    let cur = dummy
+    for (const node of stack) {
+        cur.next = node
+        cur = cur.next
+    }
+    return dummy.next
+};
