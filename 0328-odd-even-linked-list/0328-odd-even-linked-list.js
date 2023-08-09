0fe797687116c1237e08790b53897166d07ba619
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-09 12:30:55 -0700

    Time: 75 ms (34.19%), Space: 43.7 MB (98.83%) - LeetHub

--- 0328-odd-even-linked-list/0328-odd-even-linked-list.js
+++ 0328-odd-even-linked-list/0328-odd-even-linked-list.js
@@ -28,3 +28,18 @@ var oddEvenList = function(head) {
     curOdd.next = evens.next
     return odds.next
 };
+
+oddEvenList = function(head) {
+    if (!head || !head.next) return head
+    let odd = head
+    let even = head.next
+    let evenHead = even
+    while (even && even.next) {
+        odd.next = even.next
+        odd = odd.next
+        even.next = odd.next
+        even = even.next
+    }
+    odd.next = evenHead
+    return head
+}
