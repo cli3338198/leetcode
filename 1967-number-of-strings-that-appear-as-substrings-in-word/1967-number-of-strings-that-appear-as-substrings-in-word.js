ffa6c4de06d0afdce6e02426c47ff9c67d9ebb62
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-09 16:54:02 -0700

    Time: 76 ms (5.29%), Space: 48.7 MB (5.29%) - LeetHub

--- 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.js
+++ 1967-number-of-strings-that-appear-as-substrings-in-word/1967-number-of-strings-that-appear-as-substrings-in-word.js
@@ -6,3 +6,18 @@
 var numOfStrings = function(patterns, word) {
     return patterns.reduce((acc, substr) => acc + (word.includes(substr) ? 1 : 0), 0)
 };
+
+numOfStrings = function(patterns, word) {
+    const set = new Set([...allSubstrings(word)])
+    return patterns.reduce((acc, word) => acc + (set.has(word) ? 1 : 0), 0)
+    
+    function allSubstrings(word) {
+        const res = []
+        for (let i=0; i < word.length; i++) {
+            for (let j=i+1; j <= word.length; j++) {
+                res.push(word.substring(i, j))
+            }
+        }
+        return res
+    }
+}
