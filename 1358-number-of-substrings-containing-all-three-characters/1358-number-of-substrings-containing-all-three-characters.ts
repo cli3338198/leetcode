a37c12a31373c96bcca2ef4ed81c5168dc28ad5e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-10 20:48:14 -0700

    Time: 66 ms (87.50%), Space: 44.6 MB (62.50%) - LeetHub

--- 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.ts
+++ 1358-number-of-substrings-containing-all-three-characters/1358-number-of-substrings-containing-all-three-characters.ts
@@ -1,4 +1,12 @@
 function numberOfSubstrings(s: string): number {
+    const arr: [number, number, number] = [-1, -1, -1]
+    let ans = 0
+    for (let i=0; i < s.length; i++) {
+        arr[s[i].charCodeAt(0)-97] = i
+        ans += 1 + Math.min(...arr)
+    }
+    return ans
+    
     const map: Record<string, number> = {}
     for (const c of 'abc') map[c] = 0
     let l = 0
