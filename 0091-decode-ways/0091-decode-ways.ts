cbe527b6fbf350c9f50f995103527b7943fdf80c
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-10 21:30:36 -0700

    Time: 56 ms (95.49%), Space: 44.8 MB (48.87%) - LeetHub

--- 0091-decode-ways/0091-decode-ways.ts
+++ 0091-decode-ways/0091-decode-ways.ts
@@ -1,7 +1,8 @@
 function numDecodings(s: string): number {
+    if (s[0] === "0") return 0
     const dp: number[] = Array(s.length+1).fill(0)
     dp[0] = 1
-    dp[1] = s[0] === "0" ? 0 : 1
+    dp[1] = 1
     for (let i=2; i <= s.length; i++) {
         if (s[i-1] !== "0") dp[i] += dp[i-1]
         if ((s[i-2] === "1") || (s[i-2] === "2" && Number(s[i-1]) <= 6)) dp[i] += dp[i-2]
