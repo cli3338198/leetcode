adc4a77247117c2c171ab899cd46b43f7a7c2002
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-11 13:37:18 -0700

    Time: 49 ms (30.46%), Space: 16.3 MB (66.98%) - LeetHub

--- 2103-rings-and-rods/2103-rings-and-rods.py
+++ 2103-rings-and-rods/2103-rings-and-rods.py
@@ -1,5 +1,12 @@
 class Solution:
     def countPoints(self, rings: str) -> int:
+        ans = 0
+        for i in range(0, 10):
+            j = str(i)
+            if f"R{j}" in rings and f"G{j}" in rings and f"B{j}" in rings:
+                ans += 1
+        return ans
+        
         rngs = []
         for i in range(0, 10):
             rngs.append(set())
