2002ff00edcc571fb3da2233d8da432660954890
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-13 11:57:42 -0700

    Time: 81 ms (46.79%), Space: 46.1 MB (31.41%) - LeetHub

--- 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.js
+++ 2570-merge-two-2d-arrays-by-summing-values/2570-merge-two-2d-arrays-by-summing-values.js
@@ -21,3 +21,31 @@ var mergeArrays = function(nums1, nums2) {
     }
     return res.filter(Boolean)
 };
+
+mergeArrays = function(nums1, nums2) {
+    let i = 0
+    let j = 0
+    const res = []
+    while (i < nums1.length && j < nums2.length) {
+        const [idx1, num1] = nums1[i]
+        const [idx2, num2] = nums2[j]
+        if (idx1 === idx2) {
+            res.push([idx1, num1 + num2])
+            i++
+            j++
+        } else if (idx1 < idx2) {
+            res.push([idx1, num1])
+            i++
+        } else {
+            res.push([idx2, num2])
+            j++
+        }
+    }
+    while (i < nums1.length) {
+        res.push(nums1[i++])
+    }
+    while (j < nums2.length) {
+        res.push(nums2[j++])
+    }
+    return res
+}
