86c65660abb3f7af4f5dc2bacf36b92b0f3e9415
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-13 21:26:17 -0700

    Time: 1770 ms (6.25%), Space: 105.1 MB (7.61%) - LeetHub

--- 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
+++ 2149-rearrange-array-elements-by-sign/2149-rearrange-array-elements-by-sign.js
@@ -17,3 +17,33 @@ var rearrangeArray = function(nums) {
     }
     return res
 };
+
+rearrangeArray = function(nums) {
+    let outOfPlace = -1
+    for (let i=0; i < nums.length; i++) {
+        if (outOfPlace !== -1) {
+            if ((nums[i] >= 0 && nums[outOfPlace] < 0) || (nums[i] < 0 && nums[outOfPlace] >= 0)) {
+                rightRotate(outOfPlace, i)
+                if (i - outOfPlace > 2) {
+                    outOfPlace += 2
+                } else {
+                    outOfPlace = -1
+                }
+            }
+        }
+        if (outOfPlace === -1) {
+            if ((nums[i] >= 0 && i % 2 !== 0) || (nums[i] < 0 && i % 2 === 0)) {
+                outOfPlace = i
+            }
+        }
+    }
+    return nums
+    
+    function rightRotate(outOfPlace, cur) {
+        const temp = nums[cur]
+        for (let i=cur; i > outOfPlace; i--) {
+            nums[i] = nums[i-1]
+        }
+        nums[outOfPlace] = temp
+    }
+}
