0a59670504da40ac2e5a4e80b704e994d7e0bf74
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-14 21:46:15 -0700

    Time: 243 ms (95.10%), Space: 17.8 MB (77.09%) - LeetHub

--- 0658-find-k-closest-elements/0658-find-k-closest-elements.py
+++ 0658-find-k-closest-elements/0658-find-k-closest-elements.py
@@ -1,18 +1,10 @@
-/**
- * @param {number[]} arr
- * @param {number} k
- * @param {number} x
- * @return {number[]}
- */
-var findClosestElements = function(arr, k, x) {
-    let l = 0, r = arr.length-k
-    while (l < r) {
-        const m = Math.floor((l+r)/2)
-        if (x - arr[m] > arr[m+k] - x) {
-            l = m + 1
-        } else {
-            r = m
-        }
-    }
-    return arr.slice(l, l+k)
-};
+class Solution:
+    def findClosestElements(self, arr: List[int], k: int, x: int) -> List[int]:
+        l, r = 0, len(arr) - k
+        while l < r:
+            m = (l + r) // 2
+            if x - arr[m] > arr[m + k] - x:
+                l = m + 1
+            else:
+                r = m
+        return arr[l:l+k:]
