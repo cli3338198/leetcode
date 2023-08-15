c189446476e9adc6525acfcdc59ef2efcb938a04
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-15 12:44:15 -0700

    Time: 380 ms (47.62%), Space: 148.4 MB (40.47%) - LeetHub

--- 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.js
+++ 2385-amount-of-time-for-binary-tree-to-be-infected/2385-amount-of-time-for-binary-tree-to-be-infected.js
@@ -40,3 +40,42 @@ var amountOfTime = function(root, start) {
         makeGraph(root, root.right)
     }
 };
+
+amountOfTime = function(root, start) {
+    const graph = {}
+    makeGraph(null, root)
+    return bfs(start)
+    
+    function bfs(start) {
+        const q = [start]
+        let res = -1
+        const set = new Set()
+        set.add(start)
+        while (q.length) {
+            const len = q.length
+            for (let i=0; i < len; i++) {
+                const cur = q.shift()
+                for (const next of graph[cur] || []) {
+                    if (!set.has(next)) {
+                        set.add(next)
+                        q.push(next)
+                    }
+                }
+            }
+            res++
+        }
+        return res
+    }
+    
+    function makeGraph(parent, root) {
+        if (!root) return
+        graph[root.val] = graph[root.val] || []
+        if (parent) {
+            graph[parent.val] = graph[parent.val] || []
+            graph[root.val].push(parent.val)
+            graph[parent.val].push(root.val)
+        }
+        makeGraph(root, root.left)
+        makeGraph(root, root.right)
+    }
+}
