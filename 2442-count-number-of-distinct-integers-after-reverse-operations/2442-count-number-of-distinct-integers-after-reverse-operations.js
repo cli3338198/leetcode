8681f2e037ddf47ae97d2aea4c8bd24659a0a08b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-16 20:28:49 -0700

    Time: 118 ms (98.48%), Space: 78.4 MB (95.45%) - LeetHub

--- 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.js
+++ 2442-count-number-of-distinct-integers-after-reverse-operations/2442-count-number-of-distinct-integers-after-reverse-operations.js
@@ -13,3 +13,21 @@ var countDistinctIntegers = function(nums) {
         return Number((String(n)).split("").reverse().join(""))
     }
 };
+
+countDistinctIntegers = function(nums) {
+    const set = new Set(nums)
+    for (const n of nums) {
+        set.add(reverse(n))
+    }
+    return set.size
+    
+    function reverse(n) {
+        let res = 0
+        while (n) {
+            const lastDigit = n % 10
+            res = res * 10 + lastDigit
+            n = Math.floor(n / 10)
+        }
+        return res
+    }
+}
