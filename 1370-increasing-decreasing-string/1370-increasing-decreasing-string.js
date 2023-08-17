629d5f86da9f7cf0b5b7f0eff6a5494c192dabcc
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-17 10:24:25 -0700

    Time: 83 ms (58.24%), Space: 50.7 MB (9.89%) - LeetHub

---
+++ 1370-increasing-decreasing-string/1370-increasing-decreasing-string.js
@@ -0,0 +1,60 @@
+/**
+ * @param {string} s
+ * @return {string}
+ */
+var sortString = function(s) {
+    const arr = Array(26).fill(0)
+    for (const c of s) {
+        arr[c.charCodeAt(0)-97]++
+    }
+    let res = ""
+    while (arr.some(n => n > 0)) {
+        pickSmallest()
+        pickGreatest()
+    }
+    return res
+    
+    function pickSmallest() {
+        for (let i=0; i < arr.length; i++) {
+            if (arr[i] > 0) {
+                arr[i]--
+                res += String.fromCharCode(i+97)
+                break
+            }
+        }
+        while (true) {
+            let found = false
+            for (let i=res.length ? res.at(-1).charCodeAt(0) - 97 + 1 : 0; i < arr.length; i++) {
+                if (arr[i] > 0) {
+                    arr[i]--
+                    res += String.fromCharCode(i+97)
+                    found = true
+                    break
+                }
+            }
+            if (!found) break
+        }
+    }
+    
+    function pickGreatest() {
+        for (let i=arr.length-1; i >= 0; i--) {
+            if (arr[i] > 0) {
+                arr[i]--
+                res += String.fromCharCode(i+97)
+                break
+            }
+        }
+        while (true) {
+            let found = false
+            for (let i=res.length ? res.at(-1).charCodeAt(0) - 97 -1 : arr.length-1; i >= 0; i--) {
+                if (arr[i] > 0) {
+                    arr[i]--
+                    res += String.fromCharCode(i+97)
+                    found = true
+                    break
+                }
+            }
+            if (!found) break
+        }
+    }
+};
