c46330466ad74ea30399db4809e8dd160d8ab8c5
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-18 19:19:25 -0700

    Time: 44 ms (96.25%), Space: 41.4 MB (93.63%) - LeetHub

--- 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
+++ 1700-number-of-students-unable-to-eat-lunch/1700-number-of-students-unable-to-eat-lunch.js
@@ -14,3 +14,19 @@ var countStudents = function(students, sandwiches) {
     }
     return students.length-i
 };
+
+countStudents = function(students, sandwiches) {
+    let haveNotEaten = 0
+    while (students.length) {
+        if (students.at(0) === sandwiches.at(0)) {
+            students.shift()
+            sandwiches.shift()
+            haveNotEaten = 0
+        } else {
+            students.push(students.shift())
+            haveNotEaten++
+            if (haveNotEaten === students.length) break
+        }
+    }
+    return haveNotEaten
+}
