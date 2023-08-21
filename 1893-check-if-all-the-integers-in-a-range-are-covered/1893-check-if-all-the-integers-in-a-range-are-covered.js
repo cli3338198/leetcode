e4f7b4ef41097fe9dbfb8facbcd1e6a449031546
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-21 13:19:14 -0700

    Time: 65 ms (34.25%), Space: 43.6 MB (52.05%) - LeetHub

--- 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.js
+++ 1893-check-if-all-the-integers-in-a-range-are-covered/1893-check-if-all-the-integers-in-a-range-are-covered.js
@@ -16,3 +16,18 @@ var isCovered = function(ranges, left, right) {
     }
     return true
 };
+
+isCovered = function(ranges, left, right) {
+    const arr = Array(52).fill(0)
+    for (const [lo, hi] of ranges) {
+        arr[lo]++
+        arr[hi + 1]--
+    }
+    for (let i=1, overlaps=0; i <= right; i++) {
+        overlaps += arr[i]
+        if (i >= left && overlaps === 0) {
+            return false
+        }
+    }
+    return true
+}
