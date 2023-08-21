11e13eb30ee7f33994765ff14b1b06f6fa62b08f
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-20 23:20:06 -0700

    Time: 591 ms (20.27%), Space: 112.7 MB (5.40%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string[]} nums
+ * @return {string}
+ */
+var findDifferentBinaryString = function(nums) {
+    nums = new Set(nums)
+    return permutations(0).filter(p => !nums.has(p))[0]
+    
+    function permutations(i) {
+        if (i >= nums.size) return [""]
+        const res = []
+        for (const p of permutations(i+1)) {
+            res.push("0" + p)
+            res.push("1" + p)
+        }
+        return res        
+    }
+};
