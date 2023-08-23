b2daad6e3bdc71ded82158aada212b8930187ab2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-23 10:43:45 -0700

    Time: 59 ms (40.19%), Space: 44 MB (14.95%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.js
@@ -0,0 +1,18 @@
+/**
+ * @param {string} s
+ * @param {string} part
+ * @return {string}
+ */
+var removeOccurrences = function(s, part) {
+    let res = ""
+    for (const c of s) {
+        res += c
+        if (res.length >= part.length) {
+            const idx = res.indexOf(part)
+            if (idx !== -1) {
+                res = res.substring(0, idx) + res.substring(idx + part.length)
+            }
+        }
+    }
+    return res
+};
