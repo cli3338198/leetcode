721014fcc1443685788102ef64c8e0901778e7bc
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-22 21:20:38 -0700

    Time: 100 ms (5.88%), Space: 57.5 MB (5.26%) - LeetHub

---
+++ 2553-separate-the-digits-in-an-array/2553-separate-the-digits-in-an-array.js
@@ -0,0 +1,7 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var separateDigits = function(nums) {
+    return nums.reduce((acc, num) => [...acc, ...(String(num).split("").reduce((acc, val) => [...acc, val], []))], [])
+};
