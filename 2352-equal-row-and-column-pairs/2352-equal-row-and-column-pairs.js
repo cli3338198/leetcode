eb9c62d94e576c0a45d84cb6733373c45a1553b6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-24 12:48:51 -0700

    Time: 348 ms (11.12%), Space: 190 MB (5.03%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.js
@@ -19,3 +19,50 @@ var equalPairs = function(grid) {
     }
     return res
 };
+
+equalPairs = function(grid) {
+    class Trie {
+        constructor() {
+            this.children = {}
+            this.count = 0
+            this.end = false
+        }
+        insert(arr) {
+            let cur = this
+            for (const n of arr) {
+                if (!(n in cur.children)) {
+                    cur.children[n] = new Trie()
+                }
+                cur.children[n].count++
+                cur = cur.children[n]
+            }
+            cur.end = true
+        }
+        find(arr) {
+            let cur = this
+            for (const n of arr) {
+                if (n in cur.children) {
+                    cur = cur.children[n]
+                } else {
+                    return 0
+                }
+            }
+            return cur.count
+        }
+    }
+    
+    const n = grid.length
+    const trie = new Trie()
+    for (const row of grid) {
+        trie.insert(row)
+    }
+    let res = 0
+    for (let c=0; c < n; c++) {
+        const arr = []
+        for (let r=0; r < n; r++) {
+            arr.push(grid[r][c])
+        }
+        res += trie.find(arr)
+    }
+    return res
+}
