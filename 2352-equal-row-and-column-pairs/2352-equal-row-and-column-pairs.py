b78a811fe8baca18830a0a1a1645193c4347c7e0
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-24 12:54:36 -0700

    Time: 695 ms (22.82%), Space: 34.3 MB (5.03%) - LeetHub

--- 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
+++ 2352-equal-row-and-column-pairs/2352-equal-row-and-column-pairs.py
@@ -1,5 +1,38 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.count = 0
+    
+    def insert(self, arr: List[int]) -> None:
+        cur = self
+        for n in arr:
+            if str(n) not in cur.children:
+                cur.children[str(n)] = Trie()
+            cur.children[str(n)].count += 1
+            cur = cur.children[str(n)]
+    
+    def find(self, arr: List[int]) -> int:
+        cur = self
+        for n in arr:
+            if str(n) in cur.children:
+                cur = cur.children[str(n)]
+            else:
+                return 0
+        return cur.count
+
 class Solution:
     def equalPairs(self, grid: List[List[int]]) -> int:
+        n, trie, res = len(grid), Trie(), 0
+        for row in grid:
+            trie.insert(row)
+        for c in range(0, n):
+            arr = []
+            for r in range(0, n):
+                arr.append(grid[r][c])
+            res += trie.find(arr)
+        return res
+            
+            
         n, cnt, res = len(grid), {}, 0
         for row in grid:
             key = "|" + "|".join([str(c) for c in row])
