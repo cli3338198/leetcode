bbd288b345fbc1bc09ace9f780aa0d03e45a5326
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-24 10:13:00 -0700

    Time: 78 ms (90.53%), Space: 50.7 MB (65.88%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.js
@@ -11,3 +11,12 @@ var findDuplicates = function(nums) {
     }
     return res
 };
+
+findDuplicates = function(nums) {
+    const res = []
+    for (let i=0; i < nums.length; i++) {
+        if (nums[Math.abs(nums[i])-1] < 0) res.push(Math.abs(nums[i]))
+        nums[Math.abs(nums[i])-1] *= -1
+    }
+    return res
+}
