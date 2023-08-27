cb7c9191a684ee90b5dc1146df05d50bf51f79d6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-26 23:00:34 -0700

    Time: 114 ms (33.48%), Space: 65.8 MB (41.96%) - LeetHub

---
+++ 0523-continuous-subarray-sum/0523-continuous-subarray-sum.js
@@ -0,0 +1,28 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var checkSubarraySum = function(nums, k) {
+    const prefixes = Array(nums.length+1).fill(0)
+    for (let i=0; i < nums.length; i++) {
+        prefixes[i+1] = (prefixes[i] + nums[i] % k) % k
+    }
+    const map = {}
+    for (let i=0; i < prefixes.length; i++) {
+        if (prefixes[i] in map && i - map[prefixes[i]]) {
+            return true
+        }
+        if (!(prefixes[i] in map)) map[prefixes[i]] = i
+    }
+    return false
+};
+
+checkSubarraySum = function(nums, k) {
+    const set = new Set()
+    for (let i=0, curSum=0; i < nums.length; curSum += nums[i], i++) {
+        if (set.has((curSum + nums[i]) % k)) return true
+        set.add(curSum % k)
+    }
+    return false
+}
