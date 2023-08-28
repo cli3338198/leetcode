bc6ea04a17863c6f4a0e080b02c306bb1a2ab08f
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-28 16:32:31 -0700

    Time: 468 ms (88.13%), Space: 22.1 MB (71.46%) - LeetHub

---
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.py
@@ -0,0 +1,23 @@
+class Solution:
+    def shipWithinDays(self, weights: List[int], days: int) -> int:
+        max_wt = total_wt = 0
+        for wt in weights:
+            max_wt = max(max_wt, wt)
+            total_wt += wt
+            
+        def can_ship(cur_wt: int) -> bool:
+            day, wt = 1, 0
+            for w in weights:
+                wt += w
+                if wt > cur_wt:
+                    wt = w
+                    day += 1
+            return day <= days
+        
+        while max_wt < total_wt:
+            cur_wt = (max_wt+total_wt)//2
+            if can_ship(cur_wt):
+                total_wt = cur_wt
+            else:
+                max_wt = cur_wt + 1
+        return max_wt
