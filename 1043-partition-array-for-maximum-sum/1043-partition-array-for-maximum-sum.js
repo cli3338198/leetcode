2c5c6f301d2d13ecb242c07ea30058051aeece9c
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-29 21:06:28 -0700

    Time: 62 ms (86.96%), Space: 42.3 MB (78.26%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.js
@@ -7,11 +7,10 @@ var maxSumAfterPartitioning = function(arr, k) {
     const n = arr.length
     const dp = Array(n+1).fill(0)
     for (let i=1; i <= n; i++) {
-        let max = 0
-        let curBest = 0
-        for (let subarraySize=1; subarraySize <= k && i - subarraySize >= 0; subarraySize++) {
-            max = Math.max(max, arr[i - subarraySize])
-            curBest = Math.max(curBest, dp[i - subarraySize] + (max * subarraySize))
+        let max = 0, curBest = 0
+        for (let j=1; j <= k && i - j >= 0; j++) {
+            max = Math.max(max, arr[i-j])
+            curBest = Math.max(curBest, dp[i - j] + max * j)
         }
         dp[i] = curBest
     }
