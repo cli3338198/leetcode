e3c35d491f6fdab8e4b7a1a55b8db3c9051782c7
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-29 21:20:50 -0700

    Time: 306 ms (87.17%), Space: 16.5 MB (63.17%) - LeetHub

--- 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
+++ 1043-partition-array-for-maximum-sum/1043-partition-array-for-maximum-sum.py
@@ -1,5 +1,14 @@
 class Solution:
     def maxSumAfterPartitioning(self, arr: List[int], k: int) -> int:
+        n = len(arr)
+        dp = [0] * (n+1)
+        for i in range(1, n+1):
+            curmax = 0
+            for j in range(1, min(k, i)+1):
+                curmax = max(curmax, arr[i-j])
+                dp[i] = max(dp[i], dp[i-j] + curmax * j)
+        return dp[n]
+        
         dp = [0] * (len(arr) + 1)
         for i in range(1, len(arr)+1):                      # find solutions for all subarrays 
             mx = 0                                          # find max in current subarray
