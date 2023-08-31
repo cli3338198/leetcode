f25eecdb1d7bdf6c6b91cf3e3013c6d855c85835
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-31 16:28:33 -0700

    Time: 732 ms (21.80%), Space: 22.4 MB (64.52%) - LeetHub

--- 0886-possible-bipartition/0886-possible-bipartition.py
+++ 0886-possible-bipartition/0886-possible-bipartition.py
@@ -1,5 +1,42 @@
 class Solution:
     def possibleBipartition(self, n: int, dislikes: List[List[int]]) -> bool:
+        parent = {}
+        
+        def insert(n: int) -> None:
+            nonlocal parent
+            if n not in parent:
+                parent[n] = n
+        
+        def find(n: int) -> int:
+            nonlocal parent
+            if parent[n] != n:
+                parent[n] = find(parent[n])
+            return parent[n]
+        
+        def union(a: int, b: int) -> bool:
+            nonlocal parent
+            ra, rb = find(a), find(b)
+            if ra != rb:
+                parent[rb] = ra
+        
+        graph = {}
+        for i in range(1, n+1):
+            graph[i] = []
+            parent[i] = i
+        for u, v in dislikes:
+            graph[u].append(v)
+            graph[v].append(u)
+        for i in range(1, n+1):
+            if i not in graph:
+                continue
+            for j in graph.get(i, []):
+                if find(i) == find(j):
+                    return False
+                union(graph[i][0], j)
+        return True            
+        
+        #
+        
         graph = {}
         for i in range(1, n+1):
             graph[i] = []
