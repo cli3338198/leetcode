3c0d85281a55db948a2bdd9ca6e8818d72ffdbab
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-30 21:37:31 -0700

    Time: 49 ms (73.43%), Space: 40.7 MB (99.43%) - LeetHub

--- 0096-unique-binary-search-trees/0096-unique-binary-search-trees.js
+++ 0096-unique-binary-search-trees/0096-unique-binary-search-trees.js
@@ -4,14 +4,14 @@
  */
 var numTrees = function(n) {
     const dp = Array(n+1).fill(1)
-    for (let nodes=2; nodes <= n; nodes++) {
-        let res = 0
-        for (let root=1; root <= nodes; root++) {
-            const left = root - 1
-            const right = nodes - root
-            res += dp[left] * dp[right]
+    for (let numNodes=2; numNodes <= n; numNodes++) {
+        let total = 0
+        for (let root=1; root <= numNodes; root++) {
+            const left = root-1
+            const right = numNodes - root
+            total += dp[left] * dp[right]
         }
-        dp[nodes] = res
+        dp[numNodes] = total
     }
     return dp[n]
 };
