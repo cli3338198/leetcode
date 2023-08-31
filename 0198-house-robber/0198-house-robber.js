26981f9a7c255eb60034f72f576eeebed93de188
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-30 21:19:53 -0700

    Time: 51 ms (70.61%), Space: 41.4 MB (92.93%) - LeetHub

--- 0198-house-robber/0198-house-robber.js
+++ 0198-house-robber/0198-house-robber.js
@@ -6,8 +6,9 @@ var rob = function(nums) {
     const n = nums.length
     const dp = Array(n).fill(0)
     dp[0] = nums[0]
-    for (let i=1; i < n; i++) {
-        dp[i] = Math.max(dp[i-1], nums[i] + (dp[i-2] || 0))
+    dp[1] = Math.max(nums[0], nums[1])
+    for (let i=2; i < n; i++) {
+        dp[i] = Math.max(dp[i-1], dp[i-2] + nums[i])
     }
     return dp[n-1]
 };
