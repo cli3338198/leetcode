3b2985476a82a4be6fe6254ebc0f45b97e0e13eb
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-30 22:07:27 -0700

    Time: 72 ms (25.58%), Space: 45.2 MB (31.67%) - LeetHub

--- 0046-permutations/0046-permutations.js
+++ 0046-permutations/0046-permutations.js
@@ -7,27 +7,8 @@ var permute = function(nums) {
     const res = []
     for (let i=0; i < nums.length; i++) {
         for (const sub of permute(nums.slice(0, i).concat(nums.slice(i+1)))) {
-            res.push([...sub, nums[i]])
+            res.push([nums[i], ...sub])
         }
     }
     return res
 };
-
-permute = function(nums) {
-    const n = nums.length
-    const q = [[[], Array(n).fill(false)]]
-    for (let i=0; i < n; i++) {
-        const len = q.length
-        for (let j=0; j < len; j++) {
-            const [curList, used] = q.shift()
-            for (let k=0; k < n; k++) {
-                if (!used[k]) {
-                    const copyUsed = used.slice()
-                    copyUsed[k] = true
-                    q.push([[...curList, nums[k]], copyUsed])
-                }
-            }
-        }
-    }
-    return q.map(x => x[0])
-}
