d057c97ee2f8299759ee50fe8ab13ee848c3e825
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-31 20:13:14 -0700

    Time: 87 ms (82.81%), Space: 46.7 MB (60.08%) - LeetHub

--- 0322-coin-change/0322-coin-change.js
+++ 0322-coin-change/0322-coin-change.js
@@ -4,32 +4,14 @@
  * @return {number}
  */
 var coinChange = function(coins, amount) {
-    const dp = {}
-    const res = helper(amount)
-    return res === Infinity ? -1 : res
-    
-    function helper(amount) {
-        if (amount === 0) return 0
-        if (amount in dp) return dp[amount]
-        let res = Infinity
-        for (const coin of coins) {
-            if (coin <= amount) {
-                res = Math.min(res, 1 + helper(amount - coin))
-            }
-        }
-        return dp[amount] = res
-    }
-};
-
-coinChange = function(coins, amount) {
     const dp = Array(amount+1).fill(Infinity)
     dp[0] = 0
-    for (let i=1; i <= amount; i++) {
+    for (let amt=1; amt <= amount; amt++) {
         for (const coin of coins) {
-            if (coin <= i) {
-                dp[i] = Math.min(dp[i], 1 + dp[i-coin])
+            if (coin <= amt) {
+                dp[amt] = Math.min(dp[amt], 1 + (amt-coin < 0 ? Infinity : dp[amt-coin]))
             }
         }
     }
     return dp[amount] === Infinity ? -1 : dp[amount]
-}
+};
