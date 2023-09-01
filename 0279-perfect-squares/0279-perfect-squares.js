16c2d454f714720e836d3cbcfa72dd0fdf33dc02
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-08-31 20:38:34 -0700

    Time: 169 ms (71.55%), Space: 45.8 MB (52.21%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.js
+++ 0279-perfect-squares/0279-perfect-squares.js
@@ -3,24 +3,14 @@
  * @return {number}
  */
 var numSquares = function(n) {
-    const squares = findSquares(n)
     const dp = Array(n+1).fill(Infinity)
-    dp[0] = 0
-    dp[1] = 1 
-    for (let i=2; i <= n; i++) {
-        for (const sqr of squares) {
-            if (sqr <= i) {
-                dp[i] = Math.min(dp[i], 1 + dp[i-sqr])
+    dp[0] = 0, dp[1] = 1, dp[2] = 2
+    for (let i=3; i <= n; i++) {
+        for (let j=1; j * j <= i; j++) {
+            if (j * j <= i) {
+                dp[i] = Math.min(dp[i], 1 + dp[i-j*j])
             }
         }
     }
     return dp[n]
-    
-    function findSquares(n) {
-        const res = []
-        for (let i=1; i * i <= n; i++) {
-            res.push(i * i)
-        }
-        return new Set(res)
-    }
 };
