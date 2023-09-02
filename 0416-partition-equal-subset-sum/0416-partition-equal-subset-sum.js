cd193b9262b7b3dad8484c6b2ef173f2e50b4a74
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-01 20:49:20 -0700

    Time: 179 ms (73.28%), Space: 42.5 MB (99.74%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.js
@@ -6,10 +6,10 @@ var canPartition = function(nums) {
     const sum = nums.reduce((acc, val) => acc + val, 0)
     if (sum % 2 !== 0) return false
     const halfSum = sum / 2
-    const dp = Array(halfSum + 1).fill(false)
+    const dp = Array(halfSum+1).fill(false)
     dp[0] = true
     for (let i=0; i < nums.length; i++) {
-        for (let j=halfSum; j >= nums[i]; j--) {
+        for (let j=halfSum; j >= i; j--) {
             if (dp[j-nums[i]] === true) {
                 dp[j] = true
             }
@@ -17,41 +17,3 @@ var canPartition = function(nums) {
     }
     return dp[halfSum]
 };
-
-canPartition = function(nums) {
-    const sum = nums.reduce((acc, val) => acc + val, 0)
-    if (sum % 2 !== 0) return false
-    const halfSum = sum / 2
-    const n = nums.length
-    const dp = Array(n+1).fill(0).map(() => Array(halfSum + 1).fill(false))
-    dp[0][0] = true
-    for (let i=1; i <= n; i++) {
-        for (let j=1; j <= halfSum; j++) {
-            dp[i][j] = dp[i-1][j]
-            if (nums[i-1] <= j) {
-                dp[i][j] = dp[i][j] || dp[i-1][j-nums[i-1]]
-            }
-            if (j === halfSum && dp[i][j] === true) return true
-        }
-    }
-    return false
-}
-
-canPartition = function(nums) {
-    const sum = nums.reduce((acc, val) => acc + val, 0)
-    if (sum % 2 !== 0) return false
-    nums.sort((a, b) => a - b)
-    const halfSum = sum / 2
-    const dp = {}
-    return helper(0, halfSum)
-    
-    function helper(idx, amount) {
-        const key = `${idx} ${amount}`
-        if (amount === 0) return true
-        if (key in dp) return dp[key]
-        for (let i=idx; i < nums.length; i++) {
-            if (nums[i] <= amount && helper(i+1, amount-nums[i])) return true
-        }
-        return dp[key] = false
-    }
-}
