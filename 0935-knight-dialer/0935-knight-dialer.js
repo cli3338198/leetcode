aa78b20dae53b1d8a803764ab80fe21943a0e182
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-04 21:05:20 -0700

    Time: 185 ms (54.17%), Space: 57 MB (54.17%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.js
+++ 0935-knight-dialer/0935-knight-dialer.js
@@ -39,3 +39,18 @@ knightDialer = function(n) {
     }
     return dp.reduce((acc, val) => acc + val, 0) % mod
 }
+
+knightDialer = function(n) {
+    const mod = Math.pow(10, 9) + 7
+    const moves = [[4, 6], [6, 8], [7, 9], [4, 8], [0, 3, 9], [], [0, 1, 7], [2, 6], [1, 3], [2, 4]]
+    const dp = Array(n+1).fill(0).map(() => Array(10).fill(0))
+    dp[1].fill(1)
+    for (let numMoves=2; numMoves <= n; numMoves++) {
+        for (let i=0; i < 10; i++) {
+            for (const next of moves[i]) {
+                dp[numMoves][i] += dp[numMoves-1][next] % mod
+            }
+        }
+    }
+    return dp[n].reduce((acc, val) => acc + val, 0) % mod
+}
