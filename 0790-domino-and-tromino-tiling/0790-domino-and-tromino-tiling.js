bff2e80cbfd02675800d141ecf5b5fe31851175f
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-04 20:37:10 -0700

    Time: 43 ms (99.31%), Space: 43.7 MB (60.42%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.js
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.js
@@ -3,15 +3,15 @@
  * @return {number}
  */
 var numTilings = function(n) {
-    const mod = 10 ** 9 + 7
-    const dp = Array(n+1).fill(0)
-    dp[1] = 1
-    dp[2] = 2
-    dp[3] = 5
-    for (let i=4; i <= n; i++) {
-        // dp[i] = dp[i-1] + dp[i-2] + 2*(dp[i-3] + ... dp[0])
-        dp[i] = ((2 * dp[i-1] % mod) + (dp[i-3] % mod)) % mod
-    }
-    return dp[n] % mod
+    const mod = Math.pow(10, 9) + 7
+    const dp = {}
+    return helper(n) % mod
     
+    function helper(n) {
+        if (n in dp) return dp[n]
+        if (n === 1) return 1
+        if (n === 2) return 2
+        if (n === 3) return 5
+        return dp[n] = (2 * helper(n-1) + helper(n-3)) % mod
+    }
 };
