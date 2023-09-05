f71d665052208e6f7c30df728e4175c4b4553cc5
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-04 21:12:07 -0700

    Time: 874 ms (70.19%), Space: 19.2 MB (54.40%) - LeetHub

--- 0935-knight-dialer/0935-knight-dialer.py
+++ 0935-knight-dialer/0935-knight-dialer.py
@@ -1,17 +1,11 @@
 class Solution:
     def knightDialer(self, n: int) -> int:
-        
-        mod = 1000000007
+        mod = pow(10, 9) + 7
         moves = [[4, 6], [6, 8], [7, 9], [4, 8], [0, 3, 9], [], [0, 1, 7], [2, 6], [1, 3], [2, 4]]
-        dp = [[0] * 10 for _ in range(n+1)]
-        
-        for i in range(10):
-            dp[1][i] = 1
-            
-        for i in range(2, n+1):
-            for j in range(0, 10):
-                for next in moves[j]:
-                    dp[i][j] += dp[i-1][next] % mod
-                    
-        return sum(dp[n]) % mod
-        
+        dp = [[0] * 10 for _ in range(0, n+1)]
+        dp[1] = [1] * 10
+        for move in range(2, n+1):
+            for i in range(0, 10):
+                for next in moves[i]:
+                    dp[move][i] += dp[move-1][next] % mod
+        return sum(dp[-1]) % mod
