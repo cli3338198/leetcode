d600c3303ebc1d779fb0c3f12b33627d2f8b0fea
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-04 20:41:00 -0700

    Time: 53 ms (75.00%), Space: 41.7 MB (97.92%) - LeetHub

--- 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.js
+++ 0790-domino-and-tromino-tiling/0790-domino-and-tromino-tiling.js
@@ -15,3 +15,15 @@ var numTilings = function(n) {
         return dp[n] = (2 * helper(n-1) + helper(n-3)) % mod
     }
 };
+
+numTilings = function(n) {
+    const mod = Math.pow(10, 9) + 7
+    const dp = Array(n+1).fill(0)
+    dp[1] = 1
+    dp[2] = 2
+    dp[3] = 5
+    for (let i=4; i <= n; i++) {
+        dp[i] = ((2 * dp[i-1] % mod) + (dp[i-3] % mod)) % mod
+    }
+    return dp[n] % mod
+}
