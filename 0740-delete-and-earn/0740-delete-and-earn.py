187bc02ce72ea2807278fecf92123c56666bbf44
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-04 20:23:28 -0700

    Time: 79 ms (87.53%), Space: 18.2 MB (81.30%) - LeetHub

--- 0740-delete-and-earn/0740-delete-and-earn.py
+++ 0740-delete-and-earn/0740-delete-and-earn.py
@@ -1,16 +1,9 @@
 class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
-        
-        max_ = max(nums)
-        dp = [0] * (max_ + 1)
-        freq = [0] * (max_ + 1)
-        
+        _max = max(nums)
+        freq, dp = [0] * (_max + 1), [0] * (_max + 1)
         for n in nums:
             freq[n] += 1
-            
-        dp[1] = 1 * freq[1]
-            
-        for i in range(2, max_ + 1):
+        for i in range(1, _max+1):
             dp[i] = max(dp[i-1], dp[i-2] + i * freq[i])
-            
-        return dp[max_]
+        return dp[_max]
