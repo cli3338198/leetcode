01bbeb0fceb59f0bd2d6b69f2adfa8c7167b6f5b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-06 12:38:52 -0700

    Time: 74 ms (86.21%), Space: 50.4 MB (93.10%) - LeetHub

---
+++ 1871-jump-game-vii/1871-jump-game-vii.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @param {number} minJump
+ * @param {number} maxJump
+ * @return {boolean}
+ */
+var canReach = function(s, minJump, maxJump) {
+    const n = s.length
+    const dp = Array(n).fill(false)
+    dp[0] = true
+    let jumpPoints = 0
+    for (let i=1; i < n; i++) {
+        if (i > maxJump) {
+            jumpPoints -= dp[i-maxJump-1]
+        }
+        if (i >= minJump) {
+            jumpPoints += dp[i-minJump]
+        }
+        if (jumpPoints > 0 && s[i] === '0') {
+            dp[i] = true
+        }
+    }
+    return dp[n-1]
+};
