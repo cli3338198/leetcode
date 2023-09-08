859554118a638864fafc9c133dae44a413e26104
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-07 21:02:54 -0700

    Time: 124 ms (52.98%), Space: 52.2 MB (92.01%) - LeetHub

--- 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.js
+++ 1218-longest-arithmetic-subsequence-of-given-difference/1218-longest-arithmetic-subsequence-of-given-difference.js
@@ -18,8 +18,7 @@ longestSubsequence = function(arr, diff) {
     const dp = {}
     let res = 0
     for (const cur of arr) {
-        dp[cur] = 1 + (dp[cur-diff] || 0)
-        res = Math.max(res, dp[cur])
+        res = Math.max(res, dp[cur] = 1 + (dp[cur-diff] || 0))
     }
     return res
 }
