e68c5225796f7144d07e2f730c9e219356ae9e31
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-08 21:01:35 -0700

    Time: 89 ms (23.08%), Space: 67.1 MB (7.69%) - LeetHub

---
+++ 1262-greatest-sum-divisible-by-three/1262-greatest-sum-divisible-by-three.js
@@ -0,0 +1,17 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var maxSumDivThree = function(nums) {
+    const n = nums.length
+    const dp = Array(n+1).fill(0).map(() => Array(3).fill(0))
+    for (let i=1; i <= n; i++) {
+        dp[i] = dp[i-1].slice()
+        for (let j=0; j < 3; j++) {
+            const sum = dp[i-1][j] + nums[i-1]
+            const mod = sum % 3
+            dp[i][mod] = Math.max(dp[i][mod], sum)
+        }
+    }
+    return dp[n][0]
+};
