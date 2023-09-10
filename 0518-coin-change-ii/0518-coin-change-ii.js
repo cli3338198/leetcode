6cf3e35491410f23df9b4c911228cbce0527b85b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-09 20:16:46 -0700

    Time: 71 ms (44.36%), Space: 70.2 MB (16.85%) - LeetHub

---
+++ 0518-coin-change-ii/0518-coin-change-ii.js
@@ -0,0 +1,20 @@
+/**
+ * @param {number} amount
+ * @param {number[]} coins
+ * @return {number}
+ */
+var change = function(amount, coins) {
+    const dp = Array(coins.length+1).fill(0).map(() => Array(amount+1).fill(0))
+    for (let i=0; i <= coins.length; i++) {
+        dp[i][0] = 1
+    }
+    for (let i=1; i <= coins.length; i++) {
+        for (let j=1; j <= amount; j++) {
+            dp[i][j] += dp[i-1][j]
+            if (coins[i-1] <= j) {
+                dp[i][j] += dp[i][j-coins[i-1]]
+            }
+        }
+    }
+    return dp[coins.length][amount]
+};
