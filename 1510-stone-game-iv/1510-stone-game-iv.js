9063e418a4357a48613d4c33246265432e9bab27
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-11 22:16:44 -0700

    Time: 71 ms (87.50%), Space: 44 MB (100.00%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.js
+++ 1510-stone-game-iv/1510-stone-game-iv.js
@@ -26,3 +26,16 @@ var winnerSquareGame = function(n) {
         return dp[n] = false
     }
 };
+
+winnerSquareGame = function(n) {
+    const dp = Array(n+1).fill(false)
+    for (let i=1; i <= n; i++) {
+        for (let j=1; j*j <= i; j++) {
+            if (dp[i-j*j] === false) {
+                dp[i] = true
+                break
+            }
+        }
+    }
+    return dp[n]
+}
