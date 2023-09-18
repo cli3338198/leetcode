c8d3c5bcf5aa8962f01aeca37c8e39ad6803beda
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-18 14:36:18 -0700

    Time: 195 ms (93.84%), Space: 39.4 MB (39.13%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.kt
+++ 0045-jump-game-ii/0045-jump-game-ii.kt
@@ -1,16 +1,31 @@
 class Solution {
     fun jump(nums: IntArray): Int {
-        var i = 0
+        // var i = 0
+        // var lastPosition = 0
+        // var maxReach = 0
+        // var jumps = 0
+        // while (lastPosition < nums.size-1) {
+        //     maxReach = Math.max(maxReach, i + nums[i])
+        //     if (i === lastPosition) {
+        //         lastPosition = maxReach
+        //         jumps++
+        //     }
+        //     i++
+        // }
+        // return jumps
+        
+        var jumps = 0
         var lastPosition = 0
         var maxReach = 0
-        var jumps = 0
-        while (lastPosition < nums.size-1) {
+        for (i in 0..nums.size-1) {
+            if (lastPosition === nums.size-1) {
+                break
+            }
             maxReach = Math.max(maxReach, i + nums[i])
             if (i === lastPosition) {
                 lastPosition = maxReach
                 jumps++
             }
-            i++
         }
         return jumps
     }
