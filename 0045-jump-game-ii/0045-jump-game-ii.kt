2df89821944c04b74863759f1550b844fa29d5dc
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-18 14:49:11 -0700

    Time: 193 ms (94.57%), Space: 39.1 MB (56.52%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.kt
+++ 0045-jump-game-ii/0045-jump-game-ii.kt
@@ -18,10 +18,15 @@ class Solution {
         var lastPosition = 0
         var maxReach = 0
         for (i in 0..nums.size-1) {
+            // have reached end, break
             if (lastPosition === nums.size-1) {
                 break
             }
+            // update the farthest we can jump within the current "window"
             maxReach = Math.max(maxReach, i + nums[i])
+            // we've reached the end of our "window"
+            // set the start of our next "window" to the farthest we can reach from the prev window
+            // increment the jumps
             if (i === lastPosition) {
                 lastPosition = maxReach
                 jumps++
