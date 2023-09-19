ad64a23a9dab6272bba2975fe18ddd5cdf0a4ea3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-18 20:00:52 -0700

    Time: 216 ms (73.55%), Space: 44.4 MB (8.40%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,23 +1,11 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        @lru_cache(None)
+        def backtrack(i: int, cur: int) -> int:
+            nonlocal nums, target
+            if i >= len(nums):
+                return 1 if cur == target else 0
+            return backtrack(i+1, cur+nums[i]) + backtrack(i+1, cur-nums[i])
         
-        n = len(nums)
-        total_sum = sum(nums)
-        
-        if abs(target) > total_sum or (total_sum + target) % 2 != 0: 
-            return 0
-        
-        s1 = (total_sum + target) // 2
-        dp = [[0] * (s1 + 1) for _ in range(n + 1)]
-        dp[0][0] = 1
-        
-        for i in range(1, n+1):
-            for j in range(s1 + 1):
-                dp[i][j] += dp[i-1][j]
-                
-                if nums[i-1] <= j:
-                    dp[i][j] += dp[i-1][j-nums[i-1]]
-        
-        return dp[n][s1]
-        
-        
+        return backtrack(0, 0)
+            
