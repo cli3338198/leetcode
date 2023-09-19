3be1c86ecaad8891acbc422cb04f47932d2d8b66
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-18 20:20:04 -0700

    Time: 1289 ms (5.00%), Space: 21.9 MB (60.24%) - LeetHub

--- 0494-target-sum/0494-target-sum.py
+++ 0494-target-sum/0494-target-sum.py
@@ -1,5 +1,21 @@
 class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
+        def get_key(a: int, b: int) -> str:
+            return f"{a} {b}"
+        
+        total = sum(nums)
+        dp = defaultdict(lambda: 0)
+        dp[get_key(0, nums[0]+total)] = 1
+        dp[get_key(0, -nums[0]+total)] += 1
+        for i in range(1, len(nums)):
+            for j in range(-total, total+1):
+                if dp[get_key(i-1, j+total)] > 0:
+                    dp[get_key(i, j+nums[i]+total)] += dp[get_key(i-1, j+total)]
+                    dp[get_key(i, j-nums[i]+total)] += dp[get_key(i-1, j+total)]
+        return 0 if abs(target) > total else dp[get_key(len(nums)-1, target+total)]
+        
+        #
+        
         @lru_cache(None)
         def backtrack(i: int, cur: int) -> int:
             nonlocal nums, target
