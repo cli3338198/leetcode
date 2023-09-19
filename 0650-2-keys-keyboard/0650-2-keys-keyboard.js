9a727023726c419d4ca59c9f48a6a09b7bd544fa
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-18 21:27:49 -0700

    Time: 99 ms (34.29%), Space: 42.8 MB (51.43%) - LeetHub

--- 0650-2-keys-keyboard/0650-2-keys-keyboard.js
+++ 0650-2-keys-keyboard/0650-2-keys-keyboard.js
@@ -4,14 +4,12 @@
  */
 var minSteps = function(n) {
     const dp = Array(n+1).fill(0).map((_,i) => i)
-    dp[0] = 0
     dp[1] = 0
-    dp[2] = 2
-    dp[3] = 3
     for (let i=4; i <= n; i++) {
         for (let j=2; j <= i; j++) {
+            // can use some number of j subunits to make i
             if (i % j === 0) {
-                dp[i] = Math.min(dp[i], (i/j) + dp[j])
+                dp[i] = Math.min(dp[i], (i / j) + dp[j])
             }
         }
     }
