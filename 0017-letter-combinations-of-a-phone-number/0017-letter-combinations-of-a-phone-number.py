ba30d677c6941bf6ef5f86f69393792e9b35e464
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-19 11:32:56 -0700

    Time: 41 ms (41.96%), Space: 16.3 MB (81.33%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,6 +1,19 @@
 class Solution:
     def letterCombinations(self, digits: str) -> List[str]:
-        mp, res = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}, []
+        mp, res = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}, deque([])
+        
+        if len(digits) == 0:
+            return res
+        res.append("")
+        for i in range(0, len(digits)):
+            length = len(res)
+            for j in range(0, length):
+                cur = res.popleft()
+                for k in mp[str(digits[i])]:
+                    res.append(cur + k)
+        return list(res)
+        
+        # 
         
         def backtrack(idx: int, cur: str) -> None:
             nonlocal res, mp, digits
