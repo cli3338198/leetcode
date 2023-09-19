79ee9d4f67204fbf1ecf21c65f2904bba2f17af9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-19 12:41:59 -0700

    Time: 45 ms (98.10%), Space: 16.4 MB (51.07%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,5 +1,16 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
+        candidates.sort()
+        dp = defaultdict(lambda: [])
+        dp[0] = [[]]
+        for c in candidates:
+            for t in range(1, target+1):
+                for combo in dp[t-c]:
+                    dp[t].append([*combo, c])
+        return dp[target]
+        
+        #
+        
         res = []
         
         def backtrack(idx: int, cur: List[int], target: int) -> None:
