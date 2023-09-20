477a2d50fc25a88d70c10d71daba01742893a673
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-09-19 23:17:58 -0700

    Time: 103 ms (39.29%), Space: 51.7 MB (46.43%) - LeetHub

--- 0576-out-of-boundary-paths/0576-out-of-boundary-paths.js
+++ 0576-out-of-boundary-paths/0576-out-of-boundary-paths.js
@@ -24,3 +24,24 @@ var findPaths = function(m, n, maxMove, startRow, startColumn) {
         return dp[key] = res % mod
     }
 };
+
+findPaths = function(m, n, maxMove, startRow, startColumn) {
+    const mod = Math.pow(10, 9) + 7
+    let dp = Array(m).fill(0).map(() => Array(n).fill(0))
+    dp[startRow][startColumn] = 1
+    let res = 0
+    for (let move=1; move <= maxMove; move++) {
+        const nextDp = Array(m).fill(0).map(() => Array(n).fill(0))
+        for (let r=0; r < m; r++) {
+            for (let c=0; c < n; c++) {
+                if (r === 0) res = (res + dp[r][c]) % mod
+                if (r === m-1) res = (res + dp[r][c] % mod)
+                if (c === 0) res = (res + dp[r][c]) % mod
+                if (c === n-1) res = (res + dp[r][c]) % mod
+                nextDp[r][c] = ((r > 0 ? dp[r-1][c] : 0) + (r < m-1 ? dp[r+1][c] : 0)) % mod + ((c > 0 ? dp[r][c-1] : 0) + (c < n-1 ? dp[r][c+1] : 0)) % mod
+            }
+        }
+        dp = nextDp
+    }
+    return res
+}
