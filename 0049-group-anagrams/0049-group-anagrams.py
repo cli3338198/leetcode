ec24d7408f39e52c49eb9677231191fb59f71ad2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-06 13:35:46 -0700

    Time: 103 ms (46.39%), Space: 20.1 MB (54.33%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -3,6 +3,13 @@ class Solution:
         return "".join(sorted([c for c in word]))
     
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        mp = defaultdict(list)
+        for st in strs:
+            mp["".join(sorted(st))].append(st)
+        return mp.values()
+        
+        # 
+        
         mp = {}
         for st in strs:
             ana = self.str_to_ana(st)
