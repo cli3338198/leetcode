1354cf3765d53c6a5025e30a7c43be69718aeae2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-10 15:52:21 -0700

    Time: 71 ms (36.48%), Space: 47 MB (26.16%) - LeetHub

--- 0046-permutations/0046-permutations.ts
+++ 0046-permutations/0046-permutations.ts
@@ -1,19 +1,16 @@
-type TDp = [number[], Set<number>]
-
 function permute(nums: number[]): number[][] {
-    const n = nums.length
-    const dp: TDp[][] = Array(n+1).fill(0).map(() => ([] as unknown as TDp[]))
-    dp[1] = nums.map((n, i) => ([[n], new Set([i])]))
-    for (let len=2; len <= n; len++) {
-        for (const [list, set] of dp[len-1]) {
-            for (let i=0; i < n; i++) {
-                if (!set.has(i)) {
-                    const newSet = new Set(Array.from(set))
-                    newSet.add(i)
-                    dp[len].push([[...list, nums[i]], newSet])
-                }
+    const dp: Record<string, number[][]> = {}
+    return helper(nums)
+    
+    function helper(nums) {
+        if (nums.length === 1) return [nums]
+        if (nums.toString() in dp) return dp[nums.toString()]
+        const res: number[][] = []
+        for (let i=0; i < nums.length; i++) {
+            for (const j of helper(nums.slice(0, i).concat(nums.slice(i+1)))) {
+                res.push([nums[i], ...j])
             }
         }
+        return dp[nums.toString()] = res
     }
-    return dp[n].map((x) => x[0])
 };
