93b487fc0ed488624f65b7174456dac31f996d67
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-17 21:55:52 -0700

    Time: 42 ms (75.46%), Space: 17 MB (76.98%) - LeetHub

--- 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
+++ 0102-binary-tree-level-order-traversal/0102-binary-tree-level-order-traversal.py
@@ -13,6 +13,23 @@ class Solution:
         self.dfs(mp, root.right, lvl+1)
     
     def levelOrder(self, root: Optional[TreeNode]) -> List[List[int]]:
+        if not root:
+            return []
+        q, res = deque([root]), []
+        while q:
+            subres, l = [], len(q)
+            for i in range(0, l):
+                j = q.popleft()
+                subres.append(j.val)
+                if j.left:
+                    q.append(j.left)
+                if j.right:
+                    q.append(j.right)
+            res.append(subres)
+        return res
+        
+        #
+        
         mp = defaultdict(list)
         self.dfs(mp, root, 0)
         return [x for _, x in sorted(mp.items(), key=lambda x: x[0])]
