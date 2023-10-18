f30d0ac6155d31d9af2ead741a05a869d3036c4c
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-17 20:56:16 -0700

    Time: 82 ms (43.88%), Space: 19.2 MB (86.53%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.py
@@ -1,39 +1,33 @@
 # Definition for singly-linked list.
-# class ListNode:
+# class ListNode(object):
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
-class Solution:
-    def mergeKLists(self, lists: List[Optional[ListNode]]) -> Optional[ListNode]:
-        
-        if not len(lists):
+class Solution(object):
+    def merge(self, list1, list2):
+        new_list = ListNode()
+        l0, l1, l2 = new_list, list1, list2
+        while l1 and l2:
+            if l1.val < l2.val:
+                l0.next = l1
+                l1 = l1.next
+            else:
+                l0.next = l2
+                l2 = l2.next
+            l0 = l0.next
+        if l1:
+            l0.next = l1
+        if l2:
+            l0.next = l2
+        return new_list.next            
+    
+    def mergeKLists(self, lists):
+        """
+        :type lists: List[ListNode]
+        :rtype: ListNode
+        """
+        if len(lists) == 0:
             return None
-        
         if len(lists) == 1:
             return lists[0]
-        
-        def merge(left: List[Optional[ListNode]], right: List[Optional[ListNode]]) -> Optional[ListNode]:
-            
-            dummy = ListNode()
-            temp = dummy
-            
-            while left and right:
-                if left.val < right.val:
-                    temp.next = left
-                    left = left.next
-                
-                else:
-                    temp.next = right
-                    right = right.next
-                
-                temp = temp.next
-            
-            if left: temp.next = left
-                
-            if right: temp.next = right
-            
-            return dummy.next
-    
-        mid = len(lists) // 2
-        
-        return merge(self.mergeKLists(lists[0:mid:]), self.mergeKLists(lists[mid::]))
+        return self.merge(self.mergeKLists(lists[0:len(lists)//2:]), self.mergeKLists(lists[len(lists)//2::]))
