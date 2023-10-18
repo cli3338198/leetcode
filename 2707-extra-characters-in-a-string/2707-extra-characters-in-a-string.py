6fa78c295ddf75e4651e6d6c85eb7194b1a88388
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-18 12:53:48 -0700

    Time: 939 ms (5.00%), Space: 17.3 MB (15.50%) - LeetHub

--- 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
+++ 2707-extra-characters-in-a-string/2707-extra-characters-in-a-string.py
@@ -1,5 +1,42 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+    
+    def insert(self, word: str) -> None:
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = Trie()
+            cur = cur.children[c]
+        cur.end = True
+        
+    def find(self, word: str, cur) -> bool:
+        if len(word) == 0:
+            return cur.end
+        if word[0] not in cur.children:
+            return False
+        for child in cur.children:
+            if word[0] == child and self.find(word[1::], cur.children[child]):
+                return True
+        return False
+
 class Solution:
     def minExtraChar(self, s: str, dictionary: List[str]) -> int:
+        trie = Trie()
+        for word in dictionary:
+            trie.insert(word)
+        n = len(s)
+        dp = [0] * (n+1)
+        for i in range(1, n+1):
+            dp[i] = 1 + dp[i-1]
+            for j in range(0, i):
+                if trie.find(s[j:i:], trie):
+                    dp[i] = min(dp[i], dp[j])
+        return dp[n]
+        
+        #
+        
         n = len(s)
         dp = [0] * (n+1)
         for i in range(1, n+1):
