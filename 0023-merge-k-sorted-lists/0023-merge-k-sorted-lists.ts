6edc6c819fa7b2f9830fc5401161c9262570ba43
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-17 21:32:47 -0700

    Time: 104 ms (60.41%), Space: 52.5 MB (10.80%) - LeetHub

--- 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.ts
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.ts
@@ -10,7 +10,70 @@
  * }
  */
 
+class Heap<T> {
+    public h: T[]
+    private cmp: (p: number, c: number) => boolean
+    constructor(compare: (a: T, b: T) => boolean) {
+        this.h = []
+        this.cmp = this.buildCompare(compare)
+    }
+    push(a: T) {
+        this.h.push(a)
+        this.bubbleUp(this.h.length-1)
+    }
+    bubbleUp(c: number) {
+        let p = Math.ceil(c/2) + 1
+        while (p in this.h && this.cmp(p, c)) {
+            this.swap(p, c)
+            c = p
+            p = Math.ceil(c/2) + 1
+        }
+    }
+    pop() {
+        if (this.h.length > 1) {
+            const temp = this.h[0]
+            this.h[0] = this.h.pop()
+            this.heapify(0)
+            return temp
+        } else {
+            return this.h.pop()
+        }
+    }
+    heapify(p: number) {
+        let l = 2 * p + 1
+        let r = 2 * p + 2
+        while ((l in this.h && this.cmp(p, l)) || (r in this.h && this.cmp(p, r))) {
+            let t = p
+            if (l in this.h && this.cmp(p, l)) t = l
+            if (r in this.h && this.cmp(p, r)) t = r
+            if (t !== p) {
+                this.swap(t, p)
+                p = t
+                l = 2 * p + 1
+                r = 2 * p + 2
+            }
+        }
+    }
+    swap(p: number, c: number) {
+        [this.h[p], this.h[c]] = [this.h[c], this.h[p]]
+    }
+    buildCompare(compare: (a: T, b: T) => boolean) {
+        return (p: number, c: number) => compare(this.h[p], this.h[c])
+    }
+}
+
 function mergeKLists(lists: Array<ListNode | null>): ListNode | null {
+    const heap = new Heap<ListNode>((a: ListNode, b: ListNode) => a.val > b.val)
+    for (const list of lists) {
+        if (list) heap.push(list)
+    }
+    while (heap.h.length > 1) {
+        heap.push(merge(heap.pop(), heap.pop()))
+    }
+    return heap.h.length ? heap.pop() : null
+    
+    // 
+    
     if (lists.length === 0) {
         return null
     }
