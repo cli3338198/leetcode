f36a67a632f3fd483bb8b52ae406b5858901290e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-19 09:34:42 -0700

    Time: 76 ms (44.62%), Space: 48.5 MB (36.92%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.ts
+++ 0039-combination-sum/0039-combination-sum.ts
@@ -1,14 +1,16 @@
 function combinationSum(candidates: number[], target: number): number[][] {
-    candidates.sort((a, b) => a - b)
-    const dp: Record<string, number[][]> = {}
-    dp[0] = [[]]
-    for (const candidate of candidates) {
-        for (let t=1; t <= target; t++) {
-            if (!(t in dp)) dp[t] = []
-            for (const sub of dp[t-candidate] || []) {
-                dp[t].push([...sub, candidate])
+    const res: number[][] = []
+    backtrack(0, target, [])
+    return res
+    
+    function backtrack(i: number, target: number, curList: number[]) {
+        if (target === 0) {
+            return res.push(curList)
+        }
+        for (let j=i; j < candidates.length; j++) {
+            if (candidates[j] <= target) {
+                backtrack(j, target-candidates[j], [...curList, candidates[j]])
             }
         }
     }
-    return dp[target]
 };
