a4417400a11ad78e1d9e57520b8a70617db73a3d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-19 09:54:59 -0700

    Time: 165 ms (8.99%), Space: 29.3 MB (13.88%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -1,27 +1,10 @@
 class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
-        candidates.sort()
-        dp = defaultdict(lambda: [])
-        dp[0] = [[]]
-        for c in candidates:
-            for t in range(1, target+1):
-                for combo in dp[t-c]:
-                    dp[t].append([*combo, c])
-        return dp[target]
-        
-        #
-        
-        res = []
-        
-        def backtrack(idx: int, cur: List[int], target: int) -> None:
-            nonlocal res, candidates
-            if target == 0:
-                res.append(cur)
-                return
-            else:
-                for i in range(idx, len(candidates)):
-                    if candidates[i] <= target:
-                        backtrack(i, [*cur, candidates[i]], target-candidates[i])
-        
-        backtrack(0, [], target)
-        return res
+        dp = [[] for _ in range(0, target+1)]
+        dp[0].append([])
+        for i in range(0, target+1):
+            for c in candidates:
+                if c <= i:
+                    for j in dp[i-c]:
+                        dp[i].append(j + [c])
+        return list(set(map(tuple, map(sorted, dp[target]))))
