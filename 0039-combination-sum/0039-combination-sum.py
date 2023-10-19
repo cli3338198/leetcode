2805c116773eef8791ede4b37e7da19466bcb84b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-19 09:57:17 -0700

    Time: 56 ms (77.42%), Space: 16.3 MB (68.01%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.py
+++ 0039-combination-sum/0039-combination-sum.py
@@ -2,6 +2,17 @@ class Solution:
     def combinationSum(self, candidates: List[int], target: int) -> List[List[int]]:
         dp = [[] for _ in range(0, target+1)]
         dp[0].append([])
+        for c in candidates:
+            for t in range(1, target+1):
+                if c <= t:
+                    for j in dp[t-c]:
+                        dp[t].append(j + [c])
+        return dp[target]
+        
+        #
+        
+        dp = [[] for _ in range(0, target+1)]
+        dp[0].append([])
         for i in range(0, target+1):
             for c in candidates:
                 if c <= i:
