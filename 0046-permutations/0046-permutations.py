d2458a9e4f274aef39a4f3f0b02b140aa5a52cc9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-20 22:11:43 -0700

    Time: 18 ms (79.67%), Space: 13.4 MB (43.58%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,16 +1,15 @@
-class Solution:
-    def permute(self, nums: List[int]) -> List[List[int]]:
-        
-        def helper(nums: List[int]) -> List[int]:
-            res = []
-            
-            if len(nums) == 1:
-                return [nums]
-            
-            for i in range(len(nums)):
-                for sub in helper(nums[0:i:] + nums[i+1::]):
-                    res.append([*sub, nums[i]])
-                    
-            return res
-        
-        return helper(nums)
+class Solution(object):
+    def permute(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: List[List[int]]
+        """
+        n = len(nums)
+        dp = [[] for _ in range(0, n+1)]
+        dp[0].append([])
+        for i in range(1, n+1):
+            for j in dp[i-1]:
+                l = len(j)
+                for k in range(0, l+1):
+                    dp[i].append(j[0:k:] + [nums[i-1]] + j[k::])
+        return dp[n]
