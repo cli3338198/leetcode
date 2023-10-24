63145f0e18078c665118be0d80158bde2c7762c8
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-23 20:16:56 -0700

    Time: 65 ms (27.44%), Space: 45.6 MB (8.66%) - LeetHub

--- 0078-subsets/0078-subsets.ts
+++ 0078-subsets/0078-subsets.ts
@@ -1,4 +1,17 @@
 function subsets(nums: number[]): number[][] {
+    const ans: number[][] = [[]]
+    for (let i=0; i < nums.length; i++) {
+        if (i > 0 && nums[i] === nums[i-1]) continue
+        const sub: number[][] = []
+        for (const a of ans) {
+            sub.push([...a, nums[i]])
+        }
+        ans.push(...sub)
+    }
+    return ans
+    
+    // 
+    
     nums.sort()
     const res: number[][] = []
     dfs(0, [])
