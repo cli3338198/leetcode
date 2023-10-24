0859e4cd8a25bd4a593f6d009fbaa9afe8a91b8b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-23 21:51:05 -0700

    Time: 27 ms (85.20%), Space: 13.2 MB (99.48%) - LeetHub

--- 0051-n-queens/0051-n-queens.py
+++ 0051-n-queens/0051-n-queens.py
@@ -1,51 +1,29 @@
-class Solution:
-    def solveNQueens(self, n: int) -> List[List[str]]:
+class Solution(object):
+    def solveNQueens(self, n):
+        """
+        :type n: int
+        :rtype: List[List[str]]
+        """
+        def convert(arr):
+            return ["".join(row) for row in arr]
         
-        board = [["."] * n for _ in range(n)]
-        res = []
-        
-        def clone(board: List[List[int]]) -> List[List[int]]:
-            
-            b = []
-            
-            for r in range(n):
-                row = []
-                for c in range(n):
-                    row.append(board[r][c])
-                    
-                b.append("".join(row))
-            
-            return b
-        
-        def is_valid(r: int, c: int, set_: set) -> bool:
-            
-            if f"r{r}" in set_: return False
-            if f"c{c}" in set_: return False
-            if f"+{r+c}" in set_: return False
-            if f"-{r-c}" in set_: return False
-            
-            return True
-            
-        
-        def helper(r: int, set_: set):
+        res, arr = [], [["."] * n for _ in range(0, n)]
+
+        def dfs(r, arr, col, pos, neg):
             if r == n:
-                res.append(clone(board))
+                res.append(convert(arr))
                 return
+            for c in range(0, n):
+                if c not in col and r + c not in pos and r - c not in neg:
+                    col.add(c)
+                    pos.add(r + c)
+                    neg.add(r - c)
+                    arr[r][c] = "Q"
+                    dfs(r+1, arr, col, pos, neg)
+                    col.remove(c)
+                    pos.remove(r + c)
+                    neg.remove(r - c)
+                    arr[r][c] = "."
             
-            for c in range(n):
-                if is_valid(r, c, set_):
-                    set_.add(f"r{r}")
-                    set_.add(f"c{c}")
-                    set_.add(f"+{r+c}")
-                    set_.add(f"-{r-c}")
-                    board[r][c] = "Q"
-                    helper(r+1, set_)
-                    board[r][c] = '.'
-                    set_.discard(f"r{r}")
-                    set_.discard(f"c{c}")
-                    set_.discard(f"+{r+c}")
-                    set_.discard(f"-{r-c}")          
-            
-        helper(0, set())
+        dfs(0, arr, set(), set(), set())
         return res
-        
