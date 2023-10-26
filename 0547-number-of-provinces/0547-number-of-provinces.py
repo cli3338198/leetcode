0b610544eb1acd0d5b51073d72ec6f5a802ed46a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-26 10:32:21 -0700

    Time: 201 ms (83.20%), Space: 17.2 MB (50.89%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,4 +1,15 @@
 class Solution:
+    def bfs(self, isConnected: List[List[int]], city: int, visited: set) -> None:
+        q = deque([city])
+        while q:
+            cur_city = q.popleft()
+            if cur_city in visited:
+                continue
+            visited.add(cur_city)
+            for next_city, yes in enumerate(isConnected[cur_city]):
+                if yes == 1:
+                    q.append(next_city)
+    
     def dfs(self, isConnected: List[List[int]], city: int, visited: set) -> None:
         if city in visited:
             return
@@ -8,6 +19,15 @@ class Solution:
                 self.dfs(isConnected, next_city, visited)
     
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        n, visited, provinces = len(isConnected), set(), 0
+        for city in range(0, n):
+            if city not in visited:
+                provinces += 1
+                self.bfs(isConnected, city, visited)
+        return provinces
+        
+        # 
+        
         n, visited, provinces = len(isConnected), set(), 0
         for city in range(0, n):
             if city not in visited:
