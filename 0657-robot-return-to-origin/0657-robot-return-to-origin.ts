6b4ce3beb40bacc18b4c8c9f54741de598d9038a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-26 09:37:33 -0700

    Time: 74 ms (23.47%), Space: 45.5 MB (37.76%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.ts
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.ts
@@ -1,4 +1,12 @@
 function judgeCircle(moves: string): boolean {
+    const count = {"L": 0, "R": 0, "U": 0, "D": 0}
+    for (const move of moves) {
+        count[move] = count[move] + 1 || 1
+    }
+    return count["L"] === count["R"] && count["U"] === count["D"]
+    
+    //
+    
     let x = 0, y = 0
     for (const move of moves) {
         if (move === "L") {
