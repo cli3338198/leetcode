e6d12423ca37cecce7d15e03a07300eaed90af8d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-26 10:38:51 -0700

    Time: 243 ms (10.60%), Space: 17.1 MB (69.73%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.py
+++ 0547-number-of-provinces/0547-number-of-provinces.py
@@ -1,3 +1,24 @@
+class UF:
+    def __init__(self):
+        self.root = {}
+        self.provinces = 0
+    
+    def insert(self, city: int) -> None:
+        if city not in self.root:
+            self.root[city] = city
+            self.provinces += 1
+    
+    def find(self, city: int) -> int:
+        if self.root[city] != city:
+            self.root[city] = self.find(self.root[city])
+        return self.root[city]
+    
+    def union(self, i: int, j: int) -> None:
+        a, b = self.find(i), self.find(j)
+        if a != b:
+            self.root[b] = a
+            self.provinces -= 1
+
 class Solution:
     def bfs(self, isConnected: List[List[int]], city: int, visited: set) -> None:
         q = deque([city])
@@ -19,6 +40,17 @@ class Solution:
                 self.dfs(isConnected, next_city, visited)
     
     def findCircleNum(self, isConnected: List[List[int]]) -> int:
+        uf = UF()
+        for i in range(0, len(isConnected)):
+            uf.insert(i)
+            for j in range(0, len(isConnected[i])):
+                uf.insert(j)
+                if isConnected[i][j] == 1:
+                    uf.union(i, j)
+        return uf.provinces
+        
+        #
+        
         n, visited, provinces = len(isConnected), set(), 0
         for city in range(0, n):
             if city not in visited:
