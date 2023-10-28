7368646d43f626800652e055e2a178b469f2a4a5
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-10-27 21:01:19 -0700

    Time: 2419 ms (17.90%), Space: 32.2 MB (39.51%) - LeetHub

--- 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
+++ 0416-partition-equal-subset-sum/0416-partition-equal-subset-sum.py
@@ -1,32 +1,30 @@
 class Solution:
     def canPartition(self, nums: List[int]) -> bool:
-        
-        n = len(nums)
-        total_sum = sum(nums)
+        total_sum, n = sum(nums), len(nums)
         half_sum = total_sum // 2
-        
-        if total_sum / 2 != half_sum:
+        if half_sum != total_sum / 2:
             return False
-        
         nums.sort()
-        
-        dp = [[False] * (half_sum + 1) for _ in range(n + 1)]
-        
-        for i in range(n + 1):
-            for j in range(half_sum + 1):
-                if j == 0:
-                    dp[i][j] = True
-                    
-        for i in range(1, n + 1):
-            for j in range(1, half_sum + 1):
-                dp[i][j] = dp[i-1][j]
-                
-                if nums[i-1] <= j:
-                    dp[i][j] = dp[i][j] or dp[i-1][j-nums[i-1]]
-                    
-                if j == half_sum and dp[i][j] == True:
+        dp = [[False] * (half_sum + 1) for _ in range(0, n+1)]
+        dp[0][0] = True
+        for i in range(1, n+1):
+            for cur_sum in range(0, half_sum+1):
+                dp[i][cur_sum] = dp[i-1][cur_sum]
+                if cur_sum == 0:
+                    continue
+                if nums[i-1] <= cur_sum:
+                    dp[i][cur_sum] = dp[i][cur_sum] or dp[i-1][cur_sum-nums[i-1]]
+                if cur_sum == half_sum and dp[i][cur_sum]:
                     return True
-        
         return False
         
+        # 
         
+        def backtrack(i: int, half_sum: int) -> bool:
+            if half_sum < 0 or i >= len(nums):
+                return False
+            if half_sum == 0:
+                return True
+            return backtrack(i+1, half_sum - nums[i]) or backtrack(i+1, half_sum)
+        
+        return backtrack(0, half_sum)
