401f43096a70d25b4afe8c9b9406d90c18e816e3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-11-07 21:39:17 -0800

    Time: 62 ms (28.21%), Space: 47.8 MB (5.64%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.ts
@@ -0,0 +1,21 @@
+function combinationSum3(k: number, n: number): number[][] {
+    const dp = Array(10).fill(0).map(() => Array(n+1).fill(0).map(() => Array(k+1).fill(0).map(() => [])))
+    for (let i=0; i < 10; i++) {
+        dp[i][0][0].push([])
+    }
+    for (let curNum=1; curNum < 10; curNum++) {
+        for (let curTarget=1; curTarget <= n; curTarget++) {
+            for (let i=1; i <= k; i++) {
+                dp[curNum][curTarget][i] = dp[curNum-1][curTarget][i]
+                if (curNum <= curTarget) {
+                    for (const sub of dp[curNum-1][curTarget-curNum][i-1]) {
+                        if (!sub.includes(curNum)) {
+                            dp[curNum][curTarget][i].push([...sub, curNum])
+                        }
+                    }
+                }
+            }
+        }
+    }
+    return dp[9][n][k]
+};
