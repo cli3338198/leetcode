ced42d6aa800823c53d599c1ac9fc77d5a0184f2
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-11-09 09:24:40 -0800

    Time: 77 ms (30.00%), Space: 48 MB (24.83%) - LeetHub

---
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.ts
@@ -0,0 +1,17 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+function removeElements(head: ListNode | null, val: number): ListNode | null {
+    if (!head) return head
+    head.next = removeElements(head.next, val)
+    return head.val === val ? head.next : head
+};
