4973c9065f9ca4b5b87bb8779e2c29eef4342451
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-11-10 10:12:06 -0800

    Time: 44 ms (59.32%), Space: 16.7 MB (9.47%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.py
@@ -1,5 +1,16 @@
 class Solution:
-    def findTheWinner(self, n: int, k: int) -> int:
+    def findTheWinner(self, n: int, k: int) -> int:  
+        # ???
+        
+        def go(n: int, k: int) -> int:
+            if n == 1:
+                return 0
+            return (go(n-1, k) + k) % n
+        
+        return go(n, k) + 1
+        
+        #
+        
         q = deque([n for n in range(1, n+1)])
         while len(q) > 1:
             for i in range(1, k):
