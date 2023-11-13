7098639ffe864e3d22be08f0c5031b43106573dc
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-11-13 15:32:54 -0800

    Time: 59 ms (77.15%), Space: 44.8 MB (64.39%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.ts
@@ -11,19 +11,10 @@
  */
 
 function mergeTwoLists(list1: ListNode | null, list2: ListNode | null): ListNode | null {
-    const dummy = new ListNode()
-    let cur = dummy
-    while (list1 && list2) {
-        if (list1.val < list2.val) {
-            cur.next = list1
-            list1 = list1.next
-        } else {
-            cur.next = list2
-            list2 = list2.next
-        }
-        cur = cur.next
-    }
-    if (list1) cur.next = list1
-    if (list2) cur.next = list2
-    return dummy.next
+    if (!list1) return list2
+    if (!list2) return list1
+    const cur = list1.val < list2.val ? list1 : list2
+    const notCur = cur === list1 ? list2 : list1
+    cur.next = mergeTwoLists(cur.next, notCur)
+    return cur
 };
