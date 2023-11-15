3eeee67cf5cac4aee9f864ee445c580b74b7f8b9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-11-15 10:46:01 -0800

    Time: 107 ms (24.63%), Space: 20 MB (7.67%) - LeetHub

--- 0047-permutations-ii/0047-permutations-ii.py
+++ 0047-permutations-ii/0047-permutations-ii.py
@@ -1,6 +1,21 @@
 class Solution:
     def permuteUnique(self, nums: List[int]) -> List[List[int]]:
         nums.sort()
+        n = len(nums)
+        dp = [[[] for _ in range(0, n+1)] for _ in range(0, n+1)]
+        for row in dp:
+            row[0].append([])
+        for row in range(1, n+1):
+            for col in range(1, n+1):
+                for sub in dp[row-1][col-1]:
+                    l = 1 if len(sub) == 1 and sub[0] == nums[col-1] else len(sub)+1
+                    for i in range(0, l):
+                        dp[row][col].append(sub[0:i:] + [nums[col-1]] + sub[i::])
+        return map(list, set(map(tuple, dp[-1][-1])))
+        
+        #
+        
+        nums.sort()
         
         def permute(nums: List[int]) -> List[List[int]]:
             if len(nums) == 1:
