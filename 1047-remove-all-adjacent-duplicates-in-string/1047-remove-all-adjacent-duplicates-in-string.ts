18038386483876116790129f71d98ec828fbdc86
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-11-16 09:26:03 -0800

    Time: 72 ms (84.16%), Space: 52.4 MB (28.71%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.ts
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.ts
@@ -1,4 +1,14 @@
 function removeDuplicates(s: string): string {
+    const arr = s.split("")
+    let i = 0
+    for (let j=0; j < s.length; j++, i++) {
+        arr[i] = arr[j]
+        if (j > 0 && arr[i] === arr[i-1]) i -= 2
+    }
+    return arr.slice(0, i).join("")
+    
+    //
+    
     const stack = []
     for (const c of s) {
         if (stack.length && stack.at(-1) === c) {
