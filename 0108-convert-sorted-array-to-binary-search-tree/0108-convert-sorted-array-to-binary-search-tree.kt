cbcdc2abd92fc8ecb885d69713726da94e8d57b9
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-11-28 14:51:24 -0800

    Time: 151 ms (92.83%), Space: 37.2 MB (58.57%) - LeetHub

--- 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.kt
+++ 0108-convert-sorted-array-to-binary-search-tree/0108-convert-sorted-array-to-binary-search-tree.kt
@@ -10,11 +10,24 @@
  */
 class Solution {
     fun sortedArrayToBST(nums: IntArray): TreeNode? {
-        if (nums.size === 0) return null
-        val mid = nums.size/2
-        val root = TreeNode(nums[mid])
-        root.left = sortedArrayToBST(nums.sliceArray(0 until mid))
-        root.right = sortedArrayToBST(nums.sliceArray(mid+1 until nums.size))
+        return go(0, nums.size-1, nums)
+        
+        //
+        
+        // if (nums.size === 0) return null
+        // val mid = nums.size/2
+        // val root = TreeNode(nums[mid])
+        // root.left = sortedArrayToBST(nums.sliceArray(0 until mid))
+        // root.right = sortedArrayToBST(nums.sliceArray(mid+1 until nums.size))
+        // return root
+    }
+    
+    fun go(l: Int, r: Int, nums: IntArray): TreeNode? {
+        if (l > r) return null
+        val m = (l+r)/2
+        val root = TreeNode(nums[m])
+        root.left = go(l, m-1, nums)
+        root.right = go(m+1, r, nums)
         return root
     }
 }
