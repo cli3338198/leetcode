7487b37c56fbb7f20321a5cb71bddd1c3d7164eb
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-11-30 09:23:44 -0800

    Time: 74 ms (51.19%), Space: 49.6 MB (80.24%) - LeetHub

---
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.ts
@@ -0,0 +1,23 @@
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+function searchBST(root: TreeNode | null, val: number): TreeNode | null {
+    if (!root || root.val === val) {
+        return root
+    } else if (root.val < val) {
+        return searchBST(root.right, val)
+    } else {
+        return searchBST(root.left, val)
+    }
+};
