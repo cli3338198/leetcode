d95f808237fcf8ab5909e69f0b0044df9b71b65e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-12-05 09:26:37 -0800

    Time: 79 ms (6.54%), Space: 46.7 MB (26.92%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.ts
@@ -14,5 +14,21 @@
 
 function maxDepth(root: TreeNode | null): number {
     if (!root) return 0
+    const q: TreeNode[] = [root]
+    let depth = 0
+    while (q.length) {
+        const len = q.length
+        for (let i=0; i < len; i++) {
+            const {left, right} = q.shift()
+            if (left) q.push(left)
+            if (right) q.push(right)
+        }
+        depth++
+    }
+    return depth
+    
+    //
+    
+    if (!root) return 0
     return 1 + Math.max(maxDepth(root.left), maxDepth(root.right))
 };
