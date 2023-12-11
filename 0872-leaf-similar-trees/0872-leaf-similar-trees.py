19764557930b0da1d3f6d53bc8bc7c1c7f400b74
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-12-11 11:38:51 -0800

    Time: 45 ms (19.33%), Space: 16.4 MB (10.89%) - LeetHub

--- 0872-leaf-similar-trees/0872-leaf-similar-trees.py
+++ 0872-leaf-similar-trees/0872-leaf-similar-trees.py
@@ -5,6 +5,19 @@
 #         self.left = left
 #         self.right = right
 class Solution:
+    def ite(self, root: Optional[TreeNode]) -> List[int]:
+        stack, res = [], []
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+            else:
+                root = stack.pop()
+                if not root.left and not root.right:
+                    res.append(root.val)
+                root = root.right
+        return res
+    
     def dfs(self, root: Optional[TreeNode], res: List[int]) -> List[int]:
         if not root:
             return []
@@ -15,6 +28,10 @@ class Solution:
         return res
     
     def leafSimilar(self, root1: Optional[TreeNode], root2: Optional[TreeNode]) -> bool:
+        return self.ite(root1) == self.ite(root2)
+        
+        #
+        
         list1, list2 = self.dfs(root1, []), self.dfs(root2, [])
         if len(list1) != len(list2):
             return False
