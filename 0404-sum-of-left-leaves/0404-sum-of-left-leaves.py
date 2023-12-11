af4f8def475bc328f200dc5e66d84cb65fb54dd8
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-12-11 12:03:06 -0800

    Time: 41 ms (51.68%), Space: 16.5 MB (93.22%) - LeetHub

--- 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
+++ 0404-sum-of-left-leaves/0404-sum-of-left-leaves.py
@@ -6,6 +6,20 @@
 #         self.right = right
 class Solution:
     def sumOfLeftLeaves(self, root: Optional[TreeNode]) -> int:
+        stack, res = [], 0
+        while stack or root:
+            if root:
+                stack.append(root)
+                root = root.left
+                if root and not root.left and not root.right:
+                    res += root.val
+            else:
+                root = stack.pop()
+                root = root.right
+        return res
+        
+        #
+        
         if not root:
             return 0
         res = 0
