6729ac912912fa597e07313a6c885139d1aca13b
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-12-12 14:35:54 -0800

    Time: 83 ms (63.09%), Space: 49.7 MB (52.38%) - LeetHub

--- 0784-letter-case-permutation/0784-letter-case-permutation.ts
+++ 0784-letter-case-permutation/0784-letter-case-permutation.ts
@@ -1,11 +1,11 @@
 function letterCasePermutation(s: string): string[] {
-    const dp: Set<string>[] = Array(s.length+1).fill(0).map(() => new Set())
-    dp[0].add("")
+    const dp: string[][] = Array(s.length+1).fill(0).map(() => [])
+    dp[0].push("")
     for (let i=1; i <= s.length; i++) {
         for (const sub of dp[i-1]) {
-            dp[i].add(sub + s[i-1].toLowerCase())
-            dp[i].add(sub + s[i-1].toUpperCase())
+            dp[i].push(sub + s[i-1].toLowerCase())
+            if (s[i-1].toLowerCase() !== s[i-1].toUpperCase()) dp[i].push(sub + s[i-1].toUpperCase())
         }
     }
-    return Array.from(dp[s.length])
+    return dp.at(-1)
 };
