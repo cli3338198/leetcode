bcd2cc919db015eed328224094877aed87054078
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-12-18 10:49:21 -0800

    Time: 7032 ms (21.81%), Space: 16.4 MB (23.65%) - LeetHub

--- 0079-word-search/0079-word-search.py
+++ 0079-word-search/0079-word-search.py
@@ -1,21 +1,21 @@
 class Solution:
-    def dfs(self, board: List[List[str]], r: int, c: int, word: str, i: int) -> bool:
-        if i == len(word):
-            return True
-        if r < 0 or r >= len(board) or c < 0 or c >= len(board[0]) or board[r][c] != word[i]:
-            return False
-        res = False
-        temp = board[r][c]
-        board[r][c] = ""
-        for dr, dc in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
-            res = res or self.dfs(board, r+dr, c+dc, word, i+1)
-        board[r][c] = temp
-        return res
-    
     def exist(self, board: List[List[str]], word: str) -> bool:
+        def backtrack(r: int, c: int, i: int) -> bool:
+            if i == len(word):
+                return True
+            if r < 0 or r >= len(board) or c < 0 or c >= len(board[r]) or board[r][c] != word[i]:
+                return False
+            temp = board[r][c]
+            board[r][c] = ""
+            for r1, c1 in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+                r2, c2 = r + r1, c + c1
+                if backtrack(r2, c2, i+1):
+                    return True
+            board[r][c] = temp
+            return False
+        
         for r in range(0, len(board)):
-            for c in range(0, len(board[0])):
-                if board[r][c] == word[0]:
-                    if self.dfs(board, r, c, word, 0):
-                        return True
+            for c in range(0, len(board[r])):
+                if backtrack(r, c, 0):
+                    return True
         return False
