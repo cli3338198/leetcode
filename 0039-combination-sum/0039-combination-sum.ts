b636ff09026052c78befca2b567d2c09b63275dd
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-12-18 12:41:22 -0800

    Time: 84 ms (23.45%), Space: 49.1 MB (35.71%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.ts
+++ 0039-combination-sum/0039-combination-sum.ts
@@ -1,4 +1,19 @@
 function combinationSum(candidates: number[], target: number): number[][] {
+    const dp = Array(target+1).fill(0).map(() => [])
+    dp[0].push([])
+    for (const c of candidates) {
+        for (let t=1; t <= target; t++) {
+            if (c <= t) {
+                for (const sub of dp[t-c]) {
+                    dp[t].push([...sub, c])
+                }
+            }
+        }
+    }
+    return dp.at(-1)
+    
+    //
+    
     const res: number[][] = []
     backtrack(0, target, [])
     return res
