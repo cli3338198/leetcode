0a086575f313fe6bc8b4a2d6916ef2962699a2cb
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2023-12-18 10:29:11 -0800

    Time: 42 ms (31.30%), Space: 16.4 MB (10.48%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.py
@@ -1,8 +1,24 @@
 class Solution:
+    def __init__(self):
+        self.map = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        self.combinations = []
+    
+    def rec(self, i: int, digits: str, cur: str) -> None:
+        if i == len(digits):
+            self.combinations.append(cur)
+            return
+        for char in self.map[digits[i]]:
+            self.rec(i+1, digits, cur+char)
+    
     def letterCombinations(self, digits: str) -> List[str]:
         if not digits:
             return []
         mp = {"2": "abc", "3": "def", "4": "ghi", "5": "jkl", "6": "mno", "7": "pqrs", "8": "tuv", "9": "wxyz"}
+        self.rec(0, digits, "")
+        return self.combinations
+        
+        #
+        
         dp = [[] for _ in range(0, len(digits)+1)]
         dp[0].append("")
         for i in range(1, len(digits)+1):
