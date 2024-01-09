07ebccd5dd16b6c34ed63f53ce4bd30316fde0e4
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-01-09 09:39:47 -0800

    Update 0062-unique-paths.py

--- 0062-unique-paths/0062-unique-paths.py
+++ 0062-unique-paths/0062-unique-paths.py
@@ -1,5 +1,22 @@
 class Solution:
     def uniquePaths(self, m: int, n: int) -> int:
+        @lru_cache(None)
+        def rec(m: int, n: int) -> int:
+            if m == 1 or n == 1:
+                return 1
+            return rec(m-1, n) + rec(m, n-1)
+        
+        return rec(m, n)
+        
+        #
+        
+        dp = [[1 for _ in range(0, n)] for _ in range(0, m)]
+        for i in range(m-2, -1, -1):
+            for j in range(n-2, -1, -1):
+                dp[i][j] = dp[i+1][j] + dp[i][j+1]
+        return dp[0][0]
+
+        #
         
         dp = [[1] * n for _ in range(m)]
         
