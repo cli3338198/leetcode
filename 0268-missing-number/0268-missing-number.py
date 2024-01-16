72779846e9169996f2ae04955a66ff4cea8fa112
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-01-16 11:09:57 -0800

    Time: 100 ms (99.02%), Space: 18.5 MB (45.33%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,6 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums.sort()
+        cur_sum = 0
+        for i in range(0, len(nums) + 1):
+            cur_sum += i
+        for n in nums:
+            cur_sum -= n
+        return cur_sum
+        
+        #
+        
         for i in range(0, len(nums)):
             if nums[i] != i:
                 return i
