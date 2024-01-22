cecc9bae6056109503e8637f4ea811a2f597ce86
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-01-22 12:46:09 -0800

    Time: 112 ms (29.88%), Space: 60.1 MB (16.28%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.ts
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -5,6 +5,36 @@ function numIslands(grid: string[][]): number {
         for (let c=0; c < cols; c++) {
             if (grid[r][c] === '1') {
                 count++
+                bfs(r, c)
+            }
+        }
+    }
+    return count
+    
+    function bfs(r: number, c: number) {
+        const q: [number, number][] = [[r, c]], visited = new Set<string>()
+        while (q.length) {
+            const [r, c] = q.shift()
+            const key = `${r} ${c}`
+            if (!visited.has(key)) {
+                visited.add(key)
+                grid[r][c] = '0'
+                for (const [rr, cc] of [[1, 0], [0, 1], [-1, 0], [0, -1]]) {
+                    const rrr = rr + r, ccc = cc + c
+                    if (rrr in grid && ccc in grid[rrr] && grid[rrr][ccc] === '1') {
+                        q.push([rrr, ccc])
+                    }
+                }
+            }
+        }
+    }
+    
+    //
+    
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < cols; c++) {
+            if (grid[r][c] === '1') {
+                count++
                 dfs(r, c)
             }
         }
