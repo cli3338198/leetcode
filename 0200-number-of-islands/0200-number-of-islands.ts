3ef07ef0e36b4b18fe5165f11b8c4187165de257
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-01-22 12:58:26 -0800

    Time: 183 ms (10.37%), Space: 72.5 MB (5.02%) - LeetHub

--- 0200-number-of-islands/0200-number-of-islands.ts
+++ 0200-number-of-islands/0200-number-of-islands.ts
@@ -1,5 +1,52 @@
+class UF<T extends string> {
+    constructor(private root: Record<T, T> = {} as Record<T, T>, private distinct: number = 0) {
+    }
+    insert(a: T) {
+        if (!(a in this.root)) {
+            this.root[a] = a
+            this.distinct++
+        }
+    }
+    find(a: T) {
+        if (this.root[a] !== a) {
+            this.root[a] = this.find(this.root[a])
+        }
+        return this.root[a]
+    }
+    union(a: T, b: T) {
+        const A = this.find(a), B = this.find(b)
+        if (A !== B) {
+            this.root[B] = A
+            this.distinct--
+        }
+    }
+    getDistinct() {
+        return this.distinct
+    }
+}
+
 function numIslands(grid: string[][]): number {
-    const rows = grid.length, cols = grid[0].length
+    const rows = grid.length, cols = grid[0].length, uf = new UF<string>()
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < cols; c++) {
+            if (grid[r][c] === '1') {
+                const a = `${r} ${c}`
+                uf.insert(a)
+                for (const [rr, cc] of [[1, 0], [0, 1], [-1, 0], [0, -1]]) {
+                    const rrr = rr + r, ccc = cc + c
+                    if (rrr in grid && ccc in grid[rrr] && grid[rrr][ccc] === '1') {
+                        const b = `${rrr} ${ccc}`
+                        uf.insert(b)
+                        uf.union(a, b)
+                    }
+                }
+            }
+        }
+    }
+    return uf.getDistinct()
+    
+    // 
+    
     let count = 0
     for (let r=0; r < rows; r++) {
         for (let c=0; c < cols; c++) {
