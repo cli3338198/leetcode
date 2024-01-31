47b7d5785584e109b0a8d7a676d006a16be0e6a4
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-01-31 11:41:24 -0800

    Time: 64 ms (52.88%), Space: 16.7 MB (5.87%) - LeetHub

--- 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
+++ 1266-minimum-time-visiting-all-points/1266-minimum-time-visiting-all-points.py
@@ -4,6 +4,14 @@ class Solution:
         return min(abs(y1 - y0), abs(x1 - x0)) + max(abs(y1 - y0), abs(x1 - x0)) - min(abs(y1 - y0), abs(x1 - x0))
     
     def minTimeToVisitAllPoints(self, points: List[List[int]]) -> int:
+        # Chebyshev Distance
+        res = 0
+        for i in range(1, len(points)):
+            res += max(abs(points[i][0] - points[i-1][0]), abs(points[i][1] - points[i-1][1]))
+        return res
+        
+        #
+        
         ans = 0
         for i in range(1, len(points)):
             ans += self.distance(points[i-1], points[i])
