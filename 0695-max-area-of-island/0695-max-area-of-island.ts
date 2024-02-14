4a9d62512d4d0ab17f50a7a0e3c1b4edfdad7ca7
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-02-13 20:57:01 -0800

    Time: 131 ms (12.80%), Space: 58.5 MB (5.88%) - LeetHub

--- 0695-max-area-of-island/0695-max-area-of-island.ts
+++ 0695-max-area-of-island/0695-max-area-of-island.ts
@@ -1,6 +1,46 @@
 function maxAreaOfIsland(grid: number[][]): number {
-    const visited = new Set<string>()
+    const parent: Record<string, string> = {}
+    const size: Record<string, number> = {}
     const rows = grid.length, cols = grid[0].length
+    const add = (root: string) => {
+        if (!(root in parent)) {
+            parent[root] = root
+            size[root] = 1
+        }
+    }
+    const find = (root: string) => {
+        if (parent[root] !== root) {
+            parent[root] = find(parent[root])
+        }
+        return parent[root]
+    }
+    const union = (root1: string, root2: string) => {
+        const a = find(root1), b = find(root2)
+        if (a !== b) {
+            const [big, small] = [a, b].sort((x, y) => size[y] - size[x])
+            parent[small] = big
+            size[big] += size[small]
+        }
+    }
+    for (let r=0; r < rows; r++) {
+        for (let c=0; c < cols; c++) {
+            if (grid[r][c] === 1) {
+                add(`${r} ${c}`)
+                for (const [rr, cc] of [[1, 0], [0, 1], [-1, 0], [0, -1]]) {
+                    const rrr = rr + r, ccc = cc + c
+                    if (rrr in grid && ccc in grid[rrr] && grid[rrr][ccc] === 1) {
+                        add(`${rrr} ${ccc}`)
+                        union(`${r} ${c}`, `${rrr} ${ccc}`)
+                    }
+                }
+            }
+        }
+    }
+    return Math.max(0, ...Object.values(size))
+    
+    //
+    
+    const visited = new Set<string>()
     let res = 0
     for (let r=0; r < rows; r++) {
         for (let c=0; c < cols; c++) {
