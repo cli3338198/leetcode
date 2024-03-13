206a3b7161b7eef79cbd824920c365e4aa2613ca
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-13 14:20:28 -0700

    Time: 86 ms (35.94%), Space: 61.1 MB (26.57%) - LeetHub

--- 0896-monotonic-array/0896-monotonic-array.js
+++ 0896-monotonic-array/0896-monotonic-array.js
@@ -3,8 +3,11 @@
  * @return {boolean}
  */
 var isMonotonic = function(nums) {
+    if (nums.length <= 1) {
+        return true
+    }
     let increasing = false, decreasing = false
-    for (let i=0; i < nums.length; i++) {
+    for (let i=1; i < nums.length; i++) {
         if (nums[i - 1] < nums[i]) {
             increasing = true
         } else if (nums[i - 1] > nums[i]) {
