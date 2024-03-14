cfcca4ad2491cdccc923ff394290b63174607de3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-13 20:42:03 -0700

    Time: 61 ms (91.86%), Space: 52.8 MB (55.05%) - LeetHub

--- 0905-sort-array-by-parity/0905-sort-array-by-parity.ts
+++ 0905-sort-array-by-parity/0905-sort-array-by-parity.ts
@@ -1,4 +1,20 @@
 function sortArrayByParity(nums: number[]): number[] {
+    let left = 0, right = nums.length - 1
+    while (left < right) {
+        if (nums[left] % 2 === 0) {
+            left++
+        } else if (nums[right] % 2 !== 0) {
+            right--
+        } else {
+            [nums[left], nums[right]] = [nums[right], nums[left]]
+            left++
+            right--
+        }
+    }
+    return nums
+    
+    //
+    
     let i = 0
     for (let j=0; j < nums.length; j++) {
         if (nums[j] % 2 === 0) {
