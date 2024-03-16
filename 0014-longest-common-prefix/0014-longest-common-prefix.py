3913fe8158fdac3449a6ac4112fb52cf733b508d
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-15 19:19:43 -0700

    Time: 31 ms (91.77%), Space: 16.6 MB (48.78%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,45 +1,8 @@
-class Trie:
-    def __init__(self):
-        self.children = {}
-        self.word = ""
-        self.size = 0
-    
-    def insert(self, word: str) -> None:
-        cur = self
-        for c in word:
-            if c not in cur.children:
-                cur.children[c] = Trie()
-                cur.children[c].word = cur.word + c
-            cur.children[c].size += 1
-            cur = cur.children[c]
-            
-    def findLongest(self, numberWords: int) -> str:
-        cur = self
-        while True:
-            haveFound = False
-            for c in cur.children:
-                if cur.children[c].size == numberWords:
-                    cur = cur.children[c]
-                    haveFound = True
-            if not haveFound:
-                break
-        return cur.word
-        
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
         strs.sort()
-        a, b, res = strs[0], strs[-1], ""
-        i = j = 0
-        while i < len(a) and j < len(b) and a[i] == b[j]:
-            res += a[i]
+        res, i = "", 0
+        while i < min(len(strs[0]), len(strs[-1])) and strs[0][i] == strs[-1][i]:
+            res += strs[0][i]
             i += 1
-            j += 1
         return res
-        
-        #
-        
-        trie = Trie()
-        for word in strs:
-            trie.insert(word)
-        return trie.findLongest(len(strs))
-            
