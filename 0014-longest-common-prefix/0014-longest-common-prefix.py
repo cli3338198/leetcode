8ebc5151b37e16d6b4b60b4a39598a5363175cb7
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-15 19:42:16 -0700

    Time: 44 ms (18.16%), Space: 16.8 MB (16.75%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -23,6 +23,15 @@ class Trie:
 
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        for i in range(0, len(strs[0])):
+            char = strs[0][i]
+            for s in strs[1::]:
+                if len(s) == i or s[i] != char:
+                    return s[0:i:]
+        return strs[0]
+        
+        #
+        
         trie = Trie()
         for word in strs:
             trie.insert(word)
