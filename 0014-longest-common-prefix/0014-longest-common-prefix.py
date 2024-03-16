b1c6d5b78c7596e70ec37a0a297fe08c2a5d4e4c
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-15 19:38:04 -0700

    Time: 45 ms (13.43%), Space: 16.9 MB (9.39%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -1,5 +1,35 @@
+class Trie:
+    def __init__(self):
+        self.children = {}
+        self.common = 0
+        self.prefix = ""
+    
+    def insert(self, word: str) -> None:
+        cur = self
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = Trie()
+                cur.children[c].prefix = cur.prefix + c
+            cur = cur.children[c]
+            cur.common += 1
+        
+    def traverse(self, common: str, cur: "Trie") -> str:
+        if not cur:
+            return ""
+        for child in cur.children:
+            if cur.children[child].common == common:
+                return self.traverse(common, cur.children[child])
+        return cur.prefix
+
 class Solution:
     def longestCommonPrefix(self, strs: List[str]) -> str:
+        trie = Trie()
+        for word in strs:
+            trie.insert(word)
+        return trie.traverse(len(strs), trie)
+        
+        #
+        
         if len(strs) == 1:
             return strs[0]
         length, res = len(strs[0]), ""
