fd38530b976cbf8980d01feaea7989e3c93b3fa3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-18 21:20:34 -0700

    Time: 77 ms (68.26%), Space: 54.9 MB (70.29%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.ts
@@ -0,0 +1,13 @@
+function insert(intervals: number[][], newInterval: number[]): number[][] {
+    intervals.push(newInterval)
+    intervals.sort((a, b) => a[0] - b[0] || b[1] - a[1])
+    const res = [intervals[0]]
+    for (let i=1; i < intervals.length; i++) {
+        if (intervals[i][0] <= res.at(-1)[1]) {
+            res.at(-1)[1] = Math.max(res.at(-1)[1], intervals[i][1])
+        } else {
+            res.push(intervals[i])
+        }
+    }
+    return res
+};
