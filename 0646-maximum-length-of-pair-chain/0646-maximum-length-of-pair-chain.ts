521ba6b6818202c87ff95eac463707ba7f80f813
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-21 13:45:55 -0700

    Time: 70 ms (83.33%), Space: 55.1 MB (22.22%) - LeetHub

--- 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.ts
+++ 0646-maximum-length-of-pair-chain/0646-maximum-length-of-pair-chain.ts
@@ -1,4 +1,6 @@
 function findLongestChain(pairs: number[][]): number {
+    // sorting by end is greedy, can see the future
+    
     pairs.sort((a, b) => a[1] - b[1])
     let last = pairs[0][1], res = 1
     for (let i=1; i < pairs.length; i++) {
@@ -10,7 +12,7 @@ function findLongestChain(pairs: number[][]): number {
     }
     return res
     
-    //
+    // sorting by start is dp, cannot see the future
     
     pairs.sort((a, b) => a[0] - b[0])
     const dp = Array(pairs.length).fill(1)
