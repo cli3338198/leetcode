19cbb06e7ffcf176246408dff7e5562da1d1f3b1
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-21 12:11:00 -0700

    Time: 77 ms (49.40%), Space: 16.5 MB (71.23%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -1,5 +1,19 @@
 class Solution:
     def numberOfPoints(self, nums: List[List[int]]) -> int:
+        nums.sort(key=lambda x: x[0])
+        arr = []
+        for start, end in nums:
+            if arr and arr[-1][1] >= start:
+                arr[-1][1] = max(arr[-1][1], end)
+            else:
+                arr.append([start, end])
+        points = 0
+        for start, end in arr:
+            points += end - start + 1
+        return points
+        
+        #
+        
         points = [0] * (100 + 1)
         for start, end in nums:
             for i in range(start, end + 1):
