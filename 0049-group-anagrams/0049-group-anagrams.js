c8f5c939422ab764cd920da2427aaa9e26539c0e
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-22 12:19:23 -0700

    Time: 126 ms (32.74%), Space: 65 MB (6.57%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.js
+++ 0049-group-anagrams/0049-group-anagrams.js
@@ -5,12 +5,8 @@
 var groupAnagrams = function(strs) {
     const map = {}
     for (const str of strs) {
-        const ana = str.split("").sort().join("")
-        if (ana in map) {
-            map[ana].push(str)
-        } else {
-            map[ana] = [str]
-        }
+        const key = str.split("").sort().join("")
+        map[key] = [...map[key] || [], str] 
     }
     return Object.values(map)
 };
