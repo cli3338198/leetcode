551e2e8222a0b559cdf4129bf54d0dea1fd359e7
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-22 10:04:06 -0700

    Time: 106 ms (29.25%), Space: 59.1 MB (50.59%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.js
+++ 0056-merge-intervals/0056-merge-intervals.js
@@ -4,12 +4,13 @@
  */
 var merge = function(intervals) {
     intervals.sort((a, b) => a[0] - b[0])
-    const res = []
-    for (const interval of intervals) {
-        if (!res.length || interval[0] > res.at(-1)[1]) {
-            res.push(interval)
+    const res = [intervals[0]]
+    for (let i=1; i < intervals.length; i++) {
+        const [start, end] = intervals[i]
+        if (start <= res.at(-1)[1]) {
+            res.at(-1)[1] = Math.max(res.at(-1)[1], end)
         } else {
-            res.at(-1)[1] = Math.max(res.at(-1)[1], interval[1])
+            res.push(intervals[i])
         }
     }
     return res
