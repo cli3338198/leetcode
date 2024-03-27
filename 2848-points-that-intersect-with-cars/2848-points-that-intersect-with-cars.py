308606176697a83107c660137194abefba4923f6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-27 11:59:56 -0700

    Time: 83 ms (20.66%), Space: 16.6 MB (24.72%) - LeetHub

--- 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
+++ 2848-points-that-intersect-with-cars/2848-points-that-intersect-with-cars.py
@@ -10,3 +10,36 @@ class Solution:
             if cur > 0:
                 res += 1
         return res
+    
+        # 
+        
+        nums.sort(key=lambda x: x[0])
+        points = last = 0
+        for start, end in nums:
+            if end <= last:
+                continue
+            points += (end - max(last + 1, start) + 1)
+            last = end
+        return points
+        
+        #
+        
+        nums.sort(key=lambda x: x[0])
+        arr = []
+        for start, end in nums:
+            if arr and arr[-1][1] >= start:
+                arr[-1][1] = max(arr[-1][1], end)
+            else:
+                arr.append([start, end])
+        points = 0
+        for start, end in arr:
+            points += end - start + 1
+        return points
+        
+        #
+        
+        points = [0] * (100 + 1)
+        for start, end in nums:
+            for i in range(start, end + 1):
+                points[i] = 1
+        return sum(points)
