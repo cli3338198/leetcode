252e1f3f37c23193f9e06bc21aaba1db13c00e0a
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-28 09:33:45 -0700

    Time: 61 ms (16.11%), Space: 49 MB (35.46%) - LeetHub

--- 0027-remove-element/0027-remove-element.js
+++ 0027-remove-element/0027-remove-element.js
@@ -7,8 +7,23 @@ var removeElement = function(nums, val) {
     let i = 0
     for (let j=0; j < nums.length; j++) {
         if (nums[j] !== val) {
-            nums[i++] = nums[j]
+            nums[i] = nums[j]
+            i++
         }
     }
     return i
+    
+    //
+    
+    let l = 0, r = nums.length - 1
+    while (l < r) {
+        if (nums[l] === val) {
+            [nums[l], nums[r]] = [nums[r], nums[l]]
+        }
+        if (nums[l] !== val) {
+            l++
+        }
+        r--
+    }
+    return l
 };
