f987b6dbd321ca8955f9e51e7f1475e88696a47c
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-28 09:51:58 -0700

    Time: 55 ms (85.74%), Space: 51.5 MB (55.77%) - LeetHub

---
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
@@ -0,0 +1,13 @@
+function twoSum(numbers: number[], target: number): number[] {
+    let l = 0, r = numbers.length - 1
+    while (l < r) {
+        const sum = numbers[l] + numbers[r]
+        if (sum === target) {
+            return [l + 1, r + 1]
+        } else if (sum > target) {
+            r--
+        } else {
+            l++
+        }
+    }
+};
