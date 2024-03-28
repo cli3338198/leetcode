6f4b766417e6601713087c7071d921b0d97a02b6
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-28 09:59:56 -0700

    Time: 56 ms (83.28%), Space: 52.4 MB (25.48%) - LeetHub

--- 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
+++ 0167-two-sum-ii-input-array-is-sorted/0167-two-sum-ii-input-array-is-sorted.ts
@@ -1,4 +1,21 @@
 function twoSum(numbers: number[], target: number): number[] {
+    for (let i=0; i < numbers.length; i++) {
+        const complement = target - numbers[i]
+        let left = i + 1, right = numbers.length - 1
+        while (left < right) {
+            const mid = Math.floor((left + right) / 2)
+            if (numbers[mid] === complement) {
+                return [i + 1, mid + 1]
+            } else if (numbers[mid] > complement) {
+                right = mid - 1
+            } else {
+                left = mid + 1
+            }
+        }
+    }
+    
+    //
+    
     let l = 0, r = numbers.length - 1
     while (l < r) {
         const sum = numbers[l] + numbers[r]
