a798db95b370963a0cf4f62132d2d4175a0a95c3
cli3338198 <101161751+cli3338198@users.noreply.github.com>
2024-03-31 22:59:07 -0700

    Time: 39 ms (70.09%), Space: 16.8 MB (10.21%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,15 +1,9 @@
-class Solution(object):
-    def permute(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: List[List[int]]
-        """
-        n = len(nums)
-        dp = [[] for _ in range(0, n+1)]
-        dp[0].append([])
-        for i in range(1, n+1):
-            for previous_permutation in dp[i-1]:
-                length = len(previous_permutation)
-                for k in range(0, length+1):
-                    dp[i].append(previous_permutation[0:k:] + [nums[i-1]] + previous_permutation[k::])
-        return dp[n]
+class Solution:
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if not nums:
+            return [[]]
+        res = []
+        for i in range(0, len(nums)):
+            for sub in self.permute(nums[0:i:] + nums[i+1::]):
+                res.append([nums[i]] + sub)
+        return res
